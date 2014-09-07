1c89577f6b5238c9728641ec2036a556ae87f1da
Ryan Thompson <service@aiwebsystems.com>
2014-09-06 21:07:01 -0500

    Don't need this anymore

--- src/Streams/Core/Ui/Collection/FormFieldCollection.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Streams\Core\Ui\Collection;
-
-use Illuminate\Support\Collection;
-
-class FormFieldCollection extends Collection
-{
-}
--- src/Streams/Core/Ui/Collection/FormTabCollection.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Streams\Core\Ui\Collection;
-
-use Illuminate\Support\Collection;
-
-class FormTabCollection extends Collection
-{
-}
