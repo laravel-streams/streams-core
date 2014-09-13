512293c08b0dc4aff77b0343631e76ce30944a04
Ryan Thompson <service@aiwebsystems.com>
2014-09-13 10:22:25 -0500

    Boot after so the observers get a chance to bind to the dispatcher

--- src/Streams/Core/Http/Filter/BootFilter.php
+++ src/Streams/Core/Http/Filter/BootFilter.php
@@ -1,5 +1,16 @@
 <?php namespace Streams\Core\Http\Filter;
 
+use Streams\Core\Assignment\Model\AssignmentModel;
+use Streams\Core\Assignment\Observer\AssignmentObserver;
+use Streams\Core\Entry\Model\EntryModel;
+use Streams\Core\Entry\Observer\EntryObserver;
+use Streams\Core\Field\Model\FieldModel;
+use Streams\Core\Field\Observer\FieldObserver;
+use Streams\Core\Model\EloquentModel;
+use Streams\Core\Model\Observer\EloquentObserver;
+use Streams\Core\Stream\Model\StreamModel;
+use Streams\Core\Stream\Observer\StreamObserver;
+
 class BootFilter
 {
 
@@ -36,6 +47,13 @@ class BootFilter
             // Set some placeholders.
             \View::share('title', null);
             \View::share('description', null);
+
+            // Set observer on core models.
+            EntryModel::observe(new EntryObserver());
+            FieldModel::observe(new FieldObserver());
+            StreamModel::observe(new StreamObserver());
+            EloquentModel::observe(new EloquentObserver());
+            AssignmentModel::observe(new AssignmentObserver());
         }
     }
 
