17077be6e72ba5d115a0f1ddbd0384c92c332fb0
Ryan Thompson <service@aiwebsystems.com>
2014-09-16 10:03:31 -0500

    Meta lang.. this will pry go to theme too

--- resources/lang/en/misc.php
+++ resources/lang/en/misc.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'untitled' => 'Untitled',
+    'untitled'   => 'Untitled',
+    'meta'       => 'Loaded in :time using :memory',
+    'pagination' => 'Showing results FROM - TO of TOTAL'
 ];
