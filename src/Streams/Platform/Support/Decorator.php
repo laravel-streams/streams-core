c1531245043289ca7db7f4c1cb43a10a52e02947
Ryan Thompson <service@aiwebsystems.com>
2014-09-21 03:56:20 -0500

    Decorate relationships son

--- src/Streams/Platform/Support/Decorator.php
+++ src/Streams/Platform/Support/Decorator.php
@@ -1,5 +1,6 @@
 <?php namespace Streams\Platform\Support;
 
+use Streams\Platform\Model\EloquentModel;
 use Streams\Platform\Contract\PresenterInterface;
 
 class Decorator
@@ -7,15 +8,54 @@ class Decorator
     /**
      * Decorate a value.
      *
-     * @param mixed $atom
+     * @param mixed $resource
      * @return mixed
      */
     public function decorate($value)
     {
+        if ($value instanceOf EloquentModel) {
+            $value = $this->decorateRelations($value);
+        }
+
         if ($value instanceof PresenterInterface) {
             return $value->newPresenter($value);
         }
 
         return $value;
+    }
+
+    /**
+     * Decorate model relationships.
+     *
+     * @param EloquentModel $resource
+     * @return mixed
+     */
+    protected function decorateRelations(EloquentModel $resource)
+    {
+        foreach ($resource->getRelations() as $relationName => $model) {
+            if ($model instanceOf Collection) {
+                $model = $this->decorateCollection($model);
+                $resource->setRelation($relationName, $model);
+            } else {
+                $resource->setRelation($relationName, $model->newPresenter($model));
+            }
+        }
+
+        return $resource;
+    }
+
+    /**
+     * Decorate a collection of resources.
+     *
+     * @param Collection $collection
+     * @return Collection
+     */
+    protected function decorateCollection(Collection $collection)
+    {
+        foreach ($collection as $resource) {
+            $collection->put($resource, $resource->newPresenter($resource));
+        }
+
+        return $collection;
     }
 }
