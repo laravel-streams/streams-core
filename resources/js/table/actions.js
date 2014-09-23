0bd7988b3f39e0ad131bea052c80eb32d72c194d
Ryan Thompson <service@aiwebsystems.com>
2014-09-23 00:10:56 -0500

    Actions!

--- resources/js/table/actions.js
+++ resources/js/table/actions.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
 
-    alert();
+    // Toggle all table actions
+    $('[data-toggle="all"]').on('change', function () {
+        $(this).closest('table').find(':checkbox').prop('checked', $(this).is(':checked'));
+    });
+
+    // Disable / enable actions when checkboxs are checked
+    $('input[type="checkbox"]').on('change', function () {
+        if ($('input[type="checkbox"]:checked').length > 0) {
+            $('.actions button').removeAttr('disabled');
+        } else {
+            $('.actions button').attr('disabled', 'disabled');
+        }
+    });
+
+    // When an action is clicked - do something
+    $('[data-action]').on('click', function (e) {
+        $(this).closest('form').attr('action', $(this).data('action')).submit();
+    });
 });
