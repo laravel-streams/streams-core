28cc1daeef9f0bb47c5324f0db1a4947c455bd7f
Ryan Thompson <service@aiwebsystems.com>
2014-09-26 11:30:29 -0500

    Inject the addon

--- src/Streams/Platform/Addon/Tag/AddonTag.php
+++ src/Streams/Platform/Addon/Tag/AddonTag.php
@@ -1,7 +1,24 @@
 <?php namespace Streams\Platform\Addon\Tag;
 
 use Streams\Platform\Addon\TagAbstract;
+use Streams\Platform\Addon\AddonAbstract;
 
 class AddonTag extends TagAbstract
 {
+    /**
+     * The addon object.
+     *
+     * @var \Streams\Platform\Addon\AddonAbstract
+     */
+    protected $addon;
+
+    /**
+     * Create a new AddonTag instance.
+     *
+     * @param AddonAbstract $addon
+     */
+    public function __construct(AddonAbstract $addon)
+    {
+        $this->addon = $addon;
+    }
 }
