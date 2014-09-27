e0c9499114f8a7d5a13ee964234234bc1c74dfd4
Ryan Thompson <service@aiwebsystems.com>
2014-09-27 01:12:43 -0500

    Not needed

--- src/Streams/Platform/Entry/Presenter/EntryPresenter.php
+++ src/Streams/Platform/Entry/Presenter/EntryPresenter.php
@@ -6,9 +6,7 @@ class EntryPresenter extends EloquentPresenter
 {
     public function __get($key)
     {
-        if ( /*!method_exists($this, $key) and */
-        $assignment = $this->resource->findAssignmentByFieldSlug($key)
-        ) {
+        if ($assignment = $this->resource->findAssignmentByFieldSlug($key)) {
             if ($assignment) {
                 $type = $assignment->field->type
                     ->setAssignment($assignment)
