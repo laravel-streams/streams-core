9d89303b2c9079ef1def03da135e7c540ac5c2fa
Ryan Thompson <service@aiwebsystems.com>
2014-09-28 01:22:48 -0500

    Return a presenter

--- src/Streams/Platform/Addon/DistributionAbstract.php
+++ src/Streams/Platform/Addon/DistributionAbstract.php
@@ -1,5 +1,17 @@
 <?php namespace Streams\Platform\Addon;
 
+use Streams\Platform\Addon\Presenter\DistributionPresenter;
+
 abstract class DistributionAbstract extends AddonAbstract
 {
+    /**
+     * Return a new DistributionPresenter instance.
+     *
+     * @param $resource
+     * @return null|DistributionPresenter
+     */
+    public function newPresenter($resource)
+    {
+        return new DistributionPresenter($resource);
+    }
 }
