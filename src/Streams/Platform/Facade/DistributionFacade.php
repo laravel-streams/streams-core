6ae8ea6dba4a6ebca6ab851978297405793e8197
Ryan Thompson <service@aiwebsystems.com>
2014-09-28 01:15:10 -0500

    Distribution facade

---
+++ src/Streams/Platform/Facade/DistributionFacade.php
@@ -0,0 +1,16 @@
+<?php namespace Streams\Platform\Facade;
+
+use Illuminate\Support\Facades\Facade as BaseFacade;
+
+class DistributionFacade extends BaseFacade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'streams.distributions';
+    }
+}
