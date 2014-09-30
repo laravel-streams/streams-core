cf6795f0acc13e62bced33d408917ee6d54e2303
Ryan Thompson <service@aiwebsystems.com>
2014-09-30 02:25:15 -0500

    Use make

--- src/Streams/Platform/Http/Controller/AdminController.php
+++ src/Streams/Platform/Http/Controller/AdminController.php
@@ -7,7 +7,7 @@ class AdminController extends BaseController
      */
     public function login()
     {
-        return \View::make('theme::login');
+        return view('theme::login');
     }
 
     /**
@@ -17,47 +17,52 @@ class AdminController extends BaseController
      */
     public function attemptLogin()
     {
-        $messages = app()->make('streams.messages');
-        
+        $auth     = app('auth');
+        $events   = app('events');
+        $request  = app('request');
+        $redirect = app('redirect');
+
+        $messages = app('streams.messages');
+
         try {
             $credentials = [
-                'email'    => \Request::get('email'),
-                'password' => \Request::get('password'),
+                'email'    => $request->get('email'),
+                'password' => $request->get('password'),
             ];
 
             // Let an event try and handle authenticating a login.
-            if ($return = \Event::fire('user.authenticate', $credentials)) {
-                return \Redirect::intended($return);
+            if ($return = $events->fire('user.authenticate', $credentials)) {
+                return $redirect->intended($return);
             }
 
-            $user = \Sentry::authenticate($credentials, \Request::get('remember'));
+            $user = $auth->authenticate($credentials, $request->get('remember'));
 
-            \Event::fire('user.login', $user);
+            $events->fire('user.login', $user);
 
             $messages->add('info', 'Welcome, ' . $user->name . '.')->flash();
 
-            return \Redirect::intended('admin/dashboard');
+            return $redirect->intended('admin/dashboard');
         } catch (\Cartalyst\Sentry\Users\LoginRequiredException $e) {
             $messages->add('error', 'Login field is required.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         } catch (\Cartalyst\Sentry\Users\PasswordRequiredException $e) {
             $messages->add('error', 'Password field is required.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         } catch (\Cartalyst\Sentry\Users\WrongPasswordException $e) {
             $messages->add('error', 'Wrong password, try again.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         } catch (\Cartalyst\Sentry\Users\UserNotFoundException $e) {
             $messages->add('error', 'User was not found.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         } catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e) {
             $messages->add('error', 'User is not activated.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         } catch (\Cartalyst\Sentry\Throttling\UserSuspendedException $e) {
             $messages->add('error', 'User is suspended.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         } catch (\Cartalyst\Sentry\Throttling\UserBannedException $e) {
             $messages->add('error', 'User is banned.')->flash();
-            return \Redirect::to('admin/login');
+            return $redirect->to('admin/login');
         }
     }
 }
