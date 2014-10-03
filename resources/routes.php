32d72f33e376be206d13f36c44a392940b620c0c
Ryan Thompson <service@aiwebsystems.com>
2014-10-02 22:19:55 -0500

    Stash these here for now

--- resources/routes.php
+++ resources/routes.php
@@ -31,3 +31,39 @@ Route::get(
 
 // Installer loads from distribution
 app('streams.distributions')->make('base');
+
+
+
+
+
+
+
+
+
+
+
+// Go to modules by default
+Route::get(
+    'admin/addons',
+    function () {
+        return Redirect::to('admin/addons/modules');
+    }
+);
+
+crud('admin/addons/tags', 'Streams\Addon\Module\Addons\Controller\Admin\TagsController');
+crud('admin/addons/blocks', 'Streams\Addon\Module\Addons\Controller\Admin\BlocksController');
+crud('admin/addons/themes', 'Streams\Addon\Module\Addons\Controller\Admin\ThemesController');
+crud('admin/addons/modules', 'Streams\Addon\Module\Addons\Controller\Admin\ModulesController');
+crud('admin/addons/extensions', 'Streams\Addon\Module\Addons\Controller\Admin\ExtensionsController');
+crud('admin/addons/field_types', 'Streams\Addon\Module\Addons\Controller\Admin\FieldTypesController');
+
+// Install an addon
+Route::get(
+    'admin/addons/installer/install/{type}/{slug}',
+    'Streams\Addon\Module\Addons\Controller\Admin\InstallerController@install'
+);
+
+Route::get(
+    'admin/addons/installer/uninstall/{type}/{slug}',
+    'Streams\Addon\Module\Addons\Controller\Admin\InstallerController@uninstall'
+);
