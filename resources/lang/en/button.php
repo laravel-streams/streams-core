e76005398e8d2e9294d260e4505bb323f90a3b18
Ryan Thompson <service@aiwebsystems.com>
2014-10-04 03:31:30 -0500

    Moar buttons

--- resources/lang/en/button.php
+++ resources/lang/en/button.php
@@ -2,11 +2,16 @@
 
 return [
     'add'           => 'Add',
-    'cancel'        => 'Cancel',
-    'delete'        => 'Delete',
     'edit'          => 'Edit',
+    'delete'        => 'Delete',
+    'cancel'        => 'Cancel',
+    'continue'      => 'Continue',
     'save'          => 'Save',
     'save_exit'     => 'Save &amp; Exit',
     'save_create'   => 'Save &amp; Create',
     'save_continue' => 'Save &amp; Continue',
+    'install'       => 'Install',
+    'uninstall'     => 'Uninstall',
+    'login'         => 'Login',
+    'logout'        => 'Logout'
 ];
