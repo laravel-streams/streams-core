747446a71d267acaa02a529a324b0ebbfc9c4713
Ryan Thompson <service@aiwebsystems.com>
2014-10-13 00:53:24 -0500

    No coverage

--- src/Streams/Platform/Provider/AddonServiceProvider.php
+++ src/Streams/Platform/Provider/AddonServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class AddonClassServiceProvider extends ServiceProvider
+class AddonServiceProvider extends ServiceProvider
 {
     /**
      * Defer loading this service provider.
