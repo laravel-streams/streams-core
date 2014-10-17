f122666b0d4290aa0d8c01e5dbc13053ac797a24
Ryan Thompson <service@aiwebsystems.com>
2014-10-17 02:26:38 -0500

    Test the block service provider

---
+++ tests/functional/Addon/Block/BlockServiceProviderTest.php
@@ -0,0 +1,40 @@
+<?php namespace Streams\Platform\Addon\Block;
+
+class BlockServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        $provider = new \Streams\Platform\Provider\AddonServiceProvider(app());
+
+        $provider->register();
+    }
+
+    public function testItRegistersBlocksToContainer()
+    {
+        $provider = new BlockServiceProvider(app());
+
+        $provider->addLocation(__DIR__ . '/../../../tests/addons');
+
+        $provider->register();
+
+        $expected = 'Streams\Platform\Addon\Block\BlockPresenter';
+        $actual   = app('streams.block.testable');
+
+        $this->assertInstanceOf($expected, $actual);
+    }
+
+    public function testItPushesBlocksToCollection()
+    {
+        $provider = new BlockServiceProvider(app());
+
+        $provider->addLocation(__DIR__ . '/../../../tests/addons');
+
+        $provider->register();
+
+        $expected = 'Streams\Platform\Addon\Block\BlockPresenter';
+        $actual   = app('streams.blocks')->findBySlug('testable');
+
+        $this->assertInstanceOf($expected, $actual);
+    }
+}
+ 
