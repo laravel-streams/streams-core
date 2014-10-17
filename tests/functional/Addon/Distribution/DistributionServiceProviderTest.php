498c1cbcaf3bcaccfbb3c825a4292134b11393e2
Ryan Thompson <service@aiwebsystems.com>
2014-10-17 02:26:50 -0500

    Test the distribution service provider

---
+++ tests/functional/Addon/Distribution/DistributionServiceProviderTest.php
@@ -0,0 +1,40 @@
+<?php namespace Streams\Platform\Addon\Distribution;
+
+class DistributionServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public static function setUpBeforeClass()
+    {
+        $provider = new \Streams\Platform\Provider\AddonServiceProvider(app());
+
+        $provider->register();
+    }
+
+    public function testItRegistersDistributionsToContainer()
+    {
+        $provider = new DistributionServiceProvider(app());
+
+        $provider->addLocation(__DIR__ . '/../../../tests/addons');
+
+        $provider->register();
+
+        $expected = 'Streams\Platform\Addon\Distribution\DistributionPresenter';
+        $actual   = app('streams.distribution.testable');
+
+        $this->assertInstanceOf($expected, $actual);
+    }
+
+    public function testItPushesDistributionsToCollection()
+    {
+        $provider = new DistributionServiceProvider(app());
+
+        $provider->addLocation(__DIR__ . '/../../../tests/addons');
+
+        $provider->register();
+
+        $expected = 'Streams\Platform\Addon\Distribution\DistributionPresenter';
+        $actual   = app('streams.distributions')->findBySlug('testable');
+
+        $this->assertInstanceOf($expected, $actual);
+    }
+}
+ 
