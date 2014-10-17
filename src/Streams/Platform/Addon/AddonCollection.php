b38726b531869fccddb15322d6d685a12d20db15
Ryan Thompson <service@aiwebsystems.com>
2014-10-17 02:01:33 -0500

    Tested

--- src/Streams/Platform/Addon/AddonCollection.php
+++ src/Streams/Platform/Addon/AddonCollection.php
@@ -5,23 +5,12 @@ use Streams\Platform\Support\Collection;
 
 class AddonCollection extends Collection
 {
-    public function push($value)
-    {
-        parent::push(app('streams.decorator')->decorate($value));
-    }
-
-    /**
-     * Find an addon by it's slug.
-     *
-     * @param $slug
-     * @return null
-     */
     public function findBySlug($slug)
     {
         $addon = null;
 
         foreach ($this->items as $item) {
-            if ($item->getSlug() == $slug) {
+            if ($item->slug == $slug) {
                 $addon = $item;
             }
         }
