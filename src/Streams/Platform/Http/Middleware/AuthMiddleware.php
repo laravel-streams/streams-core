8beeaba687de4929fcb27c9110c71fa9eaf6e775
Ryan Thompson <service@aiwebsystems.com>
2014-10-17 04:53:14 -0500

    ldfjasf

--- src/Streams/Platform/Http/Middleware/AuthMiddleware.php
+++ src/Streams/Platform/Http/Middleware/AuthMiddleware.php
@@ -1,16 +1,12 @@
 <?php namespace Streams\Platform\Http\Middleware;
 
+use Illuminate\Contracts\Routing\Middleware;
+
 class AuthMiddleware extends Middleware
 {
-    /**
-     * Authorize the request.
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function handle(Request $request, \Closure $next)
+    public function handle($request, \Closure $next)
     {
-        $request = app('request');
-        $ignore  = array('login', 'logout');
+        $ignore = array('login', 'logout');
 
         if (!in_array($request->segment(2), $ignore) and !app('auth')->check()) {
             app('session')->put('url.intended', $request->url());
