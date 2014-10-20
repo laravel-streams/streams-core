574a8422f0b5fd44036791dca7f2e1326b23e12f
Ryan Thompson <service@aiwebsystems.com>
2014-10-19 22:38:07 -0500

    BAH.. we register the abstract

--- src/Streams/Platform/Addon/Tag/TagPresenter.php
+++ src/Streams/Platform/Addon/Tag/TagPresenter.php
@@ -5,5 +5,46 @@ use Anomaly\Lexicon\Contract\Plugin\PluginInterface;
 
 class TagPresenter extends AddonPresenter implements PluginInterface
 {
+
+    public function setPluginName($pluginName)
+    {
+        parent::setPluginName($pluginName);
+    }
+
+    public function getPluginName()
+    {
+        parent::__FUNCTIOgetPluginNameN__();
+    }
+
+    public function setContent($content)
+    {
+        parent::setContent($content);
+    }
+
+    public function setAttributes(array $attributes)
+    {
+        parent::setAttributes($attributes);
+    }
+
+    public function getAttribute($name, $offset = 0, $default = null)
+    {
+        parent::getAttribute($name, $offset, $default);
+    }
+
+    public function isFilter($key)
+    {
+        parent::isFilter($key);
+    }
+
+    public function isParse($key)
+    {
+        parent::isParse($key);
+    }
+
+    public function __call($key, array $params = [])
+    {
+        parent::__call($key, $params);
+    }
+
 }
  
