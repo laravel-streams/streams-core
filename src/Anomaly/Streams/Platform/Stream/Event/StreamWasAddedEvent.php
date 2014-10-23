96f8d414ef46d520f7d8ef71de41cd6e62f2995f
Ryan Thompson <service@aiwebsystems.com>
2014-10-22 19:26:45 -0500

    When stream was added / removed events

---
+++ src/Anomaly/Streams/Platform/Stream/Event/StreamWasAddedEvent.php
@@ -0,0 +1,22 @@
+<?php namespace Anomaly\Streams\Platform\Stream\Event;
+
+use Anomaly\Streams\Platform\Stream\StreamModel;
+
+class StreamWasAddedEvent
+{
+    protected $stream;
+
+    function __construct(StreamModel $stream)
+    {
+        $this->stream = $stream;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStream()
+    {
+        return $this->stream;
+    }
+}
+ 
