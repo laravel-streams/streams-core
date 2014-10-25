ca82eb1f5d6ec6ccdc8b9b6db721ddd5826b4624
Ryan Thompson <service@aiwebsystems.com>
2014-10-25 12:54:23 -0500

    Oop

--- src/Anomaly/Streams/Platform/StreamsServiceProvider.php
+++ src/Anomaly/Streams/Platform/StreamsServiceProvider.php
@@ -75,7 +75,6 @@ class StreamsServiceProvider extends ServiceProvider
         $this->app->register('Anomaly\Streams\Platform\Provider\LoaderServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\AssetServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\ImageServiceProvider');
-        $this->app->register('Anomaly\Streams\Platform\Provider\RouteServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\ModelServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\ViewServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\LogServiceProvider');
