7a0c0e1704949bec87e33ee9f19d814a5a8c2ea1
Ryan Thompson <service@aiwebsystems.com>
2014-10-27 14:12:16 -0500

    Allow the ID to be fetched - this can be overriden on parent classes

--- src/Anomaly/Streams/Platform/Model/EloquentPresenter.php
+++ src/Anomaly/Streams/Platform/Model/EloquentPresenter.php
@@ -2,6 +2,26 @@
 
 use Anomaly\Streams\Platform\Support\Presenter;
 
+/**
+ * Class EloquentPresenter
+ * The base presenter for all our models.
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Model
+ */
 class EloquentPresenter extends Presenter
 {
+
+    /**
+     * Return the ID.
+     *
+     * @return mixed
+     */
+    public function id()
+    {
+        return $this->resource->getKey();
+    }
+
 }
