f6ea25c925a0b4fac1e3a2c4eea37cf40324bfc1
Ryan Thompson <service@aiwebsystems.com>
2014-10-27 14:12:27 -0500

    Doc, lock and wrap.

--- src/Anomaly/Streams/Platform/Ui/Table/Command/BuildTableRowsCommandHandler.php
+++ src/Anomaly/Streams/Platform/Ui/Table/Command/BuildTableRowsCommandHandler.php
@@ -1,11 +1,28 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Table\Command;
 
 use Anomaly\Streams\Platform\Traits\CommandableTrait;
+use Anomaly\Streams\Platform\Contract\PresentableInterface;
 
+/**
+ * Class BuildTableRowsCommandHandler
+ * Build the row data for the table view.
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Ui\Table\Command
+ */
 class BuildTableRowsCommandHandler
 {
+
     use CommandableTrait;
 
+    /**
+     * Handle the command.
+     *
+     * @param BuildTableRowsCommand $command
+     * @return array
+     */
     public function handle(BuildTableRowsCommand $command)
     {
         $rows = [];
@@ -14,8 +31,10 @@ class BuildTableRowsCommandHandler
 
         foreach ($ui->getEntries() as $entry) {
 
-            $columns = $this->makeColumns($entry, $ui);
-            $buttons = $this->makeButtons($entry, $ui);
+            // Build out our required data.
+            $columns = $this->getColumns($entry, $ui);
+            $buttons = $this->getButtons($entry, $ui);
+            $entry   = $this->getDecoratedEntry($entry);
 
             $rows[] = compact('columns', 'buttons', 'entry');
 
@@ -24,18 +43,52 @@ class BuildTableRowsCommandHandler
         return $rows;
     }
 
-    protected function makeColumns($entry, $ui)
+    /**
+     * Get our column data for each row.
+     *
+     * @param $entry
+     * @param $ui
+     * @return mixed
+     */
+    protected function getColumns($entry, $ui)
     {
         $command = new BuildTableColumnsCommand($ui, $entry);
 
         return $this->execute($command);
     }
 
-    private function makeButtons($entry, $ui)
+    /**
+     * Get our button data for each row.
+     *
+     * @param $entry
+     * @param $ui
+     * @return mixed
+     */
+    protected function getButtons($entry, $ui)
     {
         $command = new BuildTableButtonsCommand($ui, $entry);
 
         return $this->execute($command);
     }
+
+    /**
+     * Decorate the entry before sending it to the view.
+     * Typically this would be auto-decorated but
+     * it is buried a little in this case.
+     *
+     * @param $entry
+     * @return mixed
+     */
+    protected function getDecoratedEntry($entry)
+    {
+        if ($entry instanceof PresentableInterface) {
+
+            $entry = $entry->decorate();
+
+        }
+
+        return $entry;
+    }
+
 }
  
