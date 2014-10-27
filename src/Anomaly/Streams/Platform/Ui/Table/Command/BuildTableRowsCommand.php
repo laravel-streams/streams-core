f9f8b44504c16ebad76c48ae9c4404bbf3f8021c
Ryan Thompson <service@aiwebsystems.com>
2014-10-27 14:13:33 -0500

    Doc, lock and wrap.

--- src/Anomaly/Streams/Platform/Ui/Table/Command/BuildTableRowsCommand.php
+++ src/Anomaly/Streams/Platform/Ui/Table/Command/BuildTableRowsCommand.php
@@ -2,21 +2,44 @@
 
 use Anomaly\Streams\Platform\Ui\Table\TableUi;
 
+/**
+ * Class BuildTableRowsCommand
+ * DTO for creating table row configuration.
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Ui\Table\Command
+ */
 class BuildTableRowsCommand
 {
+
+    /**
+     * The table UI object.
+     *
+     * @var \Anomaly\Streams\Platform\Ui\Table\TableUi
+     */
     protected $ui;
 
+    /**
+     * Create a new BuildTableRowsCommand instance.
+     *
+     * @param TableUi $ui
+     */
     function __construct(TableUi $ui)
     {
         $this->ui = $ui;
     }
 
     /**
+     * Get the table UI object.
+     *
      * @return mixed
      */
     public function getUi()
     {
         return $this->ui;
     }
+
 }
  
