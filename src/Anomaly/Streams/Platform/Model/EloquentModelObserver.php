e90c8a518bfcfc052b50bc2d562722cbac80acb1
Ryan Thompson <service@aiwebsystems.com>
2014-10-31 04:15:07 -0500

    Observer improvements

--- src/Anomaly/Streams/Platform/Model/EloquentModelObserver.php
+++ src/Anomaly/Streams/Platform/Model/EloquentModelObserver.php
@@ -3,6 +3,14 @@
 use Anomaly\Streams\Platform\Traits\DispatchableTrait;
 use Anomaly\Streams\Platform\Traits\EventableTrait;
 
+/**
+ * Class EloquentModelObserver
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Model
+ */
 class EloquentModelObserver
 {
 
---
+++ src/Anomaly/Streams/Platform/Support/Observer.php
@@ -0,0 +1,18 @@
+<?php namespace Anomaly\Streams\Platform\Support;
+
+use Anomaly\Streams\Platform\Traits\DispatchableTrait;
+
+/**
+ * Class Observer
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Support
+ */
+class Observer
+{
+
+    use DispatchableTrait;
+}
+ 
