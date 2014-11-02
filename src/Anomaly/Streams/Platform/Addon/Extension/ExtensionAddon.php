4616e8cf1bb83a1cee8dfa2aa4a6993d43fb6ae6
Ryan Thompson <service@aiwebsystems.com>
2014-11-02 10:30:55 -0600

    Implement a handler pattern for extensions

--- src/Anomaly/Streams/Platform/Addon/Extension/ExtensionAddon.php
+++ src/Anomaly/Streams/Platform/Addon/Extension/ExtensionAddon.php
@@ -3,9 +3,26 @@
 use Anomaly\Streams\Platform\Addon\Addon;
 use Anomaly\Streams\Platform\Contract\PresentableInterface;
 
+/**
+ * Class ExtensionAddon
+ *
+ * This is the default authenticator for the Users module.
+ * Extensions generally return a handler to do their business
+ * through transformer methods but can utilize any pattern.
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Addon\Extension
+ */
 class ExtensionAddon extends Addon implements PresentableInterface
 {
 
+    /**
+     * Return a decorated addon.
+     *
+     * @return ExtensionPresenter
+     */
     public function decorate()
     {
         return new ExtensionPresenter($this);
