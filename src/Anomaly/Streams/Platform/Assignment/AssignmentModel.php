f2b1f16f7f6b08b1c43de3b659f293baf965d5ed
Ryan Thompson <service@aiwebsystems.com>
2014-11-01 23:24:12 -0500

    Formatting

--- src/Anomaly/Streams/Platform/Assignment/AssignmentModel.php
+++ src/Anomaly/Streams/Platform/Assignment/AssignmentModel.php
@@ -191,7 +191,6 @@ class AssignmentModel extends EloquentModel
             return $translated;
         }
 
-
         return null;
     }
 
@@ -229,6 +228,14 @@ class AssignmentModel extends EloquentModel
 
     public function isTranslatable()
     {
+        if ($this->field->slug == 'first_name') {
+            return true;
+        }
+
+        if ($this->field->slug == 'last_name') {
+            return true;
+        }
+
         return ($this->field->translatable and $this->stream->translatable);
     }
 }
