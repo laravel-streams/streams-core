59682e50478c04b2e91bdcea0fb47558356b6ded
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 01:45:55 -0600

    Renamed

--- src/Anomaly/Streams/Platform/Addon/Theme/Theme.php
+++ src/Anomaly/Streams/Platform/Addon/Theme/Theme.php
@@ -3,7 +3,7 @@
 use Anomaly\Streams\Platform\Addon\Addon;
 use Anomaly\Streams\Platform\Contract\PresentableInterface;
 
-class ThemeAddon extends Addon implements PresentableInterface
+class Theme extends Addon implements PresentableInterface
 {
 
     protected $type = 'theme';
