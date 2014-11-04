7522f0c11987d6dee89565568a9b662756666816
Ryan Thompson <service@aiwebsystems.com>
2014-11-04 06:03:27 -0600

    Messages can accept arrays now

--- src/Anomaly/Streams/Platform/Support/Messages.php
+++ src/Anomaly/Streams/Platform/Support/Messages.php
@@ -3,6 +3,14 @@
 use Illuminate\Session\Store;
 use Illuminate\Support\MessageBag;
 
+/**
+ * Class Messages
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Support
+ */
 class Messages extends MessageBag
 {
 
@@ -19,6 +27,28 @@ class Messages extends MessageBag
      * @var Store
      */
     protected $session;
+
+    /**
+     * Add one or more messages to a key.
+     *
+     * @param string       $key
+     * @param array|string $message
+     * @return $this
+     */
+    public function add($key, $message)
+    {
+        if (is_array($message) and $messages = $message) {
+
+            foreach ($messages as $message) {
+
+                parent::add($key, $message);
+            }
+
+            return $this;
+        }
+
+        return parent::add($key, $message);
+    }
 
     /**
      * Flash the messages for a redirect.
