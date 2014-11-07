1ea22114049ea7f42df13215780f11690e8e3e37
Ryan Thompson <service@aiwebsystems.com>
2014-11-07 17:03:44 -0600

    Merge in validation rules from field types

--- src/Anomaly/Streams/Platform/Ui/Form/Command/BuildSubmissionValidationRulesCommandHandler.php
+++ src/Anomaly/Streams/Platform/Ui/Form/Command/BuildSubmissionValidationRulesCommandHandler.php
@@ -20,13 +20,35 @@ class BuildSubmissionValidationRulesCommandHandler
     {
         $form = $command->getForm();
 
-        $model = $form->getModel();
+        $model  = $form->getModel();
+        $stream = $form->getStream();
 
         $rules = $model::$rules;
 
+        /**
+         * Remove skips from validation entirely.
+         */
         foreach ($form->getSkips() as $field) {
 
             unset($rules[$field]);
+        }
+
+        /**
+         * Merge in field type rules.
+         */
+        foreach ($stream->assignments as $assignment) {
+
+            if (isset($rules[$assignment->field->slug]) and $type = $assignment->type()) {
+
+
+                $rules[$assignment->field->slug] = implode(
+                    '|',
+                    array_merge(
+                        explode('|', $rules[$assignment->field->slug]),
+                        $type->getRules()
+                    )
+                );
+            }
         }
 
         $form->setRules($rules);
