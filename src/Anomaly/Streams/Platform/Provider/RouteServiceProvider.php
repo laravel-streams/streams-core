f5c2af2806ba49138f04ae601844c5e9c648aacb
Ryan Thompson <service@aiwebsystems.com>
2014-11-14 01:32:13 -0600

    Sexy little helper

--- src/Anomaly/Streams/Platform/Provider/RouteServiceProvider.php
+++ src/Anomaly/Streams/Platform/Provider/RouteServiceProvider.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Routing\Router;
 
+/**
+ * Class RouteServiceProvider
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Provider
+ */
 class RouteServiceProvider extends \Illuminate\Foundation\Support\Providers\RouteServiceProvider
 {
 
@@ -12,9 +20,7 @@ class RouteServiceProvider extends \Illuminate\Foundation\Support\Providers\Rout
      *
      * @var array
      */
-    protected $middleware = [
-        'auth' => 'App\Http\Middleware\Authenticated',
-    ];
+    protected $middleware = [];
 
     /**
      * Called before routes are registered.
@@ -37,6 +43,64 @@ class RouteServiceProvider extends \Illuminate\Foundation\Support\Providers\Rout
     public function map(Router $router)
     {
         //
+    }
+
+    /**
+     * Route a bunch of routes.
+     *
+     * @param Router $router
+     * @param array  $routes
+     */
+    protected function route(Router $router, array $routes)
+    {
+        $prefix = $this->guessControllerNamespace();
+
+        foreach ($routes as $route => $action) {
+
+            if (is_string($action) and !starts_with($action, 'Anomaly')) {
+
+                $action = $prefix . $action;
+            }
+
+            if (starts_with($route, 'any::')) {
+
+                $router->any(substr($route, 5), $action);
+            }
+
+            if (starts_with($route, 'get::')) {
+
+                $router->get(substr($route, 5), $action);
+            }
+
+            if (starts_with($route, 'post::')) {
+
+                $router->post(substr($route, 6), $action);
+            }
+        }
+    }
+
+    /**
+     * Guess the base namespace.
+     *
+     * @return string
+     */
+    protected function guessBaseNamespace()
+    {
+        $namespace = explode('\\', get_class($this));
+
+        return implode('\\', array_slice($namespace, 0, -2));
+    }
+
+    /**
+     * Guess the controller namespace.
+     *
+     * @return string
+     */
+    private function guessControllerNamespace()
+    {
+        $base = $this->guessBaseNamespace();
+
+        return $base . '\Http\Controller\\';
     }
 }
  
