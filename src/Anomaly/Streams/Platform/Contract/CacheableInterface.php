00155e269fc804606b19ef6c3a4b49a37cf22e36
Ryan Thompson <service@aiwebsystems.com>
2014-11-19 23:55:13 -0600

    Clean up contracts a bit

---
+++ src/Anomaly/Streams/Platform/Contract/CacheableInterface.php
@@ -0,0 +1,28 @@
+<?php namespace Anomaly\Streams\Platform\Contract;
+
+/**
+ * Interface CacheableInterface
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Contract
+ */
+interface CacheableInterface
+{
+
+    /**
+     * Get the cache collection key.
+     *
+     * @return mixed
+     */
+    public function getCacheCollectionKey();
+
+    /**
+     * Get the minutes to cache for.
+     *
+     * @return mixed
+     */
+    public function getCacheMinutes();
+}
+ 
