7e25e9e89ddd756c1615fb55307728ce4935f404
Ryan Thompson <service@aiwebsystems.com>
2014-11-20 01:54:57 -0600

    Clean up events and add some nice globals to help modify the table from outside if desired.

--- src/Anomaly/Streams/Platform/Ui/Table/Event/BootedEvent.php
+++ src/Anomaly/Streams/Platform/Ui/Table/Event/BootedEvent.php
@@ -3,14 +3,14 @@
 use Anomaly\Streams\Platform\Ui\Table\Table;
 
 /**
- * Class AbstractTableEvent
+ * Class BootedEvent
  *
  * @link          http://anomaly.is/streams-platform
  * @author        AnomalyLabs, Inc. <hello@anomaly.is>
  * @author        Ryan Thompson <ryan@anomaly.is>
  * @package       Anomaly\Streams\Platform\Ui\Table\Event
  */
-abstract class AbstractTableEvent
+class BootedEvent
 {
 
     /**
---
+++ src/Anomaly/Streams/Platform/Ui/Table/Event/MakingEvent.php
@@ -0,0 +1,43 @@
+<?php namespace Anomaly\Streams\Platform\Ui\Table\Event;
+
+use Anomaly\Streams\Platform\Ui\Table\Table;
+
+/**
+ * Class MakingEvent
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Ui\Table\Event
+ */
+class MakingEvent
+{
+
+    /**
+     * The table object.
+     *
+     * @var
+     */
+    protected $table;
+
+    /**
+     * Create a new table event instance.
+     *
+     * @param Table $table
+     */
+    function __construct(Table $table)
+    {
+        $this->table = $table;
+    }
+
+    /**
+     * Get the table object.
+     *
+     * @return mixed
+     */
+    public function getTable()
+    {
+        return $this->table;
+    }
+}
+ 
