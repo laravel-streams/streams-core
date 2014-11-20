5a37930ac44283d0366306e1421d151bae65d985
Ryan Thompson <service@aiwebsystems.com>
2014-11-20 00:09:37 -0600

    Doc

--- src/Anomaly/Streams/Platform/StreamsServiceProvider.php
+++ src/Anomaly/Streams/Platform/StreamsServiceProvider.php
@@ -3,6 +3,14 @@
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Class StreamsServiceProvider
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform
+ */
 class StreamsServiceProvider extends ServiceProvider
 {
 
