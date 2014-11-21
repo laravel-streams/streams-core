b0d5daa842dbd988a94dedeaaa8d19beb243d687
Ryan Thompson <service@aiwebsystems.com>
2014-11-21 04:12:02 -0600

    Available translations / locales

--- src/Anomaly/Streams/Platform/Ui/Form/Command/BuildFormOptionsCommandHandler.php
+++ src/Anomaly/Streams/Platform/Ui/Form/Command/BuildFormOptionsCommandHandler.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Form\Command;
 
 use Anomaly\Streams\Platform\Stream\Contract\StreamInterface;
+use Anomaly\Streams\Platform\Ui\Form\Form;
 
 /**
  * Class BuildFormOptionsCommandHandler
@@ -23,16 +24,48 @@ class BuildFormOptionsCommandHandler
     {
         $form = $command->getForm();
 
-        $stream = $form->getStream();
+        $locales      = $this->getLocales($form);
+        $translatable = $this->getTranslatable($form);
 
-        $translatable = false;
+        return compact('translatable', 'locales');
+    }
+
+    /**
+     * Get available locales.
+     *
+     * @param Form $form
+     * @return array
+     */
+    protected function getLocales(Form $form)
+    {
+        $locales = [];
+
+        foreach (config('streams.available_locales') as $locale) {
+
+            $language = trans('language.' . $locale);
+
+            $locales[$locale] = compact('locale', 'language');
+        }
+
+        return $locales;
+    }
+
+    /**
+     * Get the translatable flag.
+     *
+     * @param Form $form
+     * @return bool
+     */
+    protected function getTranslatable(Form $form)
+    {
+        $stream = $form->getStream();
 
         if ($stream instanceof StreamInterface) {
 
-            $translatable = $stream->isTranslatable();
+            return $stream->isTranslatable();
         }
 
-        return compact('translatable');
+        return false;
     }
 }
  
