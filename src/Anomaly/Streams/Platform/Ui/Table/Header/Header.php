6378b920a83cd2c6a74e2c2ed86528da8aae6a0f
Ryan Thompson <service@aiwebsystems.com>
2014-12-04 13:25:36 -0600

    Tet

--- src/Anomaly/Streams/Platform/Ui/Table/Header/Header.php
+++ src/Anomaly/Streams/Platform/Ui/Table/Header/Header.php
@@ -1,5 +1,6 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Table\Header;
 
+use Anomaly\Streams\Platform\Stream\Contract\StreamInterface;
 use Anomaly\Streams\Platform\Ui\Table\Header\Contract\HeaderInterface;
 
 class Header implements HeaderInterface
@@ -9,17 +10,49 @@ class Header implements HeaderInterface
 
     protected $prefix;
 
-    function __construct($text = null, $prefix = null)
+    protected $stream;
+
+    function __construct($text = null, $prefix = null, StreamInterface $stream = null)
     {
         $this->text   = $text;
         $this->prefix = $prefix;
+        $this->stream = $stream;
     }
 
     public function viewData()
     {
-        $text = 'Header!';
+        $text = $this->getText();
+
+        if ($this->stream and is_string($text)) {
+
+            $text = $this->getTextFromField($text);
+        }
+
+        $text = trans($text);
 
         return compact('text');
+    }
+
+    protected function getTextFromField($text)
+    {
+        if ($field = $this->stream->getField($text)) {
+
+            return $field->getName();
+        }
+
+        return $text;
+    }
+
+    public function setStream(StreamInterface $stream = null)
+    {
+        $this->stream = $stream;
+
+        return $this;
+    }
+
+    public function getStream()
+    {
+        return $this->stream;
     }
 
     public function setPrefix($prefix)
