923aa4670be5ef01232eb305d81a2ca4484e9824
Ryan Thompson <service@aiwebsystems.com>
2014-12-04 13:26:48 -0600

    Get stream

--- src/Anomaly/Streams/Platform/Ui/Table/Header/Contract/HeaderInterface.php
+++ src/Anomaly/Streams/Platform/Ui/Table/Header/Contract/HeaderInterface.php
@@ -1,9 +1,15 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Table\Header\Contract;
 
+use Anomaly\Streams\Platform\Stream\Contract\StreamInterface;
+
 interface HeaderInterface
 {
 
     public function viewData();
+
+    public function setStream(StreamInterface $stream = null);
+
+    public function getStream();
 
     public function setPrefix($prefix);
 
