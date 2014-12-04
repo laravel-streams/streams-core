e66e10da5086dd67f2d799a9df3f5ab1f332ce3a
Ryan Thompson <service@aiwebsystems.com>
2014-12-04 02:17:59 -0600

    Table model interface

---
+++ src/Anomaly/Streams/Platform/Ui/Table/Contract/TableModelInterface.php
@@ -0,0 +1,10 @@
+<?php namespace Anomaly\Streams\Platform\Ui\Table\Contract;
+
+use Anomaly\Streams\Platform\Ui\Table\Table;
+
+interface TableModelInterface
+{
+
+    public function getTableEntries(Table $table);
+}
+ 
