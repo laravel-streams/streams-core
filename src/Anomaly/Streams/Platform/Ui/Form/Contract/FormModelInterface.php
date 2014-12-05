8a76808efd5d193cef741d7f85e4897e242fe458
Ryan Thompson <service@aiwebsystems.com>
2014-12-04 23:23:26 -0600

    Form interface

---
+++ src/Anomaly/Streams/Platform/Ui/Form/Contract/FormModelInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Anomaly\Streams\Platform\Ui\Form\Contract;
+
+use Anomaly\Streams\Platform\Ui\Form\Form;
+
+interface FormModelInterface
+{
+
+    public static function findOrNew($id);
+
+    public static function saveFormInput(Form $form);
+}
+ 
