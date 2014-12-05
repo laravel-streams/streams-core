8d2f580e42164082a1194f52205ea1a30b84153e
Ryan Thompson <service@aiwebsystems.com>
2014-12-04 23:40:56 -0600

    Layout columns

---
+++ src/Anomaly/Streams/Platform/Ui/Form/Column/Column.php
@@ -0,0 +1,13 @@
+<?php namespace Anomaly\Streams\Platform\Ui\Form\Column;
+
+use Anomaly\Streams\Platform\Ui\Form\Column\Contract\ColumnInterface;
+
+class Column implements ColumnInterface
+{
+
+    public function viewData()
+    {
+        // TODO: Implement viewData() method.
+    }
+}
+ 
---
+++ src/Anomaly/Streams/Platform/Ui/Form/Column/Contract/ColumnInterface.php
@@ -0,0 +1,8 @@
+<?php namespace Anomaly\Streams\Platform\Ui\Form\Column\Contract;
+
+interface ColumnInterface
+{
+
+    public function viewData();
+}
+ 
