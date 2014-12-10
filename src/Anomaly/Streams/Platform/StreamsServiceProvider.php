9a925b802afaf986f9521c27f1e78377ef72ab51
Ryan Thompson <service@aiwebsystems.com>
2014-12-10 02:15:55 -0600

    Garbage

--- src/Anomaly/Streams/Platform/StreamsServiceProvider.php
+++ src/Anomaly/Streams/Platform/StreamsServiceProvider.php
@@ -86,7 +86,6 @@ class StreamsServiceProvider extends ServiceProvider
         $this->app->register('Anomaly\Streams\Platform\Provider\AssetServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\ImageServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\ModelServiceProvider');
-        $this->app->register('Anomaly\Streams\Platform\Provider\CacheServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\ViewServiceProvider');
         $this->app->register('Anomaly\Streams\Platform\Provider\LogServiceProvider');
 
