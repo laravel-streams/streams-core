5cdeb9ee8737d9cd88489d6b3b81bfe8c74a6b4e
Ryan Thompson <service@aiwebsystems.com>
2014-12-11 05:31:40 -0600

    Too many nests!

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -1,18 +1,15 @@
 <?php
 
 return [
-    [
-        'no_results' => 'No results.',
-        'success'    => [
-            'generic' => 'Success!',
-        ],
-        'error'      => [
-            'generic'        => 'There was an error.',
-            'no_results'     => 'No results.',
-            'not_authorized' => 'Authorization failed.',
-        ],
-        'confirm'    => [
-            'delete' => 'Are you sure you want to delete this?',
-        ],
-    ]
+    'success' => [
+        'generic' => 'Success!',
+    ],
+    'error'   => [
+        'generic'        => 'There was an error.',
+        'no_results'     => 'No results.',
+        'not_authorized' => 'Authorization failed.',
+    ],
+    'confirm' => [
+        'delete' => 'Are you sure you want to delete this?',
+    ],
 ];
