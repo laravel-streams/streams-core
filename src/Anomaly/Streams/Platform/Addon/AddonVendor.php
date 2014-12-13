5250a7b45fa2531941ef74f354e84d453e0e164b
Ryan Thompson <service@aiwebsystems.com>
2014-12-13 14:09:39 -0600

    This class is responsible for autoloading the vendor directory for addons

---
+++ src/Anomaly/Streams/Platform/Addon/AddonVendor.php
@@ -0,0 +1,42 @@
+<?php namespace Anomaly\Streams\Platform\Addon;
+
+use Composer\Autoload\ClassLoader;
+
+class AddonVendor extends ClassLoader
+{
+    public function load($path)
+    {
+        $vendorPath = $path . '/vendor/';
+        $vendorFile = 'autoload.php';
+
+        if (is_file($vendorPath . $vendorFile)) {
+
+            $autoload = require_once $vendorPath . $vendorFile;
+
+            if (!empty($autoload['psr-0'])) {
+                foreach ($autoload['psr-0'] as $namespace => $path) {
+                    $this->add($namespace, $this->vendorPaths($vendorPath, $path));
+                }
+            }
+
+            if (!empty($autoload['psr-4'])) {
+                foreach ($autoload['psr-4'] as $namespace => $path) {
+                    $this->addPsr4($namespace, $this->vendorPaths($vendorPath, $path));
+                }
+            }
+        }
+    }
+
+    protected function vendorPaths($vendorPath, $path)
+    {
+        if (is_array($path)) {
+            foreach ($path as &$p) {
+                $p = $vendorPath . $p;
+            }
+        } else {
+            $path = $vendorPath . $path;
+        }
+
+        return $path;
+    }
+}
