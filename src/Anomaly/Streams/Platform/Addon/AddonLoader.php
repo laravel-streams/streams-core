6be4ab30eb07d626678eb93be706538c473f8824
Ryan Thompson <service@aiwebsystems.com>
2014-12-13 14:10:12 -0600

    This class is responsible for autolaoding the addons

---
+++ src/Anomaly/Streams/Platform/Addon/AddonLoader.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\Streams\Platform\Addon;
+
+use Composer\Autoload\ClassLoader;
+
+class AddonLoader extends ClassLoader
+{
+    public function load($type, $slug, $path)
+    {
+        $namespace = 'Anomaly\Streams\Addon\\' . studly_case($type) . '\\' . studly_case($slug) . '\\';
+
+        parent::addPsr4($namespace, $path . '/src', $prepend = false);
+
+        parent::register();
+    }
+}
+ 
