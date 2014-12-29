465a8a800febc2b49c613a6584589294e3ddd422
Ryan THompson <service@aiwebsystems.com>
2014-12-29 01:56:44 -0600

    Improve interface

--- src/Ui/Table/Contract/TableModelInterface.php
+++ src/Ui/Table/Contract/TableModelInterface.php
@@ -1,14 +1,14 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Table\Contract;
 
-use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
+use Anomaly\Streams\Platform\Ui\Table\Table;
 
 /**
  * Interface TableModelInterface
  *
- * @link    http://anomaly.is/streams-platform
- * @author  AnomalyLabs, Inc. <hello@anomaly.is>
- * @author  Ryan Thompson <ryan@anomaly.is>
- * @package Anomaly\Streams\Platform\Ui\Table\Contract
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Ui\Table\Contract
  */
 interface TableModelInterface
 {
@@ -16,8 +16,8 @@ interface TableModelInterface
     /**
      * Get the table entries.
      *
-     * @param  TableBuilder $builder
+     * @param Table $table
      * @return mixed
      */
-    public function getTableEntries(TableBuilder $builder);
+    public function getTableEntries(Table $table);
 }
