27877916778fe6b585672b310400f4434fe40710
Ryan Thompson <service@aiwebsystems.com>
2015-01-01 16:01:51 -0600

    Unnecessary shit

--- resources/js/table/actions.js
+++ resources/js/table/actions.js
@@ -5,24 +5,4 @@ $(document).ready(function () {
 
         $(this).closest('table').find(':checkbox').prop('checked', $(this).is(':checked'));
     });
-
-    // Disable / enable actions when checkboxs are checked
-    $('input[type="checkbox"]').on('change', function () {
-
-        if ($('input[type="checkbox"]:checked').length > 0) {
-
-            $('.actions .btn').removeAttr('disabled');
-        } else {
-
-            $('.actions .btn').attr('disabled', 'disabled');
-        }
-    });
-
-    // When an action is clicked - do something
-    $('[data-action]').on('click', function (e) {
-
-        e.preventDefault();
-
-        $(this).closest('form').attr('action', $(this).data('action')).submit();
-    });
 });
