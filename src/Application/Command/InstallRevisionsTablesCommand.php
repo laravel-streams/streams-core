2529604060efc660d65c9f59dab18ae2ab941e42
Ryan Thompson <service@aiwebsystems.com>
2015-01-01 23:43:28 -0600

    Install revisions

---
+++ src/Application/Command/InstallRevisionsTablesCommand.php
@@ -0,0 +1,20 @@
+<?php namespace Anomaly\Streams\Platform\Application\Command;
+
+/**
+ * Class InstallRevisionsTablesCommand
+ *
+ * @link    http://anomaly.is/streams-platform
+ * @author  AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author  Ryan Thompson <ryan@anomaly.is>
+ * @package Anomaly\Streams\Platform\Application\Command
+ */
+class InstallRevisionsTablesCommand
+{
+
+    /**
+     * Create a new InstallRevisionsTablesCommand instance.
+     */
+    public function __construct()
+    {
+    }
+}
