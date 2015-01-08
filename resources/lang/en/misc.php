cf7355b9dc58401224f2f834686809c01c7d1593
Ryan Thompson <service@aiwebsystems.com>
2015-01-08 00:28:35 -0600

    Meh

--- resources/lang/en/misc.php
+++ resources/lang/en/misc.php
@@ -9,6 +9,7 @@ return [
     'logout'            => 'Logout',
     'newest'            => 'Newest',
     'untitled'          => 'Untitled',
+    'permissions'       => 'Permissions',
     'recently_created'  => 'Recently Created',
     'recently_modified' => 'Recently Modified',
 ];
