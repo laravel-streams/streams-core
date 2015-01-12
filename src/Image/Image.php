19ce65b5299bb8b5447f54f93fe24e612d0f0b6e
Ryan Thompson <service@aiwebsystems.com>
2015-01-12 07:04:18 -0600

    This is shady.. remove it for now.

--- src/Image/Image.php
+++ src/Image/Image.php
@@ -104,17 +104,6 @@ class Image extends ImageManager
     }
 
     /**
-     * An alias to create a new image instance.
-     *
-     * @param string $image
-     * @return $this
-     */
-    public function make($image)
-    {
-        return $this->setImage($image);
-    }
-
-    /**
      * Publish an image to the publish directory.
      *
      * @param $path
