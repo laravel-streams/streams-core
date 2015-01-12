923c78deaa5876c28e0751455e9622ba628be1bd
Ryan Thompson <service@aiwebsystems.com>
2015-01-12 06:59:41 -0600

    Misc

--- resources/lang/en/misc.php
+++ resources/lang/en/misc.php
@@ -13,4 +13,11 @@ return [
     'permissions'       => 'Permissions',
     'recently_created'  => 'Recently Created',
     'recently_modified' => 'Recently Modified',
+    'block'             => 'Block',
+    'theme'             => 'Theme',
+    'module'            => 'Module',
+    'plugin'            => 'Plugin',
+    'extension'         => 'Extension',
+    'field_type'        => 'Field Type',
+    'distribution'      => 'Distribution',
 ];
