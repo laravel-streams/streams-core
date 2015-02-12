4a104c5e69425c45c9b899223e9afe61c85d77c5
Ryan Thompson <service@aiwebsystems.com>
2015-02-12 14:45:23 -0600

    Small tweaks

---
+++ resources/lang/en/message.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'no_results'      => 'No results.',
+    'error'           => 'Error!',
+    'success'         => 'Success!',
+    'error_message'   => 'Error! :message',
+    'success_message' => 'Success! :message'
+];
