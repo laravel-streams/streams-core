a3db4ad686b162b2d7227f50b5e00bb96a23e18e
Ryan Thompson <service@aiwebsystems.com>
2015-02-21 21:55:13 -0600

    Nice

--- resources/lang/en/setting.php
+++ resources/lang/en/setting.php
@@ -1,50 +1,51 @@
 <?php
 
 return [
-    'name'                => [
+    'name'           => [
         'label'        => 'Site Name',
         'instructions' => 'What is the name of the website or application?',
         'placeholder'  => trans('distribution::addon.name')
     ],
-    'description'         => [
+    'description'    => [
         'label'        => 'Site Description',
         'instructions' => 'What is the description or slogan of the website or application?',
         'placeholder'  => trans('distribution::addon.description')
     ],
-    'contact_email'       => [
+    'contact_email'  => [
         'label'        => 'Contact Email',
         'instructions' => 'All e-mails from users, guests and the website or application will go to this e-mail address by default.',
         'placeholder'  => 'example@domain.com'
     ],
-    'server_email'        => [
+    'server_email'   => [
         'label'        => 'Server Email',
         'instructions' => 'All emails sent from your server will come from this email address.',
         'placeholder'  => 'noreply@domain.com'
     ],
-    'date_format'         => [
+    'date_format'    => [
         'label'        => 'Date Format',
         'instructions' => 'How should dates be displayed across the website and control panel? Using the <a target="_blank" href="http://php.net/manual/en/function.date.php">date format</a> from PHP.',
         'placeholder'  => 'm/d/Y'
     ],
-    'default_locale'      => [
+    'default_locale' => [
         'label'        => 'Default Language',
         'instructions' => 'What is the default language for your website or application?',
         'placeholder'  => config('app.locale')
     ],
-    'site_enabled'        => [
+    'site_enabled'   => [
         'label'        => 'Site Status',
         'instructions' => 'Use this option to the enable or disable the public-facing part of the website or application.<br>This is useful when you want to take the website or application down for maintenance or development.'
     ],
-    'ip_whitelist'        => [
+    'ip_whitelist'   => [
         'label'        => 'IP Whitelist',
         'instructions' => 'When the status is set to "disabled" these IP addresses will be allowed to access the website or application.',
         'placeholder'  => 'Separate each IP address with a comma.'
     ],
-    '503_message' => [
+    '503_message'    => [
         'label'        => 'Unavailable Message',
-        'instructions' => 'When the site is disabled or there is a major problem, this message will display to users.'
+        'instructions' => 'When the site is disabled or there is a major problem, this message will display to users.',
+        'placeholder'  => 'Be right back.'
     ],
-    'force_https'         => [
+    'force_https'    => [
         'label'        => 'Force HTTPS',
         'instructions' => 'Allow only the HTTPS protocol when accessing the website or application?',
         'option'       => [
