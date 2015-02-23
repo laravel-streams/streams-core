4993a83c6f9d44a2825a8f229202993f2734c43c
Osvaldo Brignoni <obrignoni@gmail.com>
2015-02-23 11:05:32 -0600

    Migration stubs, to use our own migration class

---
+++ src/Database/Migration/Migration.php
@@ -0,0 +1,18 @@
+<?php namespace Anomaly\Streams\Platform\Database\Migration;
+
+use Illuminate\Database\Migrations\Migration as BaseMigration;
+
+/**
+ * Class Migration
+ *
+ * @package Anomaly\Streams\Platform\Database\Migration
+ */
+abstract class Migration extends BaseMigration
+{
+
+    public function __construct()
+    {
+
+    }
+
+}
