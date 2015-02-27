9c166eb7bfb57a06c717bceb947cca9ea9247df3
Ryan Thompson <service@aiwebsystems.com>
2015-02-27 02:48:42 -0600

    Progress

---
+++ src/Addon/Extension/ExtensionManager.php
@@ -0,0 +1,40 @@
+<?php namespace Anomaly\Streams\Platform\Addon\Extension;
+
+use Anomaly\Streams\Platform\Addon\Extension\Command\InstallExtension;
+use Anomaly\Streams\Platform\Addon\Extension\Command\UninstallExtension;
+use Illuminate\Foundation\Bus\DispatchesCommands;
+
+/**
+ * Class ExtensionManager
+ *
+ * @link    http://anomaly.is/streams-platform
+ * @author  AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author  Ryan Thompson <ryan@anomaly.is>
+ * @package Anomaly\Streams\Platform\Addon\Extension
+ */
+class ExtensionManager
+{
+
+    use DispatchesCommands;
+
+    /**
+     * Install a module.
+     *
+     * @param Extension $module
+     * @param bool      $seed
+     */
+    public function install(Extension $module, $seed = false)
+    {
+        $this->dispatch(new InstallExtension($module, $seed));
+    }
+
+    /**
+     * Uninstall a module.
+     *
+     * @param Extension $module
+     */
+    public function uninstall(Extension $module)
+    {
+        $this->dispatch(new UninstallExtension($module));
+    }
+}
