ff3171cca3d594dc8a7992a78bbf3fd3ab37f818
Ryan Thompson <service@aiwebsystems.com>
2015-03-03 14:15:19 -0600

    Improvements to the image utility

---
+++ src/Image/ImagePluginFunctions.php
@@ -0,0 +1,74 @@
+<?php namespace Anomaly\Streams\Platform\Image;
+
+/**
+ * Class ImagePluginFunctions
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Image
+ */
+class ImagePluginFunctions
+{
+
+    /**
+     * The image utility.
+     *
+     * @var Image
+     */
+    protected $image;
+
+    /**
+     * Create a new ImagePluginFunctions instance.
+     *
+     * @param Image $image
+     */
+    public function __construct(Image $image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Return the image instance with URL output.
+     *
+     * @param $image
+     * @return $this
+     */
+    public function url($image)
+    {
+        return $this->image->make($image)->setOutput('url');
+    }
+
+    /**
+     * Return the image instance with path output.
+     *
+     * @param $image
+     * @return $this
+     */
+    public function path($image)
+    {
+        return $this->image->make($image)->setOutput('path');
+    }
+
+    /**
+     * Return the image instance with img output.
+     *
+     * @param $image
+     * @return $this
+     */
+    public function tag($image)
+    {
+        return $this->image->make($image)->setOutput('img');
+    }
+
+    /**
+     * Return the image instance.
+     *
+     * @param $image
+     * @return $this
+     */
+    public function image($image)
+    {
+        return $this->image->make($image)->setOutput('image');
+    }
+}
