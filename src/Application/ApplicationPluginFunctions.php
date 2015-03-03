8d7a59d296b6dd24f3db0c4b5733f3b8f870d5c5
Ryan Thompson <service@aiwebsystems.com>
2015-03-03 14:57:05 -0600

    Truncate

--- src/Application/ApplicationPluginFunctions.php
+++ src/Application/ApplicationPluginFunctions.php
@@ -1,8 +1,5 @@
 <?php namespace Anomaly\Streams\Platform\Application;
 
-use Anomaly\LocalizationModule\Language\Contract\LanguageInterface;
-use Anomaly\Streams\Platform\Entry\EntryCollection;
-
 /**
  * Class ApplicationPluginFunctions
  *
@@ -24,5 +21,38 @@ class ApplicationPluginFunctions
     public function env($key, $default = null)
     {
         return env($key, $default);
+    }
+
+    /**
+     * Limit the number of characters in a string
+     * while preserving words.
+     *
+     * https://github.com/laravel/framework/pull/3547/files
+     *
+     * @param  string $value
+     * @param  int    $limit
+     * @param  string $end
+     * @return string
+     */
+    public function truncate($value, $limit = 100, $end = '...')
+    {
+        if (mb_strlen($value) <= $limit) {
+            return $value;
+        }
+
+        $cutArea = mb_substr($value, $limit - 1, 2, 'UTF-8');
+
+        if (strpos($cutArea, ' ') === false) {
+
+            $value = mb_substr($value, 0, $limit, 'UTF-8');
+
+            $spacePos = strrpos($value, ' ');
+
+            if ($spacePos !== false) {
+                return rtrim(mb_substr($value, 0, $spacePos, 'UTF-8')) . $end;
+            }
+        }
+
+        return rtrim(mb_substr($value, 0, $limit, 'UTF-8')) . $end;
     }
 }
