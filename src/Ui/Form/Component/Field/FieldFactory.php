c0dec52932f41dfdbc0e45e691be42cfb84f6d85
Ryan Thompson <service@aiwebsystems.com>
2015-03-19 14:52:09 -0500

    No

--- src/Ui/Form/Component/Field/FieldFactory.php
+++ src/Ui/Form/Component/Field/FieldFactory.php
@@ -65,7 +65,7 @@ class FieldFactory
         } else {
             $field->setValue(array_pull($parameters, 'value'));
         }
-$parameters['config'] = ['buttons' => ['formatting', 'bold', 'italic']];
+
         // Merge in rules and validators.
         $field->mergeRules(array_pull($parameters, 'rules', []));
         $field->mergeConfig(array_pull($parameters, 'config', []));
