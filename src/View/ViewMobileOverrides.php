f2906443abae95a189063904b02310ff09f0bf50
Ryan Thompson <service@aiwebsystems.com>
2015-03-19 02:05:21 -0400

    Add support for view / mobile overrides

--- src/View/ViewMobileOverrides.php
+++ src/View/ViewMobileOverrides.php
@@ -13,4 +13,21 @@ use Illuminate\Support\Collection;
 class ViewMobileOverrides extends Collection
 {
 
+    /**
+     * When putting overrides replace "/" with "."
+     * to match the way Laravel interprets views.
+     *
+     * @param mixed $key
+     * @param mixed $value
+     */
+    public function put($key, $value)
+    {
+        $overrides = [];
+
+        foreach ($value as $view => $override) {
+            $overrides[str_replace('/', '.', $view)] = $override;
+        }
+
+        parent::put($key, $overrides);
+    }
 }
