cbf8c16c5df3ab7a11beda2284e62cb73ca2ca2f
Ryan Thompson <service@aiwebsystems.com>
2015-03-25 00:59:57 -0500

    FiresCallbacks

---
+++ src/Traits/FiresCallbacks.php
@@ -0,0 +1,70 @@
+<?php namespace Anomaly\Streams\Platform\Traits;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+/**
+ * Class FiresCallbacks
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Traits
+ */
+trait FiresCallbacks
+{
+
+    /**
+     * The registered callbacks.
+     *
+     * @var array
+     */
+    protected $callbacks = [];
+
+    /**
+     * Register a new callback.
+     *
+     * @param $trigger
+     * @param $callback
+     * @return $this
+     */
+    public function on($trigger, $callback)
+    {
+        if (!isset($this->callbacks[$trigger])) {
+            $this->callbacks[$trigger] = [];
+        }
+
+        $this->callbacks[$trigger][] = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Fire a set of closures by trigger.
+     *
+     * @param       $trigger
+     * @param array $parameters
+     */
+    public function fire($trigger, array $parameters = [])
+    {
+        if (!isset($this->callbacks[$trigger])) {
+            return;
+        }
+
+        foreach ($this->callbacks[$trigger] as $callback) {
+
+            if (is_string($callback) || $callback instanceof \Closure) {
+                app()->call($callback, $parameters);
+            }
+
+            if ($callback instanceof SelfHandling) {
+                call_user_func_array([$callback, 'handle'], $parameters);
+            }
+        }
+
+        $method = camel_case('on_' . $trigger);
+
+        if (method_exists($this, $method)) {
+            call_user_func_array([$this, $method], $parameters);
+        }
+    }
+}
