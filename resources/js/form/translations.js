2dc470747b56038126748ebc2ccf4312af3d6e82
Ryan Thompson <service@aiwebsystems.com>
2015-04-07 02:46:53 -0500

    Eh

---
+++ resources/js/form/translations.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $('#translations').find('a[lang]').click(function (e) {
+
+        e.preventDefault();
+
+        var selected = $(this);
+        var locale = $(this).attr('lang');
+        var form = $(this).closest('form');
+
+        selected.closest('ul').find('li').removeClass('active');
+        selected.closest('li').addClass('active');
+
+        form.find('.form-group[lang]').addClass('hidden');
+        form.find('.form-group[lang="' + locale + '"]').removeClass('hidden');
+    });
+});
