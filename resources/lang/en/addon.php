1726ff797eddbbb026da2b34a4e2eb16a8b6dd1b
Ryan Thompson <service@aiwebsystems.com>
2015-04-15 16:45:03 -0500

    K

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-    'addon'         => 'Addon',
-    'addons'        => 'Addons',
-    'block'         => 'Block',
-    'blocks'        => 'Blocks',
-    'theme'         => 'Theme',
-    'themes'        => 'Themes',
-    'module'        => 'Module',
-    'modules'       => 'Modules',
-    'plugin'        => 'Plugin',
-    'plugins'       => 'Plugins',
-    'extension'     => 'Extension',
-    'extensions'    => 'Extensions',
-    'field_type'    => 'Field Type',
-    'field_types'   => 'Field Types',
-    'distribution'  => 'Distribution',
-    'distributions' => 'Distributions'
+    'addon'       => 'Addon',
+    'addons'      => 'Addons',
+    'block'       => 'Block',
+    'blocks'      => 'Blocks',
+    'theme'       => 'Theme',
+    'themes'      => 'Themes',
+    'module'      => 'Module',
+    'modules'     => 'Modules',
+    'plugin'      => 'Plugin',
+    'plugins'     => 'Plugins',
+    'extension'   => 'Extension',
+    'extensions'  => 'Extensions',
+    'field_type'  => 'Field Type',
+    'field_types' => 'Field Types',
+    'name'        => 'Name',
+    'description' => 'Description'
 ];
