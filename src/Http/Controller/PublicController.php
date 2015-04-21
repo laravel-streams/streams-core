4550c2035930f835984f61a5dc336c4584f3363f
Ryan Thompson <service@aiwebsystems.com>
2015-04-21 13:36:59 -0500

    Fix issues with HTTP caching using views

--- src/Http/Controller/PublicController.php
+++ src/Http/Controller/PublicController.php
@@ -19,6 +19,6 @@ class PublicController extends BaseController
         parent::__construct();
 
         $this->middleware('Anomaly\Streams\Platform\Http\Middleware\CheckSiteStatus');
-        $this->middleware('Barryvdh\HttpCache\Middleware\CacheRequests');
+        $this->middleware('Anomaly\Streams\Platform\Http\Middleware\CacheRequests');
     }
 }
