31f35116c4f2bf0ea00b705fc9711d9871068336
Ryan Thompson <service@aiwebsystems.com>
2015-04-23 15:30:20 -0500

    Better empty messages

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    '403'              => 'Access denied.',
-    '503'              => 'Be right back.',
-    '404'              => 'Page not found.',
-    '500'              => 'There was an error.',
-    'no_results'       => 'No results.',
-    'delete_success'   => ':count row(s) were deleted successfully.',
-    'reorder_success'  => ':count row(s) were reordered successfully.',
-    'delete_installer' => 'The installer module still exists! Please delete it from your server! Leaving it online means control of this site could be granted to somebody else.<br><br><strong>Addons Module > Modules > Installer > Delete</strong>',
+    '403'                 => 'Access denied.',
+    '503'                 => 'Be right back.',
+    '404'                 => 'Page not found.',
+    '500'                 => 'There was an error.',
+    'no_results'          => 'No results.',
+    'no_fields_available' => 'No fields available.',
+    'delete_success'      => ':count row(s) were deleted successfully.',
+    'reorder_success'     => ':count row(s) were reordered successfully.',
+    'delete_installer'    => 'The installer module still exists! Please delete it from your server! Leaving it online means control of this site could be granted to somebody else.<br><br><strong>Addons Module > Modules > Installer > Delete</strong>',
 ];
