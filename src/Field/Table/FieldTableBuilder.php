71c643475f801cedd6fd4c97d3b9d5cbd410b32a
Ryan Thompson <service@aiwebsystems.com>
2015-05-02 01:15:10 -0500

    Delete

--- src/Field/Table/FieldTableBuilder.php
+++ src/Field/Table/FieldTableBuilder.php
@@ -60,6 +60,15 @@ class FieldTableBuilder extends TableBuilder
     ];
 
     /**
+     * The table actions.
+     *
+     * @var array
+     */
+    protected $actions = [
+        'delete'
+    ];
+
+    /**
      * The table options.
      *
      * @var array
