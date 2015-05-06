1116d89308a032b1d317c3f4cf0874f9488773e2
Ryan Thompson <service@aiwebsystems.com>
2015-05-06 04:19:43 -0500

    Couple small improvements to breadcrumbs

--- src/Ui/Breadcrumb/BreadcrumbCollection.php
+++ src/Ui/Breadcrumb/BreadcrumbCollection.php
@@ -1,5 +1,6 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Breadcrumb;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
 /**
@@ -13,4 +14,53 @@ use Illuminate\Support\Collection;
 class BreadcrumbCollection extends Collection
 {
 
+    /**
+     * The request object.
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * Create a new BreadcrumbCollection instance.
+     *
+     * @param Request $request
+     * @param array   $items
+     */
+    public function __construct(Request $request, $items = [])
+    {
+        $this->request = $request;
+
+        parent::__construct($items);
+    }
+
+    /**
+     * Add a breadcrumb.
+     *
+     * @param      $key
+     * @param null $url
+     */
+    public function add($key, $url = null)
+    {
+        if (!$url) {
+            $url = $this->request->fullUrl();
+        }
+
+        $this->put($key, $url);
+    }
+
+    /**
+     * Put a breadcrumb into the collection.
+     *
+     * @param string $key
+     * @param string $value
+     */
+    public function put($key, $value)
+    {
+        if (!starts_with($value, 'http')) {
+            $value = url($value);
+        }
+
+        parent::put($key, $value);
+    }
 }
