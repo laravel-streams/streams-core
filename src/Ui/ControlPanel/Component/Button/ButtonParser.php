0f38efe1f712108e4b7a2641f878595a4232506b
Ryan Thompson <service@aiwebsystems.com>
2015-05-06 02:50:19 -0500

    Tell don't ask

---
+++ src/Ui/ControlPanel/Component/Button/ButtonParser.php
@@ -0,0 +1,53 @@
+<?php namespace Anomaly\Streams\Platform\Ui\ControlPanel\Component\Button;
+
+use Anomaly\Streams\Platform\Support\Parser;
+use Anomaly\Streams\Platform\Ui\ControlPanel\ControlPanelBuilder;
+use Illuminate\Http\Request;
+
+/**
+ * Class ButtonParser
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Platform\Ui\ControlPanel\Component\Button
+ */
+class ButtonParser
+{
+
+    /**
+     * The parser utility.
+     *
+     * @var Parser
+     */
+    protected $parser;
+
+    /**
+     * The request object.
+     *
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * Create a new ButtonParser instance.
+     *
+     * @param Parser  $parser
+     * @param Request $request
+     */
+    public function __construct(Parser $parser, Request $request)
+    {
+        $this->parser  = $parser;
+        $this->request = $request;
+    }
+
+    /**
+     * Parse the control panel buttons.
+     *
+     * @param ControlPanelBuilder $builder
+     */
+    public function parse(ControlPanelBuilder $builder)
+    {
+        $parameters = $this->request->route()->parameters();
+    }
+}
