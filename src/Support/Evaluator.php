8b2b6585928f0e2c2d34b1eade87dea928b21f7d
Ryan Thompson <service@aiwebsystems.com>
2015-06-29 18:27:48 -0500

    Do NOT match exact argument keys. This will prevent issues with fields named 'builder' for instance.

--- src/Support/Evaluator.php
+++ src/Support/Evaluator.php
@@ -61,7 +61,7 @@ class Evaluator
          * if the target is a string and is in a traversable
          * format then traverse the target using the arguments.
          */
-        if (is_string($target) && $this->isTraversable($target)) {
+        if (is_string($target) && !isset($arguments[$target]) && $this->isTraversable($target)) {
             $target = data_get($arguments, $target, $target);
         }
 
