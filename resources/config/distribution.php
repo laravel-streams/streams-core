b030bba9b7f7b52a064bbacf91a5c424ad2a1fd3
Ryan Thompson <ryan@anomaly.is>
2015-07-28 23:55:31 -0500

    Update distribution.php

--- resources/config/distribution.php
+++ resources/config/distribution.php
@@ -2,5 +2,6 @@
 
 return [
     'name'        => 'Streams Platform',
-    'description' => 'Streams is an abstracted modular platform for developing web applications.'
+    'description' => 'Streams is an abstracted modular platform for developing web applications.',
+    'version'     => 'Streams Platform V1.0'
 ];
