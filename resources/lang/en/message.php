503e66419e38c29d64787cd01d932d3bbb3dbacb
Ryan Thompson <service@aiwebsystems.com>
2015-07-31 23:41:53 -0500

    No action

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -11,10 +11,7 @@ return [
     'delete_success'      => ':count row(s) were deleted successfully.',
     'reorder_success'     => ':count row(s) were reordered successfully.',
     'csrf_token_mismatch' => 'Your security token has expired. Please submit the form again.',
-    'delete_installer'    => 'The installer module still exists! Please delete it from your server! Leaving it online means control of this site could be granted to somebody else.<br><br><strong>' . link_to(
-            'admin/addons/modules/delete/anomaly.module.installer',
-            'Click here to delete the installer now.'
-        ) . '</strong>',
+    'delete_installer'    => 'The installer module still exists! Please delete it from your server! Leaving it online means control of this site could be granted to somebody else.',
     'create_success'      => ':name created successfully.',
     'edit_success'        => ':name updated successfully.',
     'confirm_delete'      => 'Are you sure you want to delete?<br><small>This can not be undone.</small>',
