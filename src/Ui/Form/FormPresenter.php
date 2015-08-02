c7ee4d72b2baa1331da7e6f8ef687f85074a2f1b
Ryan Thompson <service@aiwebsystems.com>
2015-08-02 16:04:45 -0500

    Form presenter and fields view

--- src/Ui/Form/FormPresenter.php
+++ src/Ui/Form/FormPresenter.php
@@ -1,6 +1,8 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Form;
 
 use Anomaly\Streams\Platform\Support\Presenter;
+use Collective\Html\FormBuilder as Html;
+use Illuminate\View\Factory;
 use Illuminate\View\View;
 
 /**
@@ -15,12 +17,88 @@ class FormPresenter extends Presenter
 {
 
     /**
+     * The form HTML builder.
+     *
+     * @var Html
+     */
+    protected $html;
+
+    /**
+     * The view factory.
+     *
+     * @var Factory
+     */
+    protected $view;
+
+    /**
      * The decorated object.
      * This is for IDE support.
      *
      * @var Form
      */
     protected $object;
+
+    /**
+     * Create a new FormPresenter instance.
+     *
+     * @param Html    $form
+     * @param Factory $view
+     * @param         $object
+     */
+    public function __construct(Html $form, Factory $view, $object)
+    {
+        $this->html = $form;
+        $this->view = $view;
+
+        parent::__construct($object);
+    }
+
+    /**
+     * Return the opening form tag.
+     *
+     * @param array $options
+     * @return string
+     */
+    public function open(array $options = [])
+    {
+        return $this->html->open($options);
+    }
+
+    /**
+     * Return the closing form tag.
+     *
+     * @return string
+     */
+    public function close()
+    {
+        return $this->html->close();
+    }
+
+    /**
+     * Return the form layout.
+     *
+     * @param null $view
+     * @return string
+     */
+    public function body($view = null)
+    {
+        return $this->view
+            ->make($view ?: 'streams::form/partials/fields', ['form' => $this->object])
+            ->render();
+    }
+
+    /**
+     * Return the action buttons.
+     *
+     * @param null $view
+     * @return string
+     */
+    public function actions($view = null)
+    {
+        return $this->view
+            ->make($view ?: 'streams::buttons/buttons', ['buttons' => $this->object->getActions()])
+            ->render();
+    }
 
     /**
      * Display the form content.
