48eec81352a25746426fdfbc0cd7dbfd4f261969
Ryan Thompson <service@aiwebsystems.com>
2015-08-30 13:21:56 -0500

    Compile this stuff

--- src/StreamsCompilerProvider.php
+++ src/StreamsCompilerProvider.php
@@ -19,13 +19,33 @@ class StreamsCompilerProvider
      */
     public static function compiles()
     {
-        $files = app('files')->allFiles(__DIR__);
+        return [
 
-        return array_map(
-            function (\SplFileInfo $file) {
-                return $file->getPathname();
-            },
-            $files
-        );
+            // Models
+            'vendor/anomaly/streams-platform/src/Entry/EntryModel.php',
+            'vendor/anomaly/streams-platform/src/Field/FieldModel.php',
+            'vendor/anomaly/streams-platform/src/Model/EloquentModel.php',
+            'vendor/anomaly/streams-platform/src/Stream/StreamModel.php',
+            'vendor/anomaly/streams-platform/src/Assignment/AssignmentModel.php',
+
+            // Addons
+            'vendor/anomaly/streams-platform/src/Addon/Theme/Theme.php',
+            'vendor/anomaly/streams-platform/src/Addon/Module/Module.php',
+            'vendor/anomaly/streams-platform/src/Addon/Plugin/Plugin.php',
+            'vendor/anomaly/streams-platform/src/Addon/Extension/Extension.php',
+            'vendor/anomaly/streams-platform/src/Addon/FieldType/FieldType.php',
+
+            // Support
+            'vendor/anomaly/streams-platform/src/Support/Parser.php',
+            'vendor/anomaly/streams-platform/src/Support/String.php',
+            'vendor/anomaly/streams-platform/src/Support/Observer.php',
+            'vendor/anomaly/streams-platform/src/Support/Resolver.php',
+            'vendor/anomaly/streams-platform/src/Support/Decorator.php',
+            'vendor/anomaly/streams-platform/src/Support/Evaluator.php',
+            'vendor/anomaly/streams-platform/src/Support/Evaluator.php',
+            'vendor/anomaly/streams-platform/src/Support/Authorizer.php',
+            'vendor/anomaly/streams-platform/src/Support/Translator.php',
+            'vendor/anomaly/streams-platform/src/Support/Configurator.php'
+        ];
     }
 }
