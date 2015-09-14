9b01a4fc60964db863394f8dd63931548d722cfd
Ryan Thompson <service@aiwebsystems.com>
2015-09-14 12:49:24 -0500

    Access the autoloader with SPL

--- src/Addon/AddonLoader.php
+++ src/Addon/AddonLoader.php
@@ -10,8 +10,23 @@ use Composer\Autoload\ClassLoader;
  * @author  Ryan Thompson <ryan@anomaly.is>
  * @package Anomaly\Streams\Platform\Addon
  */
-class AddonLoader extends ClassLoader
+class AddonLoader
 {
+
+    /**
+     * The class loader instance.
+     *
+     * @var ClassLoader
+     */
+    protected $loader;
+
+    /**
+     * Create a new AddonLoader instance.
+     */
+    public function __construct()
+    {
+        $this->loader = spl_autoload_functions()[1][0];
+    }
 
     /**
      * Load the addon.
@@ -36,15 +51,23 @@ class AddonLoader extends ClassLoader
         }
 
         foreach (array_get($composer['autoload'], 'psr-4', []) as $namespace => $autoload) {
-            parent::addPsr4($namespace, $path . '/' . $autoload, false);
+            $this->loader->addPsr4($namespace, $path . '/' . $autoload, false);
         }
 
         foreach (array_get($composer['autoload'], 'psr-0', []) as $namespace => $autoload) {
-            parent::add($namespace, $path . '/' . $autoload, false);
+            $this->loader->add($namespace, $path . '/' . $autoload, false);
         }
 
         foreach (array_get($composer['autoload'], 'files', []) as $file) {
             include($path . '/' . $file);
         }
+    }
+
+    /**
+     * Register the loader.
+     */
+    public function register()
+    {
+        $this->loader->register();
     }
 }
