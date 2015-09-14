9053d0d655f01967ed848fb2dfc682036723d8ba
Joel Ekström Svensson <JoelESvensson@users.noreply.github.com>
2015-09-14 13:10:51 +0200

    Typo

--- src/Exception/ExceptionHandler.php
+++ src/Exception/ExceptionHandler.php
@@ -30,7 +30,7 @@ class ExceptionHandler extends Handler
         if ($this->isHttpException($e)) {
             return $this->renderHttpException($e);
         } elseif (!config('app.debug')) {
-            return response()>view("streams::errors.500", ['message' => $e->getMessage()], 500);
+            return response()->view("streams::errors.500", ['message' => $e->getMessage()], 500);
         } else {
             return parent::render($request, $e);
         }
