5fb10b94c86a211c46e8d80d241575127d32e357
Joel E. Svensson <joeles_93@hotmail.com>
2015-09-24 19:33:34 +0200

    Added Swedish translation. Also, SV is the correct language code

---
+++ resources/lang/sv/field.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'name'        => [
+        'name'         => 'Namn',
+        'instructions' => 'Vad är fältets namn?'
+    ],
+    'slug'        => [
+        'name'         => 'Slug',
+        'instructions' => 'Sluggen är, bland annat, använt för kolumnnamnet i databasen.'
+    ],
+    'description' => [
+        'name'         => 'Beskrivning',
+        'instructions' => 'Skriv en kortfattad beskrivning.'
+    ],
+    'type'        => [
+        'name'         => 'Fälttyp',
+        'instructions' => 'Vilken fälttyp vill du använda för detta fältet?'
+    ],
+];
