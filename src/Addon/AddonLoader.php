fd70efde3144017111451f03673af4b6b5c10474
Ryan Thompson <service@aiwebsystems.com>
2015-09-27 01:30:49 -0500

    No autoloader - it causes recursion issues

--- src/Addon/AddonLoader.php
+++ src/Addon/AddonLoader.php
@@ -43,15 +43,6 @@ class AddonLoader
      */
     public function load($path)
     {
-        $autoload = $path . '/vendor/autoload.php';
-
-        if (file_exists($autoload)) {
-
-            include $autoload;
-
-            return;
-        }
-
         $composer = json_decode(file_get_contents($path . '/composer.json'), true);
 
         if (!array_key_exists('autoload', $composer)) {
