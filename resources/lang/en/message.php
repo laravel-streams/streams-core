3a874293c43c9d015875054d6724604d25dbae64
Ryan Thompson <service@aiwebsystems.com>
2015-09-28 04:00:16 -0500

    Disabled support

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -17,4 +17,5 @@ return [
     'confirm_delete'      => '<h3>Are you sure you want to delete?</h3><p>This can not be undone.</p>',
     'confirm_uninstall'   => '<h3>Are you sure you want to uninstall?</h3><p>Any associated data will be lost.</p>',
     'prompt_delete'       => '<h3>Are you sure you want to delete?</h3><p>Type \"yes\" to confirm.</p>',
+    'form_is_read_only'   => 'This form is read only!',
 ];
