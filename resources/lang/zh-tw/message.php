85576f22f1772fb374fbb194a1128481b2cc3511
Jack Sun <sunhsilu@gmail.com>
2015-11-16 00:03:25 +0800

    Add chinese traditional translation.

---
+++ resources/lang/zh-tw/message.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    '403'                 => '無法存取',
+    '429'                 => '太多人在使用了，<br><small>請您稍後再試。</small>',
+    '503'                 => '馬上回來。',
+    '404'                 => '找不到網頁。',
+    '500'                 => '產生了錯誤。',
+    'help'                => '幫助',
+    'no_results'          => '沒有資料。',
+    'access_denied'       => '無法存取。',
+    'no_fields_available' => '沒有欄位可以使用。',
+    'delete_success'      => ':count 列資料已經成功被刪除。',
+    'reorder_success'     => ':count 列資料已經成功被重新排序',
+    'csrf_token_mismatch' => '用以提供安全性的檢核碼已經過期，請重新送出表單。',
+    'delete_installer'    => '安裝程序的模組仍然存在！請從您的伺服器中刪除！在網站中留下此模組，意味著本網站的控制權可能被授予給別人。',
+    'create_success'      => ':name 成功的建立了。',
+    'edit_success'        => ':name 成功的更新了。',
+    'confirm_delete'      => '<h3>請問確認要 刪除 嗎？</h3><p>這個將無法被還原。</p>',
+    'confirm_uninstall'   => '<h3>請問確認要 解除安裝 嗎？</h3><p>任何被關聯的資料將會被刪除。</p>',
+    'prompt_delete'       => '<h3>請問確認要 刪除 嗎？</h3><p>請輸入 \"yes\" 來進行確認。</p>',
+    'form_is_read_only'   => '這是個唯讀的表單。',
+];
