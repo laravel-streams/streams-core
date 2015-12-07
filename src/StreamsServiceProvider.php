87a2d3c8e6e2af3d188267cadefbc1405af6ded2
Ryan Thompson <service@aiwebsystems.com>
2015-12-07 03:47:53 -0600

    Mailer

--- src/StreamsServiceProvider.php
+++ src/StreamsServiceProvider.php
@@ -18,6 +18,7 @@ use Anomaly\Streams\Platform\Event\Ready;
 use Anomaly\Streams\Platform\Field\FieldModel;
 use Anomaly\Streams\Platform\Field\FieldObserver;
 use Anomaly\Streams\Platform\Image\Command\AddImageNamespaces;
+use Anomaly\Streams\Platform\Mail\Mailer;
 use Anomaly\Streams\Platform\Model\EloquentModel;
 use Anomaly\Streams\Platform\Model\EloquentObserver;
 use Anomaly\Streams\Platform\Stream\StreamModel;
@@ -232,6 +233,58 @@ class StreamsServiceProvider extends ServiceProvider
                 $manager->register();
 
                 $events->fire(new Ready());
+            }
+        );
+
+        $this->app->singleton(
+            'mailer',
+            function () {
+
+                $mailer = new Mailer(
+                    $this->app['view'], $this->app['swift.mailer'], $this->app['events']
+                );
+
+                $mailer->setContainer($this->app);
+
+                if ($this->app->bound('Psr\Log\LoggerInterface')) {
+                    $mailer->setLogger($this->app->make('Psr\Log\LoggerInterface'));
+                }
+
+                if ($this->app->bound('queue')) {
+                    $mailer->setQueue($this->app['queue.connection']);
+                }
+
+                $from = $this->app['config']['mail.from'];
+
+                if (is_array($from) && isset($from['address'])) {
+                    $mailer->alwaysFrom($from['address'], $from['name']);
+                }
+
+                $to = $this->app['config']['mail.to'];
+
+                if (is_array($to) && isset($to['address'])) {
+                    $mailer->alwaysTo($to['address'], $to['name']);
+                }
+
+                $pretend = $this->app['config']->get('mail.pretend', false);
+
+                $mailer->pretend($pretend);
+
+                return $mailer;
+            }
+        );
+
+        $this->app->singleton(
+            'Illuminate\Mail\Mailer',
+            function () {
+                return $this->app->make('mailer');
+            }
+        );
+
+        $this->app->singleton(
+            'Illuminate\Contracts\Mail\Mailer',
+            function () {
+                return $this->app->make('mailer');
             }
         );
     }
