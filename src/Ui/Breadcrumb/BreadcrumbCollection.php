0743e137157199740f053cc2d35a50fbe6d0d9ae
Ryan Thompson <ryan@pyrocms.com>
2015-12-30 11:25:13 -0600

    Breadcrumb needs to be fed explicitly

--- src/Ui/Breadcrumb/BreadcrumbCollection.php
+++ src/Ui/Breadcrumb/BreadcrumbCollection.php
@@ -1,7 +1,6 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Breadcrumb;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
+use Anomaly\Streams\Platform\Support\Collection;
 
 /**
  * Class BreadcrumbCollection
@@ -15,37 +14,13 @@ class BreadcrumbCollection extends Collection
 {
 
     /**
-     * The request object.
-     *
-     * @var Request
-     */
-    protected $request;
-
-    /**
-     * Create a new BreadcrumbCollection instance.
-     *
-     * @param Request $request
-     * @param array   $items
-     */
-    public function __construct(Request $request, $items = [])
-    {
-        $this->request = $request;
-
-        parent::__construct($items);
-    }
-
-    /**
      * Add a breadcrumb.
      *
      * @param      $key
      * @param null $url
      */
-    public function add($key, $url = null)
+    public function add($key, $url)
     {
-        if (!$url) {
-            $url = $this->request->fullUrl();
-        }
-
         $this->put($key, $url);
     }
 
