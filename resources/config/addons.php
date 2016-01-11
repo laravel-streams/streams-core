2b29bafd34b897daac9c42d17d5a886ea12ca609
Ryan Thompson <ryan@pyrocms.com>
2016-01-11 10:41:59 -0600

    Allow specifying deferred / eager addons.

--- resources/config/addons.php
+++ resources/config/addons.php
@@ -17,5 +17,33 @@ return [
         'module',
         'plugin',
         'theme'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Eager Loaded Addons
+    |--------------------------------------------------------------------------
+    |
+    | Eager loaded addons are registered first and can be defined
+    | here by specifying their relative path to the project root.
+    |
+    */
+
+    'eager' => [
+        //'core/anomaly/redirects-module'
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deferred Addons
+    |--------------------------------------------------------------------------
+    |
+    | Deferred loaded addons are registered last and can be defined
+    | here by specifying their relative path to the project root.
+    |
+    */
+
+    'deferred' => [
+        //'core/anomaly/pages-module'
     ]
 ];
