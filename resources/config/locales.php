2e653b0ab75bb57a54e5d5176527dc6a22057456
Tanel Tammik <keevitaja@gmail.com>
2016-02-25 20:13:51 +0200

    Adding Estonian localization

--- resources/config/locales.php
+++ resources/config/locales.php
@@ -63,6 +63,9 @@ return [
         'es' => [
             'direction' => 'ltr'
         ],
+        'et' => [
+            'direction' => 'ltr'
+        ],
         'fr' => [
             'direction' => 'ltr'
         ],
