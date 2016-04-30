a14fe6ca47a957c5336317052dca3be8651ff00d
Ryan Thompson <service@aiwebsystems.com>
2016-04-29 19:52:15 -0500

    Hooks are what we have needed here.

--- src/Entry/EntryQueryBuilder.php
+++ src/Entry/EntryQueryBuilder.php
@@ -19,41 +19,4 @@ class EntryQueryBuilder extends EloquentQueryBuilder
      * @var EntryModel
      */
     protected $model;
-
-    /**
-     * Dynamically handle calls into the query instance.
-     *
-     * @param  string $method
-     * @param  array  $parameters
-     * @return mixed
-     */
-    public function __call($method, $parameters)
-    {
-        if (ends_with($method, 'FieldType')) {
-            return $this->fieldType(
-                snake_case(substr_replace($method, '', strrpos($method, 'FieldType'), strlen('FieldType'))),
-                $parameters
-            );
-        }
-
-        return parent::__call($method, $parameters);
-    }
-
-    /**
-     * Check for field type querying methods.
-     *
-     * @param string $method
-     * @param array  $parameters
-     * @return mixed
-     */
-    public function fieldType($method, $parameters)
-    {
-        $query = $this->model->getFieldTypeQuery(snake_case($method));
-
-        $method = array_shift($parameters);
-
-        array_unshift($parameters, $this);
-
-        return call_user_func_array([$query, camel_case($method)], $parameters);
-    }
 }
