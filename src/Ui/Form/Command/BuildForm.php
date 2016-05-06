1dde0c1b048f911815c1cb909f1830e1950ac464
Brennon Loveless <brennon.loveless@gmail.com>
2016-05-06 10:25:17 -0600

    Setting the form entry before the setting the form options and default form options allows us to use the entry data in the form options. I.E. using the entry name (if it exists) in the form title.

--- src/Ui/Form/Command/BuildForm.php
+++ src/Ui/Form/Command/BuildForm.php
@@ -59,9 +59,9 @@ class BuildForm implements SelfHandling
         $this->dispatch(new SetDefaultParameters($this->builder));
         $this->dispatch(new SetRepository($this->builder));
 
+        $this->dispatch(new SetFormEntry($this->builder));
         $this->dispatch(new SetFormOptions($this->builder));
         $this->dispatch(new SetDefaultOptions($this->builder));
-        $this->dispatch(new SetFormEntry($this->builder)); // Do this last.
 
         /**
          * Before we go any further, authorize the request.
