19cd9d61c5bd968a86189b5643a3e355dfb4c1c0
Ryan Thompson <service@aiwebsystems.com>
2016-05-19 18:47:36 -0500

    Settings

--- resources/config/settings/sections.php
+++ resources/config/settings/sections.php
@@ -1,34 +1,29 @@
 <?php
 
 return [
-    'general'   => [
-        'title' => 'streams::label.general',
-        'tabs'  => [
-            'details' => [
-                'context' => 'info',
-                'title'   => 'streams::label.details',
-                'fields'  => [
-                    'name',
-                    'description'
-                ]
-            ],
-            'contact' => [
-                'context' => 'primary',
-                'title'   => 'streams::label.contact',
-                'fields'  => [
-                    'business',
-                    'phone',
-                    'address',
-                    'address2',
-                    'city',
-                    'state',
-                    'postal_code',
-                    'country'
-                ]
-            ]
+    'details'      => [
+        'context' => 'info',
+        'title'   => 'streams::label.details',
+        'fields'  => [
+            'name',
+            'description'
         ]
     ],
-    'display'   => [
+    'contact'      => [
+        'context' => 'primary',
+        'title'   => 'streams::label.contact',
+        'fields'  => [
+            'business',
+            'phone',
+            'address',
+            'address2',
+            'city',
+            'state',
+            'postal_code',
+            'country'
+        ]
+    ],
+    'display'      => [
         'context' => 'info',
         'title'   => 'streams::label.display',
         'fields'  => [
@@ -37,57 +32,47 @@ return [
             'per_page'
         ]
     ],
-    'standards' => [
-        'title' => 'streams::label.standards',
-        'tabs'  => [
-            'formats'      => [
-                'context' => 'danger',
-                'title'   => 'streams::label.formats',
-                'fields'  => [
-                    'timezone',
-                    'date_format',
-                    'time_format',
-                    'unit_system',
-                    'currency'
-                ]
-            ],
-            'localization' => [
-                'context' => 'info',
-                'title'   => 'streams::label.localization',
-                'fields'  => [
-                    'default_locale',
-                    'enabled_locales'
-                ]
-            ]
+    'formats'      => [
+        'context' => 'danger',
+        'title'   => 'streams::label.formats',
+        'fields'  => [
+            'timezone',
+            'date_format',
+            'time_format',
+            'unit_system',
+            'currency'
         ]
     ],
-    'advanced'  => [
-        'title' => 'streams::label.advanced',
-        'tabs'  => [
-            'email'       => [
-                'context' => 'danger',
-                'title'   => 'streams::label.email',
-                'fields'  => [
-                    'mail_debug',
-                    'email',
-                    'sender',
-                    'mail_driver',
-                    'mail_host',
-                    'mail_port',
-                    'mail_username',
-                    'mail_password'
-                ]
-            ],
-            'maintenance' => [
-                'context' => 'danger',
-                'title'   => 'streams::label.maintenance',
-                'fields'  => [
-                    'debug',
-                    'maintenance',
-                    'basic_auth',
-                    'ip_whitelist'
-                ]
-            ]
+    'localization' => [
+        'context' => 'info',
+        'title'   => 'streams::label.localization',
+        'fields'  => [
+            'default_locale',
+            'enabled_locales'
         ]
     ],
+    'email'        => [
+        'context' => 'danger',
+        'title'   => 'streams::label.email',
+        'fields'  => [
+            'mail_debug',
+            'email',
+            'sender',
+            'mail_driver',
+            'mail_host',
+            'mail_port',
+            'mail_username',
+            'mail_password'
+        ]
+    ],
+    'maintenance'  => [
+        'context' => 'danger',
+        'title'   => 'streams::label.maintenance',
+        'fields'  => [
+            'debug',
+            'maintenance',
+            'basic_auth',
+            'ip_whitelist'
+        ]
+    ]
 ];
