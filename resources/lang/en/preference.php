b4344e3d4c08a9700460738b3b11f9d1ce8ca272
Ryan Thompson <service@aiwebsystems.com>
2016-05-19 20:27:17 -0500

    Preferences

--- resources/lang/en/preference.php
+++ resources/lang/en/preference.php
@@ -1,24 +1,20 @@
 <?php
 
 return [
-    'timezone'      => [
+    'timezone'    => [
         'label'        => 'Timezone',
-        'instructions' => 'Specify your timezone. This will be used by all date and time functions.'
+        'instructions' => 'Specify your timezone.'
     ],
-    'date_format'   => [
+    'date_format' => [
         'label'        => 'Date Format',
-        'instructions' => 'How do you want dates to be displayed across the website and control panel?'
+        'instructions' => 'Specify your preferred date format.'
     ],
-    'time_format'   => [
+    'time_format' => [
         'label'        => 'Time Format',
-        'instructions' => 'How do you want time to be displayed across the website and control panel?'
+        'instructions' => 'Specify your preferred time format.'
     ],
-    'admin_locale'  => [
-        'label'        => 'Control Panel Language',
-        'instructions' => 'What language would you like to view <strong>control panel</strong> content in?'
-    ],
-    'public_locale' => [
-        'label'        => 'Public Language',
-        'instructions' => 'What language would you like to view <strong>public</strong> content in?'
+    'per_page'    => [
+        'label'        => 'Results Per Page',
+        'instructions' => 'Specify the number of results you prefer to display on each page.'
     ]
 ];
