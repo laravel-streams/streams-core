a32f443da0c3b5988d85285d97ff5aca4572ec61
Ryan Thompson <ryan@pyrocms.com>
2016-05-23 09:23:16 -0500

    Update installer.php

--- resources/lang/en/installer.php
+++ resources/lang/en/installer.php
@@ -5,6 +5,7 @@ return [
     'running_core_migrations'        => 'Running core migrations.',
     'running_application_migrations' => 'Running application migrations.',
     'running_migrations'             => 'Running other migrations.',
+    'running_seeds'                  => 'Running project seeds.',
     'installing'                     => 'Installing: :installing',
     'seeding'                        => 'Seeding: :seeding'
 ];
