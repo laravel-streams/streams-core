869a9e248a190128d032433d5d3134dfdb5dff27
Ryan Thompson <ryan@pyrocms.com>
2016-06-09 15:17:06 -0500

    Remove these settings until fully thought out.

--- resources/config/system.php
+++ resources/config/system.php
@@ -24,39 +24,4 @@ return [
     */
 
     'unit_system' => env('UNIT_SYSTEM', 'imperial'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Force HTTPS
-    |--------------------------------------------------------------------------
-    |
-    | Redirect to HTTPS as desired.
-    |
-    | Available options are:
-    |
-    | 'none'    - don't force https
-    | 'public'  - force https for public only
-    | 'admin'   - force https for control panel only
-    | 'all'     - force https for public and control panel
-    |
-    */
-
-    'force_https' => env('FORCE_HTTPS', 'none'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Prefer WWW / non-WWW
-    |--------------------------------------------------------------------------
-    |
-    | Redirect to WWW or non-WWW as preferred.
-    |
-    | Available options are:
-    |
-    | 'none'    - no preference
-    | 'www'     - redirect to www
-    | 'domain'  - redirect to non-www
-    |
-    */
-
-    'www_preference' => env('WWW_PREFERENCE', 'none'),
 ];
