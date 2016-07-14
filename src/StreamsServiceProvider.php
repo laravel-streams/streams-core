d6bf0cd8c37828f5df4b8ae1b1a3ee13df256423
Ryan Thompson <ryan@pyrocms.com>
2016-07-14 01:09:54 -0500

    Fix issue where mailer was not being bound correctly

--- src/StreamsServiceProvider.php
+++ src/StreamsServiceProvider.php
@@ -20,7 +20,7 @@ use Anomaly\Streams\Platform\Event\Ready;
 use Anomaly\Streams\Platform\Field\FieldModel;
 use Anomaly\Streams\Platform\Field\FieldObserver;
 use Anomaly\Streams\Platform\Image\Command\AddImageNamespaces;
-use Anomaly\Streams\Platform\Mail\Mailer;
+use Anomaly\Streams\Platform\Mail\Command\RegisterMailer;
 use Anomaly\Streams\Platform\Model\EloquentModel;
 use Anomaly\Streams\Platform\Model\EloquentObserver;
 use Anomaly\Streams\Platform\Routing\Command\IncludeRoutes;
@@ -71,7 +71,7 @@ class StreamsServiceProvider extends ServiceProvider
      */
     protected $providers = [
         'Anomaly\Streams\Platform\StreamsConsoleProvider',
-        'Anomaly\Streams\Platform\StreamsEventProvider'
+        'Anomaly\Streams\Platform\StreamsEventProvider',
     ];
 
     /**
@@ -220,6 +220,7 @@ class StreamsServiceProvider extends ServiceProvider
         $this->dispatch(new AddAssetNamespaces());
         $this->dispatch(new AddImageNamespaces());
         $this->dispatch(new AddViewNamespaces());
+        $this->dispatch(new RegisterMailer());
 
         // Observe our base models.
         EntryModel::observe(EntryObserver::class);
@@ -270,58 +271,6 @@ class StreamsServiceProvider extends ServiceProvider
                 $this->dispatch(new IncludeRoutes());
 
                 $events->fire(new Ready());
-            }
-        );
-
-        $this->app->singleton(
-            'mailer',
-            function () {
-
-                $mailer = new Mailer(
-                    $this->app['view'], $this->app['swift.mailer'], $this->app['events']
-                );
-
-                $mailer->setContainer($this->app);
-
-                if ($this->app->bound('Psr\Log\LoggerInterface')) {
-                    $mailer->setLogger($this->app->make('Psr\Log\LoggerInterface'));
-                }
-
-                if ($this->app->bound('queue')) {
-                    $mailer->setQueue($this->app['queue.connection']);
-                }
-
-                $from = $this->app['config']['mail.from'];
-
-                if (is_array($from) && isset($from['address'])) {
-                    $mailer->alwaysFrom($from['address'], $from['name']);
-                }
-
-                $to = $this->app['config']['mail.to'];
-
-                if (is_array($to) && isset($to['address'])) {
-                    $mailer->alwaysTo($to['address'], $to['name']);
-                }
-
-                $pretend = $this->app['config']->get('mail.pretend', false);
-
-                $mailer->pretend($pretend);
-
-                return $mailer;
-            }
-        );
-
-        $this->app->singleton(
-            'Illuminate\Mail\Mailer',
-            function () {
-                return $this->app->make('mailer');
-            }
-        );
-
-        $this->app->singleton(
-            'Illuminate\Contracts\Mail\Mailer',
-            function () {
-                return $this->app->make('mailer');
             }
         );
 
