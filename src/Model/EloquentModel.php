17b25466d5c5129c487ef3e46ab8fa8bcc70d0b9
Ryan Thompson <ryan@pyrocms.com>
2016-08-02 17:51:05 -0500

    Fires callbacks

--- src/Model/EloquentModel.php
+++ src/Model/EloquentModel.php
@@ -1,6 +1,7 @@
 <?php namespace Anomaly\Streams\Platform\Model;
 
 use Anomaly\Streams\Platform\Collection\CacheCollection;
+use Anomaly\Streams\Platform\Traits\FiresCallbacks;
 use Anomaly\Streams\Platform\Traits\Hookable;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Builder;
@@ -21,6 +22,7 @@ class EloquentModel extends Model implements Arrayable
 
     use DispatchesJobs;
     use Hookable;
+    use FiresCallbacks;
 
     /**
      * Disable timestamps for this model.
