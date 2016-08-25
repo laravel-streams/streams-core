e9b57b66ea6c5b505780f63af2f4c3f424b3803f
Ryan Thompson <ryan@pyrocms.com>
2016-08-25 09:12:04 -0500

    Update actions.js

--- resources/js/table/actions.js
+++ resources/js/table/actions.js
@@ -1,18 +1,18 @@
-$(function () {
+$(function() {
 
     // Toggle all table actions
-    $('[data-toggle="all"]').on('change', function () {
+    $('[data-toggle="all"]').on('change', function() {
 
         $(this).closest('table').find(':checkbox').prop('checked', $(this).is(':checked'));
     });
 
     // Only allow actions if rows are selected.
-    $('table').find(':checkbox').on('change', function () {
+    $('table').find(':checkbox').on('change', function() {
 
         if ($(this).closest('form').find(':checkbox:checked').length) {
-            $(this).closest('form').find('.actions').find('button:not([data-ignore])').removeClass('disabled');
+            $(this).closest('form').find('.actions').find('button:not([data-ignore])').removeAttr('disabled').removeClass('disabled');
         } else {
-            $(this).closest('form').find('.actions').find('button:not([data-ignore])').addClass('disabled');
+            $(this).closest('form').find('.actions').find('button:not([data-ignore])').attr('disabled', 'disabled').addClass('disabled');
         }
     });
 });
