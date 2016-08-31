fceaf34480d628b12afd2dc804b826b2b6c7b489
Ryan Thompson <service@aiwebsystems.com>
2016-08-31 17:22:42 -0500

    Map to view data

--- src/Notification/Message/MailMessage.php
+++ src/Notification/Message/MailMessage.php
@@ -8,4 +8,20 @@ class MailMessage extends \Illuminate\Notifications\Messages\MailMessage
      * @var string
      */
     public $view = 'streams::notifications/email';
+
+    /**
+     * Map calls to undefined functions
+     * to set public properties for later.
+     *
+     * @param string $method
+     * @param array  $arguments
+     *
+     * @return $this
+     */
+    public function __call($method, array $arguments = [])
+    {
+        $this->viewData[snake_case($method)] = array_shift($arguments);
+
+        return $this;
+    }
 }
