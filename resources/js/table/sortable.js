1795d45871fe87f3f0444f32288a3f420a5eb365
Ryan Thompson <ryan@pyrocms.com>
2016-09-06 11:53:47 -0500

    Fix issue where reorder button was not removing attr

--- resources/js/table/sortable.js
+++ resources/js/table/sortable.js
@@ -1,13 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('table[data-sortable]').sortable({
         handle: '.handle',
         itemSelector: 'tr',
         itemPath: '> tbody',
         containerSelector: 'table',
         placeholder: '<tr class="placeholder"/>',
-        afterMove: function ($placeholder) {
+        afterMove: function($placeholder) {
 
-            $placeholder.closest('table').find('button.reorder').removeClass('disabled');
+            $placeholder
+                .closest('table')
+                .find('button.reorder')
+                .removeClass('disabled')
+                .removeAttr('disabled');
 
             $placeholder.closest('table').find('.dragged').detach().insertBefore($placeholder);
         }
