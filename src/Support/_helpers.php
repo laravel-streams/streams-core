90a12bf01c26d75eaf95cd3c56b3eb669f01e018
Ryan Thompson <service@aiwebsystems.com>
2016-09-18 02:34:48 -0500

    Valuate method

--- src/Support/_helpers.php
+++ src/Support/_helpers.php
@@ -2,6 +2,7 @@
 
 use Anomaly\Streams\Platform\Support\Parser;
 use Anomaly\Streams\Platform\Support\Template;
+use Anomaly\Streams\Platform\Support\Value;
 
 if (!function_exists('parse')) {
 
@@ -30,5 +31,22 @@ if (!function_exists('render')) {
     function render($template, array $payload = [])
     {
         return app(Template::class)->render($template, $payload);
+    }
+}
+
+if (!function_exists('valuate')) {
+
+    /**
+     * Make a valuation.
+     *
+     * @param $parameters
+     * @param $entry
+     * @param string $term
+     * @param array $payload
+     * @return mixed
+     */
+    function valuate($parameters, $entry, $term = 'entry', $payload = [])
+    {
+        return app(Value::class)->make($parameters, $entry, $term, $payload);
     }
 }
