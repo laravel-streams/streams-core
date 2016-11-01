bc62e53265dade91fb61368590e197d69ce05906
Juha Remes <jremes@outlook.com>
2016-11-01 14:28:09 +0000

    Fixed another syntax error

--- resources/lang/zh-cn/installer.php
+++ resources/lang/zh-cn/installer.php
@@ -6,6 +6,6 @@ return [
     'running_application_migrations' => '正在迁移应用内容.',
     'running_migrations'             => '正在迁移其他内容.',
     'running_seeds'                  => '正在运行项目的数据种子。',
-    'installing'                     => '正在安装: :installing'
+    'installing'                     => '正在安装: :installing',
     'seeding'                        => '正在处理数据种子: :seeding'
 ];
