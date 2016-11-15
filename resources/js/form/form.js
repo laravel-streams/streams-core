fca416b2098e795ecacd6412b2cf237a609b941a
Denis Efremov <efremov.a.denis@gmail.com>
2016-11-15 23:46:11 +0300

    Form "ctrl+s" and "ctrl+shift+s" handling
    "ctrl+s" - fire first action
    "ctrl+shift+s" - fire second action

--- resources/js/form/form.js
+++ resources/js/form/form.js
@@ -2,4 +2,17 @@ $(function () {
 
     // Focus on the first input.
     $('form.form').find('input:visible').first().focus();
+    
+    // Save entry on "ctrl + s" and "ctrl + shift + s" shortcuts
+    var actions = document.querySelector('form.form').elements.action;
+    window.addEventListener('keydown', function(ev) {
+        if (ev.altKey === false && ev.ctrlKey === true && ev.keyCode === 83){
+            var i = 0;
+            if (ev.key === 'S') { // shift
+                i = 1;
+            }
+            ev.preventDefault();
+            actions.item(i).click();
+        }
+    });
 });
