030c08f41b9af5e365bbcc5b9ebadc612587b7d0
Denis Efremov <efremov.a.denis@gmail.com>
2016-11-16 04:00:54 +0300

    Fixed `caps-lock` behavior.
    The shortest function I could find.

--- resources/js/form/form.js
+++ resources/js/form/form.js
@@ -5,13 +5,50 @@ $(function () {
     
     // Save entry on "ctrl + s" and "ctrl + shift + s" shortcuts
     var actions = document.querySelector('form.form').elements.action;
+
+    var isCapslock = function(e) {
+        e = (e) ? e : window.event;
+
+        var charCode = false;
+
+        if (e.which) {
+            charCode = e.which;
+        } else if (e.keyCode) {
+            charCode = e.keyCode;
+        }
+
+        var shifton = false;
+
+        if (e.shiftKey) {
+            shifton = e.shiftKey;
+        } else if (e.modifiers) {
+            shifton = !!(e.modifiers & 4);
+        }
+
+        if (charCode >= 97 && charCode <= 122 && shifton) {
+            return true;
+        }
+
+        if (charCode >= 65 && charCode <= 90 && !shifton) {
+            return true;
+        }
+
+        return false;
+    };
+
     window.addEventListener('keydown', function(ev) {
+        
         if (ev.altKey === false && ev.ctrlKey === true && ev.keyCode === 83){
+
             var i = 0;
-            if (ev.key === 'S') { // shift
+
+            if ((!isCapslock(ev) && ev.key === 'S') ||
+                (isCapslock(ev) && ev.key === 's')) {
                 i = 1;
             }
+
             ev.preventDefault();
+        
             actions.item(i).click();
         }
     });
