b6e92fe4e219469bf5d5181ec483fb6309174c60
Ryan Thompson <service@aiwebsystems.com>
2016-11-16 09:57:03 -0600

    Fix some base migrations we do for laravel.

--- migrations/application/2015_03_15_171508_create_sessions_table.php
+++ migrations/application/2015_03_15_171508_create_sessions_table.php
@@ -3,6 +3,13 @@
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
+/**
+ * Class CreateSessionsTable
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
 class CreateSessionsTable extends Migration
 {
 
@@ -17,6 +24,9 @@ class CreateSessionsTable extends Migration
             'sessions',
             function (Blueprint $table) {
                 $table->string('id')->unique();
+                $table->integer('user_id')->nullable();
+                $table->string('ip_address', 45)->nullable();
+                $table->text('user_agent')->nullable();
                 $table->text('payload');
                 $table->integer('last_activity');
             }
