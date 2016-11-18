31f684be6b47904f0a74d5572ae4cecf62e7cacb
Ryan Thompson <service@aiwebsystems.com>
2016-11-17 23:26:36 -0600

    Correct resources view path

--- src/View/Command/AddViewNamespaces.php
+++ src/View/Command/AddViewNamespaces.php
@@ -17,15 +17,15 @@ class AddViewNamespaces
      * Handle the command.
      *
      * @param Application $application
-     * @param Factory $views
+     * @param Factory     $views
      */
     public function handle(Application $application, Factory $views)
     {
         $views->composer('*', 'Anomaly\Streams\Platform\View\ViewComposer');
         $views->addNamespace('streams', __DIR__ . '/../../../resources/views');
+        $views->addNamespace('app', $application->getResourcesPath('views'));
         $views->addNamespace('resources', base_path('resources/views'));
         $views->addNamespace('storage', $application->getStoragePath());
-        $views->addNamespace('app', $application->getResourcesPath());
         $views->addNamespace('root', base_path());
         $views->addExtension('html', 'php');
     }
