63a0409f4f907c3d7888d7d39b77e072232946ae
Ryan Thompson <service@aiwebsystems.com>
2016-11-26 01:13:49 -0600

    Cleaner

--- resources/js/form/keyboard.js
+++ resources/js/form/keyboard.js
@@ -1,28 +1,25 @@
 $(function () {
 
-    /**
-     * Listen for ctrl/meta + S (with optional
-     * shift) in order to control the form
-     * with the first / second action.
-     */
     $(window).bind('keydown', function (e) {
 
-        if (!e.ctrlKey && !e.metaKey) {
-            return true;
+        /**
+         * Listen for ctrl/meta + S (with optional
+         * shift) in order to control the form
+         * with the first / second action.
+         */
+        if ((e.ctrlKey || e.metaKey) && String.fromCharCode(e.which).toLowerCase() == 's') {
+
+            var actions = $('form.form .actions').find('button');
+
+            if (!e.shiftKey) {
+                actions.eq(0).click();
+            } else {
+                actions.eq(1).click();
+            }
+
+            return false;
         }
 
-        if (String.fromCharCode(e.which).toLowerCase() !== 's') {
-            return true;
-        }
-
-        var actions = $('form.form .actions').find('button');
-
-        if (!e.shiftKey) {
-            actions.eq(0).click();
-        } else {
-            actions.eq(1).click();
-        }
-
-        return false;
+        return true;
     });
 });
