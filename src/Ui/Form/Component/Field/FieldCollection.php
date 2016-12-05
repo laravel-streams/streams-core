b99d737234e156a02861df740c29878c102fb8d1
Ryan Thompson <ryan@pyrocms.com>
2016-12-05 14:58:22 -0600

    This should be using the set default locale instead of the fallback.

--- src/Ui/Form/Component/Field/FieldCollection.php
+++ src/Ui/Form/Component/Field/FieldCollection.php
@@ -23,7 +23,7 @@ class FieldCollection extends Collection
     {
         $fields = [];
 
-        $locale = config('app.fallback_locale');
+        $locale = config('streams::locales.default');
 
         /* @var FieldType $item */
         foreach ($this->items as $item) {
