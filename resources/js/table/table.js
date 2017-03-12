0d404c7028fd5a28f8c941eb7cdb9c49ad5b58ed
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-12 18:05:32 +0300

    Added clinging of `thead` to the top during scroll

--- resources/js/table/table.js
+++ resources/js/table/table.js
@@ -2,4 +2,50 @@ $(function () {
 
     // Focus on the first filter input.
     $('#filters').find('input:visible').first().focus();
+      
+    var
+        topBar = document.getElementById('topbar'),
+        topBarHeight = topBar.offsetHeight,
+        table = document.querySelector('table.table'),
+        tableWidth = table.offsetWidth,
+        tableTop = table.offsetParent.offsetTop,
+        thead = table.children.item(0),
+        tbody = table.children.item(1),
+        getTableColsSizes = function () {
+            var output = [];
+            tbody.children.item(0).childNodes.forEach(function (el) {
+                if (el.tagName === 'TD') {
+                    output.push(el.offsetWidth);
+                }
+            });
+            return output;
+        },
+        tableColsSizes = getTableColsSizes(),
+        scrollTop;
+    
+    thead.style.backgroundColor = '#fff';
+    thead.style.width = tableWidth + 'px';
+
+    window.addEventListener('scroll', function (event) {
+        var idx = 0;
+        scrollTop = event.target.body.scrollTop;
+
+        thead.children.item(0).childNodes.forEach(function (el) {
+            if (el.tagName === 'TH') {
+                console.log(idx);
+                el.width = tableColsSizes[idx] + 'px';
+                idx++;
+            }
+        });
+
+        if (scrollTop > tableTop - topBarHeight) {
+            thead.style.position = 'fixed';
+            thead.style.top = topBarHeight + 'px';
+            table.style.marginTop = thead.offsetHeight + 'px';
+        } else {
+            thead.style.position = 'relative';
+            thead.style.top = '0px';
+            table.style.marginTop = '0';
+        }
+    });
 });
