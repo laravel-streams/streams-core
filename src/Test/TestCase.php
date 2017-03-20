e82b5bc172efba1b1daddeae5d7292acf79e15d9
Ryan Thompson <ryan@pyrocms.com>
2017-03-20 02:04:35 -0500

    Base URL by default

--- src/Test/TestCase.php
+++ src/Test/TestCase.php
@@ -13,6 +13,13 @@ abstract class TestCase extends \Illuminate\Foundation\Testing\TestCase
 {
 
     /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
      * Creates the application.
      *
      * Needs to be implemented by subclasses.
