9faa301193fe42d25bd0ee17a1689d2bd611aff5
Ryan Thompson <ryan@pyrocms.com>
2017-03-26 11:24:41 -0500

    Fix issue where key value field value columns were not assuming fields which is really the only way this makes sense

--- src/Ui/Table/Component/Header/HeaderNormalizer.php
+++ src/Ui/Table/Component/Header/HeaderNormalizer.php
@@ -42,8 +42,8 @@ class HeaderNormalizer
              */
             if (!is_numeric($key) && !is_array($column) && !class_exists($column)) {
                 $column = [
-                    'heading' => $key,
-                    'value'   => $column,
+                    'field' => $key,
+                    'value' => $column,
                 ];
             }
 
