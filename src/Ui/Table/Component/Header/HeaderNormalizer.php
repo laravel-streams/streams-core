df7683835b30ef9dce76314185b546045d0cbefc
Ryan Thompson <ryan@pyrocms.com>
2017-03-28 23:37:59 -0500

    Fix issue with value / field normalization

--- src/Ui/Table/Component/Header/HeaderNormalizer.php
+++ src/Ui/Table/Component/Header/HeaderNormalizer.php
@@ -42,8 +42,8 @@ class HeaderNormalizer
              */
             if (!is_numeric($key) && !is_array($column) && !class_exists($column)) {
                 $column = [
-                    'field' => $key,
-                    'value' => $column,
+                    'value'   => $key,
+                    'heading' => $column,
                 ];
             }
 
