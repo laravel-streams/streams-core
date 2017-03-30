eb7984cba54f294354dcdf7e07551b85b635d70d
Denis Efremov <efremov.a.denis@gmail.com>
2017-03-30 11:46:29 +0300

    Added humanizing file sizes.

--- src/Support/_helpers.php
+++ src/Support/_helpers.php
@@ -133,3 +133,20 @@ if (!function_exists('data')) {
         return $target;
     }
 }
+
+if (!function_exists('human_filesize')) {
+
+    /**
+     * Humanize the filesizes
+     *
+     * @param      integer  $bytes     The bytes
+     * @param      integer  $decimals  The decimals
+     * @return     string
+     */
+    function human_filesize($bytes, $decimals = 2)
+    {
+        $size   = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)).@$size[$factor];
+    }
+}
