4985b5442fb81e48719ab3fdf9b3d3371007119d
Ryan Thompson <ryan@pyrocms.com>
2017-04-18 11:16:28 -0500

    Add resources asset namespace.

--- src/Asset/Command/AddAssetNamespaces.php
+++ src/Asset/Command/AddAssetNamespaces.php
@@ -25,6 +25,7 @@ class AddAssetNamespaces
         $asset->addPath('node', base_path('node_modules'));
         $asset->addPath('asset', $application->getAssetsPath());
         $asset->addPath('storage', $application->getStoragePath());
+        $asset->addPath('resources', $application->getResourcesPath());
         $asset->addPath('download', $application->getAssetsPath('assets/downloads'));
         $asset->addPath('streams', $container->make('streams.path') . '/resources');
         $asset->addPath('bower', $container->make('path.base') . '/bower_components');
