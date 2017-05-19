6caac1cc8c268d4e834277336c7163a64a8a7b92
Benjamin Horn <git@benjaminhorn.se>
2017-05-19 21:36:35 +0300

    Updated Swedish translations

--- resources/lang/sv/label.php
+++ resources/lang/sv/label.php
@@ -1,9 +1,20 @@
 <?php
 
 return [
+    'control_panel' => 'Kontrollpanel',
     'configuration' => 'Konfiguration',
     'localization'  => 'Lokalisering',
+    'maintenance'   => 'Underhåll',
+    'appearance'    => 'Utseende',
+    'standards'     => 'Standarder',
     'datetime'      => 'Datumtid',
+    'advanced'      => 'Avancerat',
+    'services'      => 'Tjänster',
+    'formats'       => 'Format',
+    'details'       => 'Detaljer',
+    'display'       => 'Visa',
+    'contact'       => 'Kontakt',
+    'system'        => 'System',
     'general'       => 'Allmänt',
     'themes'        => 'Teman',
     'access'        => 'Åtkomst',
