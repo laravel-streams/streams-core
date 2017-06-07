5a09678ad200ddba2f8491594dfe198ac57d5763
Denis Efremov <efremov.a.denis@gmail.com>
2017-06-07 15:35:24 +0300

    Update Make.php

--- src/Stream/Console/Make.php
+++ src/Stream/Console/Make.php
@@ -2,6 +2,7 @@
 
 use Anomaly\Streams\Platform\Addon\Addon;
 use Anomaly\Streams\Platform\Addon\AddonCollection;
+use Anomaly\Streams\Platform\Stream\Console\Command\UpdateAddonProvider;
 use Anomaly\Streams\Platform\Stream\Console\Command\WriteEntityCollection;
 use Anomaly\Streams\Platform\Stream\Console\Command\WriteEntityController;
 use Anomaly\Streams\Platform\Stream\Console\Command\WriteEntityCriteria;
@@ -81,6 +82,8 @@ class Make extends Command
         $this->dispatch(new WriteEntityTableBuilder($addon, $slug, $namespace));
         $this->dispatch(new WriteEntityModelInterface($addon, $slug, $namespace));
         $this->dispatch(new WriteEntityRepositoryInterface($addon, $slug, $namespace));
+        
+        $this->dispatch(new UpdateAddonProvider($addon, $slug, $namespace));
 
         // Run this last since it scans the above.
         $this->dispatch(new WriteEntityTestCases($addon, $slug, $namespace));
