04fe1915859a0cc3829e1ac91e8aeadc48e2cfaf
Denis Efremov <efremov.a.denis@gmail.com>
2017-06-14 19:10:21 +0300

    Refactored some part

--- resources/js/modal/filter.js
+++ resources/js/modal/filter.js
@@ -1,150 +1,140 @@
-var modal = $('.modal.remote.in');
-var form = modal.find('form.modal-filter');
-var input = form.find('input');
-var list = modal.find('ul');
-var items = list.find('li');
-var selected = null;
+$(function () {
 
-// Don't submit on return.
-form.on('submit', function () {
-    return false;
-});
+    var $modal = $('.modal.remote.in');
+    var $form = $modal.find('form.modal-filter');
+    var $input = $form.find('input').focus();
+    var $items = $modal.find('ul li');
+    var $links = $items.find('a');
+    var $selected = null;
 
-input.focus();
+    // Don't submit on return.
+    $form.on('submit', function () {
+        return false;
+    });
 
-input.on('keydown', function (e) {
+    // Bind keyboard press event
+    $input.on('keydown', function (e) {
 
-    /**
-     * Capture the down arrow.
-     */
-    if (e.which == 40) {
+        /**
+         * Capture the enter key.
+         */
+        if (e.which === 13) {
 
-        if (selected) {
+            if (!$selected || !$selected.length) {
 
-            /**
-             * If we have a selection then
-             * push to the next visible option.
-             */
-            if (selected.nextAll(':visible').length) {
-                items.find('a').removeClass('active');
-                selected = selected.nextAll(':visible').first();
-                selected.find('a').addClass('active');
+                /**
+                 * If nothing is selected
+                 * there's nothing to do.
+                 */
+                return false;
             }
-        } else {
 
-            /**
-             * Otherwise select the first
-             * visible option in the list.
-             */
-            selected = items.filter(':visible').first();
-            selected.find('a').addClass('active');
-        }
-    }
-
-    /**
-     * Capture the up arrow.
-     */
-    if (e.which == 38) {
-
-        if (selected) {
-
-            /**
-             * If we have a selection then push
-             * to the previous visible option.
-             */
-            if (selected.prevAll(':visible').length) {
-                items.find('a').removeClass('active');
-                selected = selected.prevAll(':visible').first();
-                selected.find('a').addClass('active');
-            }
-        } else {
-
-            /**
-             * Otherwise select the last
-             * visible option in the list.
-             */
-            selected = items.filter(':visible').last();
-            selected.find('a').addClass('active');
-        }
-    }
-
-    /**
-     * Capture the enter key.
-     */
-    if (e.which == 13) {
-
-        if (selected) {
+            var $selectedLinks = $selected.find('a');
 
             /**
              * If the key press was the return
              * key and we have a selection
              * then follow the link.
              */
-            if (selected.find('a').hasClass('has-click-event') || selected.find('a').hasClass('ajax')) {
-                selected.find('a').trigger('click');
+            if ($selectedLinks.hasClass('has-click-event')
+                || $selectedLinks.hasClass('ajax')) {
+                $selectedLinks.trigger('click');
+            } else {
+                window.location = $selectedLinks.attr('href');
+
+                $modal.find('.modal-content').append('\
+<div class="modal-loading">\
+    <div class="active large loader"></div>\
+</div>\
+                ');
+            }
+        }
+
+        /**
+         * Capture up and down arrows.
+         */
+        if (e.which === 38 || e.which === 40) {
+
+            e.preventDefault();
+
+            if ($selected) {
+
+                /**
+                 * If we have a selection then push
+                 * to the previous or next visible option.
+                 */
+                if ($selected.prevAll(':visible').length && e.which === 38) {
+                    $links.removeClass('active');
+                    $selected = $selected.prevAll(':visible').first();
+                    $selected.find('a').addClass('active');
+                }
+
+                if ($selected.nextAll(':visible').length && e.which === 40) {
+                    $links.removeClass('active');
+                    $selected = $selected.nextAll(':visible').first();
+                    $selected.find('a').addClass('active');
+                }
             } else {
 
                 /**
-                 * If nothing is selected
-                 * there's nothing to do.
+                 * Otherwise select the first or the last
+                 * visible options in the list.
                  */
-                if (!selected.length) {
-                    return false;
+                if (e.which === 38) {
+                    $selected = $items.filter(':visible').first();
                 }
 
-                window.location = selected.find('a').attr('href');
+                if (e.which === 40) {
+                    $selected = $items.filter(':visible').last();
+                }
 
-                modal.find('.modal-content').append('<div class="modal-loading"><div class="active large loader"></div></div>');
+                $selected.find('a').addClass('active');
             }
-        }
-    }
 
-    /**
-     * Capture up and down arrows.
-     */
-    if (e.which == 38 || e.which == 40) {
+            // store current positions in variables
+            var start = $input[0].selectionStart;
+            var end = $input[0].selectionEnd;
 
-        // store current positions in variables
-        var start = input[0].selectionStart,
-            end = input[0].selectionEnd;
-
-        // restore from variables...
-        input[0].setSelectionRange(start, end);
-
-        e.preventDefault();
-    }
-});
-
-input.on('keyup', function (e) {
-
-    /**
-     * If the keyup was a an arrow
-     * up or down then skip this step.
-     */
-    if (e.which == 38 || e.which == 40) {
-        return;
-    }
-
-    var value = $(this).val();
-
-    /**
-     * Filter the list by the items to
-     * show only those containing value.
-     */
-    items.each(function () {
-        if ($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-            $(this).show();
-        } else {
-            $(this).hide();
+            // restore from variables...
+            $input[0].setSelectionRange(start, end);
         }
     });
 
-    /**
-     * If we don't have a selected item
-     * then choose the first visible option.
-     */
-    if (!selected || !selected.is(':visible')) {
-        selected = items.filter(':visible').first();
-        selected.find('a').addClass('active');
-    }
+    $input.on('keyup', function (e) {
+
+        /**
+         * If the keyup was a an arrow
+         * up or down then skip this step.
+         */
+        if (e.which === 38 || e.which === 40) {
+            return;
+        }
+
+        var value = $(this).val();
+
+        /**
+         * Filter the list by the items to
+         * show only those containing value.
+         */
+        $items.each(function () {
+
+            var $this = $(this);
+
+            if ($this.text().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                $this.show();
+            } else {
+                $this.hide();
+            }
+        });
+
+        /**
+         * If we don't have a selected item
+         * then choose the first visible option.
+         */
+        if (!$selected || !$selected.is(':visible')) {
+            $selected = $items.filter(':visible').first();
+            $selected.find('a').addClass('active');
+        }
+    });
 });
+
