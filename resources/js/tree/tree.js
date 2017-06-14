00e00c77fc6ceb1f33aae98b8d272f4e03b4c8f0
Denis Efremov <efremov.a.denis@gmail.com>
2017-06-14 19:12:44 +0300

    Comments and formatting

--- resources/js/tree/tree.js
+++ resources/js/tree/tree.js
@@ -2,9 +2,19 @@ $(function () {
 
     var adjustment;
 
-    var tree = $('ul.tree.sortable').sortable({
+    var $tree = $('ul.tree.sortable').sortable({
         handle: '.handle',
+
+        /**
+         * Fires when drag just started
+         *
+         * @param      {$}          $item      The item
+         * @param      {Object}     container  The container
+         * @param      {Function}   _super     The super
+         * @param      {Event}      event      The event
+         */
         onDragStart: function ($item, container, _super, event) {
+
             $item.css({
                 height: $item.outerHeight(),
                 width: $item.outerWidth()
@@ -21,28 +31,56 @@ $(function () {
 
             _super($item, container);
         },
+
+        /**
+         * Fires when drag is continuing
+         *
+         * @param      {$}        $item     The item
+         * @param      {Object}   position  The position
+         */
         onDrag: function ($item, position) {
             $item.css({
                 left: position.left - adjustment.left,
                 top: position.top - adjustment.top
             });
         },
+
+        /**
+         * Fires when drop was made
+         *
+         * @param      {$}          $item      The item
+         * @param      {Object}     container  The container
+         * @param      {Function}   _super     The super
+         * @param      {Event}      event      The event
+         */
         onDrop: function ($item, container, _super, event) {
 
             $.post(window.location.href, {
-                items: tree.sortable('serialize').get()[0] // This needs to return [0] for some reason..
+                // This needs to return [0] for some reason..
+                items: tree.sortable('serialize').get()[0]
             });
 
             _super($item, container);
         },
+
+        /**
+         * Serialize data
+         *
+         * @param      {$}          $parent            The parent
+         * @param      {$}          $children          The children
+         * @param      {Boolean}    parentIsContainer  The parent is container
+         *
+         * @return     {Array}
+         */
         serialize: function ($parent, $children, parentIsContainer) {
 
             var result = $.extend({}, $parent.data());
 
-            if (parentIsContainer)
+            if (parentIsContainer) {
                 return [$children];
-            else if ($children[0]) {
-                result.children = $children[0]; // This needs to return [0] for some reason..
+            } else if ($children[0]) {
+                // This needs to return [0] for some reason..
+                result.children = $children[0];
             }
 
             delete result.subContainers;
@@ -55,18 +93,16 @@ $(function () {
     /**
      * Hide initial items.
      */
-    tree.find('li').each(function () {
+    $tree.find('li').each(function () {
 
-        var item = $(this);
-        var collapsed = Cookies.getJSON('tree');
-        var count = item.find('li').length;
+        var $this = $(this);
+        var $li = $this.find('li');
+        var count = $li.length;
+        var collapsed = Cookies.getJSON('tree') || {};
 
-        if (typeof collapsed == 'undefined') {
-            collapsed = {};
-        }
-
-        if (collapsed[$(this).data('id')] == true && $(this).find('li').length) {
-            $(this).addClass('collapsed').find('.card').first().append('<div class="count">' + count + '</div>');
+        if (collapsed[$this.data('id')] === true && count) {
+            $this.addClass('collapsed').find('.card').first()
+                .append('<div class="count">' + count + '</div>');
         }
     });
 
@@ -77,28 +113,33 @@ $(function () {
 
         e.preventDefault();
 
-        var item = $(this).closest('li');
+        var $item = $(this).closest('li');
         var collapsed = Cookies.getJSON('tree');
-        var count = item.find('li').length;
+        var count = $item.find('li').length;
 
         if (!count) {
             return;
         }
 
-        item.toggleClass('collapsed');
+        $item.toggleClass('collapsed');
 
-        if (typeof collapsed == 'undefined') {
+        if (typeof collapsed === 'undefined') {
             collapsed = {};
         }
 
-        collapsed[item.data('id')] = item.hasClass('collapsed');
+        collapsed[$item.data('id')] = $item.hasClass('collapsed');
 
-        if (item.hasClass('collapsed')) {
-            item.find('.card').first().append('<div class="count">' + count + '</div>');
+        if ($item.hasClass('collapsed')) {
+            $item.find('.card').first()
+                .append('<div class="count">' + count + '</div>');
         } else {
-            item.find('.count').first().remove();
+            $item.find('.count').first().remove();
         }
 
-        Cookies.set('tree', JSON.stringify(collapsed), {path: window.location.pathname});
+        Cookies.set(
+            'tree',
+            JSON.stringify(collapsed),
+            { path: window.location.pathname }
+        );
     });
 });
