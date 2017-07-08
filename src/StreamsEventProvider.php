2059b9da4d526dbd4635384ca605416fd2df9d5d
Ryan Thompson <ryan@pyrocms.com>
2017-07-08 01:58:37 -0500

    This is not the platform's responsibility. Moved and improved to installer-module

--- src/StreamsEventProvider.php
+++ src/StreamsEventProvider.php
@@ -1,6 +1,5 @@
 <?php namespace Anomaly\Streams\Platform;
 
-use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
 /**
@@ -21,7 +20,6 @@ class StreamsEventProvider extends EventServiceProvider
     protected $listen = [
         'Anomaly\Streams\Platform\Application\Event\ApplicationHasLoaded' => [
             'Anomaly\Streams\Platform\Addon\Module\Listener\DetectActiveModule',
-            'Anomaly\Streams\Platform\Application\Listener\CheckIfInstallerExists',
             'Anomaly\Streams\Platform\Ui\ControlPanel\Listener\LoadControlPanel',
             'Anomaly\Streams\Platform\Ui\Breadcrumb\Listener\GuessBreadcrumbs',
             'Anomaly\Streams\Platform\Ui\Breadcrumb\Listener\LoadBreadcrumbs',
