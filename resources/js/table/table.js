50273c18bf0da45e72b3b4252f2e501323a3b380
Denis Efremov <efremov.a.denis@gmail.com>
2017-08-23 05:29:08 +0300

    Hold shift to select checkmarks between clicks for selecting rows

--- resources/js/table/table.js
+++ resources/js/table/table.js
@@ -3,3 +3,39 @@ $(function () {
     // Focus on the first filter input.
     $('#filters').find('input:visible').first().focus();
 });
+
+let cpTable = new Vue({
+    el: 'table.table',
+    name: 'cpTable',
+
+    data: {
+        selected: [],
+    },
+
+    methods: {
+        rowClick (e, id) {
+            if (e.shiftKey) {
+                this.selectInterval(this.selected[0], id);
+                return;
+            }
+            this.selectOne(id);
+        },
+
+        selectInterval (start, end) {
+            let fromId = start < end ? start : end;
+            let toId = start < end ? end : start;
+            this.selected = [];
+            for (let i = fromId; i <= toId; i++) {
+                this.selected.push(i);
+            }
+        },
+
+        selectOne (id) {
+            this.selected = this.isSelected(id) ? [] : [id];
+        },
+
+        isSelected (id) {
+            return this.selected.includes(id);
+        }
+    },
+});
