668d7e8611dc6ee2185e267df0ab7ddd266539c7
Ryan Thompson <ryan@pyrocms.com>
2017-08-28 11:14:28 -0500

    Image fix for non-alterated images.

--- src/Image/Image.php
+++ src/Image/Image.php
@@ -458,7 +458,9 @@ class Image
      */
     public function encode($format = null, $quality = null)
     {
-        return $this->manager->make($this->getCachePath())->encode($format, $quality ?: $this->getQuality());
+        $this->setQuality($quality ?: $this->config->get('streams::images.quality', 80));
+
+        return $this->manager->make($this->getCachePath())->encode($format, $this->getQuality());
     }
 
     /**
@@ -677,7 +679,7 @@ class Image
             }
         }
 
-        $image->save($path, $this->getQuality());
+        $image->save($path, $this->getQuality() ?: $this->config->get('streams::images.quality', 80));
     }
 
     /**
@@ -1160,16 +1162,11 @@ class Image
     /**
      * Get the quality.
      *
-     * @param  null $default
-     * @return int
+     * @return int|null
      */
-    public function getQuality($default = null)
+    public function getQuality()
     {
-        if (!$default) {
-            $default = $this->config->get('streams::images.quality', 80);
-        }
-
-        return $this->quality ?: $default;
+        return $this->quality;
     }
 
     /**
