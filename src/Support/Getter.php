29497abc3116f554dba75dde739d3cef4d7b8245
Denis Efremov <efremov.a.denis@gmail.com>
2017-09-19 08:48:51 +0300

    Update Getter.php

--- src/Support/Getter.php
+++ src/Support/Getter.php
@@ -41,18 +41,66 @@ class Getter
      */
     public function get()
     {
+        return ($object = $this->checkId())
+            ? $object
+            : ($object = $this->checkSlug())
+                ? $object
+                : ($object = $this->checkPresenter())
+                    ? $object
+                    : ($object = $this->checkObject())
+                        ? $object
+                        : null;
+    }
+
+    /**
+     * Checks the id
+     *
+     * @return     EloquentInterface|null
+     */
+    private function checkId()
+    {
         if (is_numeric($this->identifier)) {
             return $this->repository->find($this->identifier);
         }
 
+        return null;
+    }
+
+    /**
+     * Checks the slug
+     *
+     * @return     EloquentInterface|null
+     */
+    private function checkSlug()
+    {
         if (is_string($this->identifier) && method_exists($this->repository, 'findBySlug')) {
             return $this->repository->findBySlug($this->identifier);
         }
 
+        return null;
+    }
+
+    /**
+     * Checks the presenter
+     *
+     * @return     EloquentInterface|null
+     */
+    private function checkPresenter()
+    {
         if ($this->identifier instanceof Presenter) {
             return $this->identifier->getObject();
         }
 
+        return null;
+    }
+
+    /**
+     * Checks the object
+     *
+     * @return     EloquentInterface|null
+     */
+    private function checkObject()
+    {
         if (is_object($this->identifier)) {
             return $this->identifier;
         }
