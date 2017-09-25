e81f62fc249654deaf8cb469e66b1c721595a239
Denis Efremov <efremov.a.denis@gmail.com>
2017-09-25 14:37:25 +0300

    Fixed static class

--- src/Database/DatabaseServiceProvider.php
+++ src/Database/DatabaseServiceProvider.php
@@ -1,5 +1,6 @@
 <?php namespace Anomaly\Streams\Platform\Database;
 
+use Anomaly\Streams\Platform\Database\Migration\MigrationServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
 class DatabaseServiceProvider extends ServiceProvider
@@ -13,6 +14,6 @@ class DatabaseServiceProvider extends ServiceProvider
     public function register()
     {
         //$this->app->register('Anomaly\Streams\Platform\Database\Seeder\SeederServiceProvider');
-        $this->app->register('Anomaly\Streams\Platform\Database\Migration\MigrationServiceProvider');
+        $this->app->register(MigrationServiceProvider::class);
     }
 }
