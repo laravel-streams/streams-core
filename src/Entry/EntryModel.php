410b9df7f8b3195cffde221a89b2f007651470f0
Ryan Thompson <service@aiwebsystems.com>
2017-11-29 12:38:23 -0600

    Catch non-translations

--- src/Entry/EntryModel.php
+++ src/Entry/EntryModel.php
@@ -8,7 +8,6 @@ use Anomaly\Streams\Platform\Assignment\Contract\AssignmentInterface;
 use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Anomaly\Streams\Platform\Field\Contract\FieldInterface;
 use Anomaly\Streams\Platform\Model\EloquentModel;
-use Anomaly\Streams\Platform\Model\Traits\Translatable;
 use Anomaly\Streams\Platform\Stream\Contract\StreamInterface;
 use Anomaly\Streams\Platform\Stream\StreamModel;
 use Carbon\Carbon;
@@ -105,7 +104,7 @@ class EntryModel extends EloquentModel implements EntryInterface, PresentableInt
      * Sort the query.
      *
      * @param Builder $builder
-     * @param string  $direction
+     * @param string $direction
      */
     public function scopeSorted(Builder $builder, $direction = 'asc')
     {
@@ -190,7 +189,7 @@ class EntryModel extends EloquentModel implements EntryInterface, PresentableInt
      * Get a field value.
      *
      * @param        $fieldSlug
-     * @param  null  $locale
+     * @param  null $locale
      * @return mixed
      */
     public function getFieldValue($fieldSlug, $locale = null)
@@ -228,7 +227,8 @@ class EntryModel extends EloquentModel implements EntryInterface, PresentableInt
         if (
             !$type->hasValue() &&
             $assignment->isTranslatable() &&
-            $translation = $this->translateOrDefault()
+            ($translation = $this->translateOrDefault()) &&
+            $translation instanceof EntryTranslationsModel
         ) {
             $type->setValue($value);
             $type->setEntry($translation);
@@ -245,7 +245,7 @@ class EntryModel extends EloquentModel implements EntryInterface, PresentableInt
      *
      * @param        $fieldSlug
      * @param        $value
-     * @param  null  $locale
+     * @param  null $locale
      * @return $this
      */
     public function setFieldValue($fieldSlug, $value, $locale = null)
@@ -375,7 +375,7 @@ class EntryModel extends EloquentModel implements EntryInterface, PresentableInt
      * the field types a chance to modify things.
      *
      * @param  string $key
-     * @param  mixed  $value
+     * @param  mixed $value
      * @return EntryModel|EloquentModel
      */
     public function setAttribute($key, $value)
@@ -413,7 +413,7 @@ class EntryModel extends EloquentModel implements EntryInterface, PresentableInt
      * Get a raw unmodified attribute.
      *
      * @param             $key
-     * @param  bool       $process
+     * @param  bool $process
      * @return mixed|null
      */
     public function getRawAttribute($key, $process = true)
