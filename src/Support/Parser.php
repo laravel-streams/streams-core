d58db69c865c8c51e21391623c00997610b6a268
Ryan Thompson <ryan@pyrocms.com>
2017-12-13 13:56:51 -0600

    More request information for the parser

--- src/Support/Parser.php
+++ src/Support/Parser.php
@@ -40,8 +40,8 @@ class Parser
      * Create a new Parser instance.
      *
      * @param UrlGenerator $url
-     * @param Engine $parser
-     * @param Request $request
+     * @param Engine       $parser
+     * @param Request      $request
      */
     public function __construct(UrlGenerator $url, Engine $parser, Request $request)
     {
@@ -144,10 +144,13 @@ class Parser
     protected function requestData()
     {
         $request = [
-            'path'  => $this->request->path(),
-            'input' => $this->request->input(),
-            'uri'   => $this->request->getRequestUri(),
-            'query' => $this->request->getQueryString(),
+            'url'      => $this->request->url(),
+            'path'     => $this->request->path(),
+            'root'     => $this->request->root(),
+            'input'    => $this->request->input(),
+            'full_url' => $this->request->fullUrl(),
+            'uri'      => $this->request->getRequestUri(),
+            'query'    => $this->request->getQueryString(),
         ];
 
         if ($route = $this->request->route()) {
