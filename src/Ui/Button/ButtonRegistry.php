601636c013b7c94cd7549079444beb237b08b40e
Ryan Thompson <ryan@pyrocms.com>
2018-02-05 08:39:06 -0600

    Break out HTML from messages and use title/message combination.

--- src/Ui/Button/ButtonRegistry.php
+++ src/Ui/Button/ButtonRegistry.php
@@ -228,7 +228,9 @@ class ButtonRegistry
             'text'       => 'streams::button.delete',
             'attributes' => [
                 'data-toggle'  => 'confirm',
-                'data-message' => 'streams::message.confirm_delete',
+                'data-icon'    => 'warning',
+                'data-title'   => 'streams::message.confirm_delete_title',
+                'data-message' => 'streams::message.confirm_delete_message',
             ],
         ],
         'prompt'        => [
@@ -239,7 +241,9 @@ class ButtonRegistry
             'attributes' => [
                 'data-match'   => 'yes',
                 'data-toggle'  => 'prompt',
-                'data-message' => 'streams::message.prompt_delete',
+                'data-icon'    => 'warning',
+                'data-title'   => 'streams::message.prompt_delete_title',
+                'data-message' => 'streams::message.prompt_delete_message',
             ],
         ],
         'uninstall'     => [
@@ -248,7 +252,9 @@ class ButtonRegistry
             'text'       => 'streams::button.uninstall',
             'attributes' => [
                 'data-toggle'  => 'confirm',
-                'data-message' => 'streams::message.confirm_uninstall',
+                'data-icon'    => 'warning',
+                'data-title'   => 'streams::message.confirm_uninstall_title',
+                'data-message' => 'streams::message.confirm_uninstall_message',
             ],
         ],
     ];
