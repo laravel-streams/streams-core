c8b1df40b159e373d17e8dd86cc2d63d3046b5ff
Ryan Thompson <service@aiwebsystems.com>
2018-02-05 14:30:13 -0600

    Use confdig

--- src/StreamsServiceProvider.php
+++ src/StreamsServiceProvider.php
@@ -270,6 +270,14 @@ class StreamsServiceProvider extends ServiceProvider
 
                 $manager->register();
 
+                /**
+                 * Load again in case anything has
+                 * changed during registration.
+                 */
+                if (config('app.debug')) {
+                    $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+                }
+
                 $this->dispatch(new LoadCurrentTheme());
                 $this->dispatch(new AddViewNamespaces());
 
@@ -299,7 +307,7 @@ class StreamsServiceProvider extends ServiceProvider
         $this->app->register(\Intervention\Image\ImageServiceProvider::class);
         $this->app->register(\TeamTNT\Scout\TNTSearchScoutServiceProvider::class);
 
-        if (env('APP_DEBUG')) {
+        if (config('app.debug')) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
         }
 
