529cf3e5b55a30c091644c5ca530d85da4d72138
Ryan Thompson <ryan@pyrocms.com>
2018-03-07 08:16:45 -0600

    Mor

---
+++ src/Version/Table/VersionTableButtons.php
@@ -0,0 +1,33 @@
+<?php namespace Anomaly\Streams\Platform\Version\Table;
+
+use Anomaly\Streams\Platform\Ui\ControlPanel\ControlPanelBuilder;
+
+/**
+ * Class VersionTableButtons
+ *
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
+ */
+class VersionTableButtons
+{
+
+    /**
+     * Handle the command.
+     *
+     * @param VersionTableBuilder $builder
+     * @param ControlPanelBuilder $controlPanel
+     */
+    public function handle(VersionTableBuilder $builder, ControlPanelBuilder $controlPanel)
+    {
+        $section = $controlPanel->getControlPanelActiveSection();
+
+        $builder->setButtons(
+            [
+                'edit' => [
+                    'href' => $section->getHref('edit/{entry.versionable_id}?_version={entry.version}'),
+                ],
+            ]
+        );
+    }
+}
