9be3c5302d595e4636ca54a991731bbf41bdb431
Ryan Thompson <service@aiwebsystems.com>
2018-05-09 10:04:14 -0500

    Strong

--- resources/lang/en/lock.php
+++ resources/lang/en/lock.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'locked_by_user' => 'This information is currently being edited by :username. <a href="#" class="btn btn-xs btn-white btn-outline pull-right" data-reload>Refresh</a>',
+    'locked_by_user' => 'This information is currently being edited by <strong>:username</strong>. <a href="#" class="btn btn-xs btn-white btn-outline pull-right" data-reload>Refresh</a>',
 ];
