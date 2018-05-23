1f2c63fb6c57985f7cc8818c042092df255e6f69
Ryan Thompson <ryan@pyrocms.com>
2018-05-23 08:17:29 -0500

    Email settings are back\!

--- resources/config/settings/sections.php
+++ resources/config/settings/sections.php
@@ -1,59 +1,63 @@
 <?php
 
 return [
-    'details'      => [
-        'context' => 'info',
-        'title'   => 'streams::label.details',
-        'fields'  => [
-            'name',
-            'description',
-        ],
-    ],
-    'display'      => [
-        'context' => 'info',
-        'title'   => 'streams::label.display',
-        'fields'  => [
-            'standard_theme',
-            'admin_theme',
-            'per_page',
-        ],
-    ],
-    'formats'      => [
-        'context' => 'danger',
-        'title'   => 'streams::label.formats',
-        'fields'  => [
-            'timezone',
-            'date_format',
-            'time_format',
-            'unit_system',
-            'currency',
-        ],
-    ],
-    'localization' => [
-        'context' => 'info',
-        'title'   => 'streams::label.localization',
-        'fields'  => [
-            'default_locale',
-            'enabled_locales',
-        ],
-    ],
-    'email'        => [
-        'context' => 'danger',
-        'title'   => 'streams::label.email',
-        'fields'  => [
-            'email',
-            'sender',
-        ],
-    ],
-    'maintenance'  => [
-        'context' => 'danger',
-        'title'   => 'streams::label.maintenance',
-        'fields'  => [
-            'debug',
-            'debug_bar',
-            'maintenance',
-            'basic_auth',
-            'ip_whitelist',
+    'system' => [
+        'stacked' => true,
+        'tabs'    => [
+            'details'      => [
+                'title'  => 'streams::label.details',
+                'fields' => [
+                    'name',
+                    'description',
+                ],
+            ],
+            'display'      => [
+                'title'  => 'streams::label.display',
+                'fields' => [
+                    'standard_theme',
+                    'admin_theme',
+                    'per_page',
+                ],
+            ],
+            'formats'      => [
+                'title'  => 'streams::label.formats',
+                'fields' => [
+                    'timezone',
+                    'date_format',
+                    'time_format',
+                    'unit_system',
+                    'currency',
+                ],
+            ],
+            'localization' => [
+                'title'  => 'streams::label.localization',
+                'fields' => [
+                    'default_locale',
+                    'enabled_locales',
+                ],
+            ],
+            'email'        => [
+                'title'  => 'streams::label.email',
+                'fields' => [
+                    'email',
+                    'sender',
+                    'mail_driver',
+                    'mail_host',
+                    'mail_port',
+                    'mail_username',
+                    'mail_password',
+                ],
+            ],
+            'maintenance'  => [
+                'title'  => 'streams::label.maintenance',
+                'fields' => [
+                    'debug',
+                    'debug_bar',
+                    'maintenance',
+                    'basic_auth',
+                    'ip_whitelist',
+                ],
+            ],
         ],
     ],
 ];
--- resources/config/settings/settings.php
+++ resources/config/settings/settings.php
@@ -262,4 +262,44 @@ return [
             },
         ],
     ],
+    'mail_driver'     => [
+        'env'      => 'MAIL_DRIVER',
+        'bind'     => 'mail.driver',
+        'type'     => 'anomaly.field_type.select',
+        'required' => true,
+        'config'   => [
+            'default_value' => 'mail',
+            'options'       => [
+                'smtp'     => 'streams::setting.mail_driver.option.smtp',
+                'mail'     => 'streams::setting.mail_driver.option.mail',
+                'sendmail' => 'streams::setting.mail_driver.option.sendmail',
+                'mailgun'  => 'streams::setting.mail_driver.option.mailgun',
+                'mandrill' => 'streams::setting.mail_driver.option.mandrill',
+                'log'      => 'streams::setting.mail_driver.option.log',
+            ],
+        ],
+    ],
+    'mail_host'       => [
+        'env'  => 'MAIL_HOST',
+        'bind' => 'mail.host',
+        'type' => 'anomaly.field_type.text',
+    ],
+    'mail_port'       => [
+        'env'  => 'MAIL_PORT',
+        'bind' => 'mail.port',
+        'type' => 'anomaly.field_type.integer',
+    ],
+    'mail_username'   => [
+        'env'  => 'MAIL_USERNAME',
+        'bind' => 'mail.username',
+        'type' => 'anomaly.field_type.text',
+    ],
+    'mail_password'   => [
+        'env'    => 'MAIL_PASSWORD',
+        'bind'   => 'mail.password',
+        'type'   => 'anomaly.field_type.text',
+        'config' => [
+            'type' => 'password',
+        ],
+    ],
 ];
