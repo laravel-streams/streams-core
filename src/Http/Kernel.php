77edf02428905322e9ec678e95a2ab7332ae5984
Ryan Thompson <service@aiwebsystems.com>
2018-12-11 12:38:37 -0600

    Cache requests

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends \Illuminate\Foundation\Http\Kernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \Anomaly\Streams\Platform\Http\Middleware\VerifyCsrfToken::class,
+        \Barryvdh\HttpCache\Middleware\CacheRequests::class,
         \Barryvdh\HttpCache\Middleware\ParseEsi::class,
     ];
 
