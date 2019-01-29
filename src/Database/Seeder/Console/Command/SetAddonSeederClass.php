b3ee61e3e541fd1e9ca4bc17194bf64f7fcde9d7
Martin Modler <modler@webformatik.com>
2019-01-29 09:07:06 +0100

    Undecorate addon in SetAddonSeederClass - fixes issue with block create()

--- src/Database/Seeder/Console/Command/SetAddonSeederClass.php
+++ src/Database/Seeder/Console/Command/SetAddonSeederClass.php
@@ -1,13 +1,14 @@
 <?php namespace Anomaly\Streams\Platform\Database\Seeder\Console\Command;
 
-use Anomaly\Streams\Platform\Addon\AddonCollection;
 use Anomaly\Streams\Platform\Addon\Command\GetAddon;
-use Symfony\Component\Console\Input\InputInterface;
-use Illuminate\Foundation\Bus\DispatchesJobs;
+use Anomaly\Streams\Platform\Support\Decorator;
 use Illuminate\Console\Command;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Symfony\Component\Console\Input\InputInterface;
 
 class SetAddonSeederClass
 {
+
     use DispatchesJobs;
 
     /**
@@ -27,7 +28,7 @@ class SetAddonSeederClass
     /**
      * Create a new SetAddonPath instance.
      *
-     * @param ResetCommand   $command
+     * @param Command        $command
      * @param InputInterface $input
      */
     public function __construct(Command $command, InputInterface $input)
@@ -39,17 +40,21 @@ class SetAddonSeederClass
     /**
      * Handle the command.
      *
-     * @param AddonCollection $addons
+     * @param Decorator $decorator
+     *
+     * @throws \Exception
      */
-    public function handle(AddonCollection $addons)
+    public function handle(Decorator $decorator)
     {
-        if (!$addon = $this->input->getOption('addon')) {
+        if (!$identifier = $this->input->getOption('addon')) {
             return;
         }
 
-        if (!$addon = $this->dispatch(new GetAddon($addon))) {
-            throw new \Exception("$identifier addon could not be found.");
+        if (!$addon = $this->dispatch(new GetAddon($identifier))) {
+            throw new \Exception("[$identifier] addon could not be found.");
         }
+
+        $addon = $decorator->undecorate($addon);
 
         $this->input->setOption('class', get_class($addon) . 'Seeder');
     }
