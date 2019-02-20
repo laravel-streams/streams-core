71ed655fafb1639bd36e6db46721af082aad67c4
Frédéric Vandebeuque <fred.vdb@newebtime.com>
2019-02-20 21:41:20 +0700

    Missed boot()

--- src/Entry/EntryTranslationsModel.php
+++ src/Entry/EntryTranslationsModel.php
@@ -42,9 +42,9 @@ class EntryTranslationsModel extends EloquentModel
      */
     protected static function boot()
     {
-        self::observe(app(substr(__CLASS__, 0, -5) . 'Observer'));
-
         parent::boot();
+        
+        self::observe(app(substr(__CLASS__, 0, -5) . 'Observer'));
     }
 
     /**
