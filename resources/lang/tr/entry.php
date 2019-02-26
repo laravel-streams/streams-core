43036749d15f3ad09ae8a17b702af7a4937bfcf3
fatihalp <alp@visiosoft.com.tr>
2019-02-26 20:06:12 +0300

    completely translated English to Turkish for stream platform 1.5

---
+++ resources/lang/tr/entry.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'id'         => 'ID',
+    'name'       => 'Giriş',
+    'created_at' => 'Oluşturuldu',
+    'created_by' => 'Tarafından oluşturuldu',
+    'updated_at' => 'Yüklendi',
+    'updated_by' => 'Tarafından yüklendi',
+];
