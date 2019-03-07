24a947b3ddff3ff1ebecd59dc53530f27d634af2
Ryan Thompson <service@aiwebsystems.com>
2019-03-06 20:54:48 -0600

    Hiccups

--- src/Model/EloquentModel.php
+++ src/Model/EloquentModel.php
@@ -246,7 +246,7 @@ class EloquentModel extends Model implements Arrayable, PresentableInterface
      */
     public function getCacheCollectionKey()
     {
-        return static::class;
+        return get_class(app(static::class));
     }
 
     /**
