d7ffb705ac50004da98e8558681d83e9650e4c3b
Ryan Thompson <service@aiwebsystems.com>
2019-03-12 16:55:25 -0500

    Support

---
+++ resources/lang/en/support.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'link'        => 'Link',
+    'rss'         => 'RSS Feed',
+    'slack'       => 'Slack Team',
+    'irc'         => 'IRC Channel',
+    'source'      => 'Source Code',
+    'issues'      => 'Known Issues',
+    'wiki'        => 'Project Wiki',
+    'docs'        => 'Documentation',
+    'forum'       => 'Support Forum',
+    'support'     => ':type Support',
+    'information' => 'Addon Information',
+];
