153b61cc69906f451b9dd18e98ea6001ce31ecb7
Ryan Thompson <ryan@pyrocms.com>
2019-04-12 14:46:22 -0500

    Allow initial values to come from an empty entry.
    Such is the case with clone/copying.

--- src/Ui/Form/Component/Field/FieldPopulator.php
+++ src/Ui/Form/Component/Field/FieldPopulator.php
@@ -61,7 +61,7 @@ class FieldPopulator
              * If the field is not already set
              * then get the value off the entry.
              */
-            if (!isset($field['value']) && $entry instanceof EloquentModel && $entry->getId()) {
+            if (!isset($field['value']) && $entry instanceof EloquentModel) {
 
                 $locale = array_get($field, 'locale');
 
