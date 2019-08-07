ee37503beb0e17f55f93df9a31a38ece98834de6
Ryan Thompson <service@aiwebsystems.com>
2019-08-06 22:55:25 -0500

    Updating Laravel and dependencies.

--- src/View/ViewOverrides.php
+++ src/View/ViewOverrides.php
@@ -12,32 +12,4 @@ use Illuminate\Support\Collection;
 class ViewOverrides extends Collection
 {
 
-    /**
-     * Add an override.
-     *
-     * @param $view
-     * @param $override
-     * @return $this
-     */
-    public function add($view, $override)
-    {
-        $this->put(str_replace(['\\', '/'], '.', $view), str_replace(['\\', '/'], '.', $override));
-
-        return $this;
-    }
-
-    /**
-     * Force an override.
-     *
-     * @param $view
-     * @param $override
-     * @return $this
-     * @deprecated since 1.6; Use add($view, $override)
-     */
-    public function force($view, $override)
-    {
-        $this->add($view, $override);
-
-        return $this;
-    }
 }
