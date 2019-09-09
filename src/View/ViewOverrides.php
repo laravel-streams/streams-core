0a1618747aa146da56f3d0989cbf689a279c3ebd
Ryan Thompson <service@aiwebsystems.com>
2019-09-09 00:56:14 -0500

    Force view overrides into dot notation view names.

--- src/View/ViewOverrides.php
+++ src/View/ViewOverrides.php
@@ -12,4 +12,14 @@ use Illuminate\Support\Collection;
 class ViewOverrides extends Collection
 {
 
+    public function put($key, $value)
+    {
+
+        /**
+         * Force the key and value to use
+         * dot notation for view names.
+         */
+        return parent::put(str_replace('/', '.', $key), str_replace('/', '.', $value));
+    }
+
 }
