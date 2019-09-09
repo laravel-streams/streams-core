ac8f1db11652f0e59ad661d9b0a2e61087bc007f
Ryan Thompson <service@aiwebsystems.com>
2019-09-09 00:55:55 -0500

    Fix theme resolution in view loader.

--- src/View/Twig/Loader.php
+++ src/View/Twig/Loader.php
@@ -146,7 +146,7 @@ class Loader extends OriginalLoader
         $this->request   = $request;
         $this->overrides = $overrides;
 
-        $area = $request->segment(1) == 'admin' ?: 'standard';
+        $area = $request->segment(1) == 'admin' ? 'admin' : 'standard';
 
         $this->theme  = $this->addons->themes->active($area);
         $this->module = $this->addons->modules->active();
