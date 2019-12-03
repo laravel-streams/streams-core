53ad5bdc74dd4ef93c8cf44cdd698d55d64a5278
Ryan Thompson <service@aiwebsystems.com>
2019-12-03 00:44:47 -0600

    Cleanup cp.sections

--- src/Ui/ControlPanel/Component/Section/SectionInput.php
+++ src/Ui/ControlPanel/Component/Section/SectionInput.php
@@ -2,6 +2,7 @@
 
 namespace Anomaly\Streams\Platform\Ui\ControlPanel\Component\Section;
 
+use Anomaly\Streams\Platform\Ui\Support\Normalizer;
 use Anomaly\Streams\Platform\Ui\ControlPanel\ControlPanelBuilder;
 
 /**
@@ -23,8 +24,8 @@ class SectionInput
     public static function read(ControlPanelBuilder $builder)
     {
         self::resolve($builder);
+        self::normalize($builder);
 
-        SectionNormalizer::normalize($builder);
         SectionGuesser::guess($builder);
 
         self::evaluate($builder);
@@ -51,6 +52,70 @@ class SectionInput
     protected static function evaluate(ControlPanelBuilder $builder)
     {
         $builder->setSections(evaluate($builder->getSections(), compact('builder')));
+    }
+
+    /**
+     * Normalize the section input.
+     *
+     * @param ControlPanelBuilder $builder
+     */
+    protected static function normalize(ControlPanelBuilder $builder)
+    {
+        $sections = $builder->getSections();
+
+        /*
+         * Move child sections into main array.
+         */
+        foreach ($sections as $slug => &$section) {
+            if (isset($section['sections'])) {
+                foreach ($section['sections'] as $key => $child) {
+
+                    /**
+                     * It's a slug only!
+                     */
+                    if (is_string($child)) {
+
+                        $key = $child;
+
+                        $child = ['slug' => $child];
+                    }
+
+                    $child['parent'] = array_get($section, 'slug', $slug);
+                    $child['slug']   = array_get($child, 'slug', $key);
+
+                    $sections[$key] = $child;
+                }
+            }
+        }
+
+        /*
+         * Loop over each section and make sense of the input
+         * provided for the given module.
+         */
+        foreach ($sections as $slug => &$section) {
+
+            /*
+             * If the slug is not valid and the section
+             * is a string then use the section as the slug.
+             */
+            if (is_numeric($slug) && is_string($section)) {
+                $section = [
+                    'slug' => $section,
+                ];
+            }
+
+            /*
+             * If the slug is a string and the title is not
+             * set then use the slug as the slug.
+             */
+            if (is_string($slug) && !isset($section['slug'])) {
+                $section['slug'] = $slug;
+            }
+        }
+
+        $sections = Normalizer::attributes($sections);
+
+        $builder->setSections(array_values($sections));
     }
 
     /**
