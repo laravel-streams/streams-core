5ebadfeed0e215f91b275ead0723d22398d4f5b4
Ryan Thompson <ryan@pyrocms.com>
2019-12-05 00:23:47 -0600

    K

--- resources/views/form/partials/fields.blade.php
+++ resources/views/form/partials/fields.blade.php
@@ -1,6 +1,6 @@
 @foreach ($fields as $field)
     @if ($form->fields->translations($field)->isNotEmpty())
-        <div id="{{ $form->prefix('field-' . $field) }}" class="form__fields">
+        <div id="{{ $form->prefix('field-' . $field) }}" class="form__fieldset">
             @foreach ($form->fields->translations($field) as $field)
                 {!! $field->render(['form' => $form]) !!}
             @endforeach        
