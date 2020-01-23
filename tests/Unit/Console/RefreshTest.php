d177a6e317ed5777e5a343f728f540c3d09d50d2
Ryan Thompson <ryan@pyrocms.com>
2020-01-22 23:01:18 -0600

    Tests

--- tests/Unit/Console/RefreshTest.php
+++ tests/Unit/Console/RefreshTest.php
@@ -5,6 +5,9 @@ class RefreshTest extends TestCase
 
     public function testConsoleCommand()
     {
+        file_put_contents(base_path('bootstrap/cache/config.php'), '');
+        file_put_contents(base_path('bootstrap/cache/routes.php'), '');
+
         $this->artisan('refresh')->assertExitCode(0);
     }
 }
