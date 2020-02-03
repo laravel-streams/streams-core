d72c7d8a98dc04c6b09a0ac2d85a466522ec2b65
spektra2147 <39536659+spektra2147@users.noreply.github.com>
2020-02-03 17:03:10 +0300

    add default lang

--- src/Model/EloquentQueryBuilder.php
+++ src/Model/EloquentQueryBuilder.php
@@ -369,8 +369,9 @@ class EloquentQueryBuilder extends Builder
          */
         $this->query->where(
             function (\Illuminate\Database\Query\Builder $query) use ($model, $locale) {
-                $query->where($model->getTranslationsTableName() . '.locale', $locale ?: config('app.locale'));
-                $query->orWhere($model->getTranslationsTableName() . '.locale',setting_value('streams::default_locale'));
+                $query->where($model->getTranslationsTableName() . '.locale', $locale ?: config('app.locale'));//active lang
+                $query->orWhere($model->getTranslationsTableName() . '.locale',setting_value('streams::default_locale'));//system lang
+                $query->orWhere($model->getTranslationsTableName() . '.locale','en');//default lang
                 $query->orWhereNull($model->getTranslationsTableName() . '.locale');
             }
         );
