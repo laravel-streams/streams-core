2db32a467b4f4dc63098dbf828a56794d2f9d3b5
Ryan Thompson <service@aiwebsystems.com>
2020-03-18 17:12:51 -0500

    Buttons

--- resources/assets/js/components/Buttons.vue
+++ resources/assets/js/components/Buttons.vue
@@ -1,13 +1,29 @@
 <template>
-    <div class="buttons">
-        <slot></slot>
+    <div class="buttons__list">
+        <cp-button v-for="(button, index) in primary" :key="index" :button="button"></cp-button>
+
+        <div class="buttons__dropdown">
+            <cp-button v-for="(button, index) in secondary" :key="index" :button="button"></cp-button>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        ready() {
-            console.log('Component ready.')
+        props: {
+            buttons: Array,
+        },
+        computed: {
+            primary () {
+                return this.buttons.filter(button => {
+                    return button.primary == true;
+                });
+            },
+            secondary () {
+                return this.buttons.filter(button => {
+                    return button.primary == false;
+                });
+            }
         }
     }
 </script>
--- resources/views/ui/buttons/buttons.blade.php
+++ resources/views/ui/buttons/buttons.blade.php
@@ -1,15 +1,1 @@
-<cp-buttons>
-
-    @foreach($buttons->primary() as $button)
-    <cp-button :button="{{ $button->toJson() }}"></cp-button>
-    @endforeach
-
-    @if (($secondary = $buttons->secondary())->isNotEmpty())
-    <div class="buttons__secondary">
-        @foreach ($secondary as $button)
-        <cp-button :button="{{ $button->toJson() }}"></cp-button>
-        @endforeach    
-    </div>
-    @endif
-
-</cp-buttons>
+<cp-buttons :buttons="{{ $buttons->toJson() }}"></cp-buttons>
