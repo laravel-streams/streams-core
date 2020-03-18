aeee4dc1073267f1a1532d1f33d809b8bb12f981
Ryan Thompson <service@aiwebsystems.com>
2020-03-18 17:19:56 -0500

    Slots

--- resources/assets/js/components/Table.vue
+++ resources/assets/js/components/Table.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="o-table">
 
-        <cp-table-filters v-show="table.filters" :filters="table.filters"></cp-table-filters>
-        <cp-table-views v-show="table.views" :views="table.views"></cp-table-views>
-        <cp-table-heading v-show="table.options.title || table.options.description" :options="table.options"></cp-table-heading>
+        <slot name="filters">
+            <cp-table-filters v-show="table.filters" :filters="table.filters"></cp-table-filters>
+        </slot>
 
-        <slot></slot>
+        <slot name="views">
+            <cp-table-views v-show="table.views" :views="table.views"></cp-table-views>
+        </slot>
+
+        <slot name="heading">
+            <cp-table-heading v-show="table.options.title || table.options.description" :options="table.options"></cp-table-heading>
+        </slot>
+
+        <slot name="table"></slot>
 
     </div>
 
