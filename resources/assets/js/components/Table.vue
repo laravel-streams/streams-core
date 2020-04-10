ec13532d225a8106b88bbb1db4dfc682c7aca327
Ryan Thompson <ryan@pyrocms.com>
2020-04-10 00:58:06 -0500

    Leave them as cp-table slots for now

--- resources/assets/js/components/Table.vue
+++ resources/assets/js/components/Table.vue
@@ -5,9 +5,9 @@
             <cp-table-filters v-show="table.filters" :filters="table.filters"></cp-table-filters>
         </slot>
 
-        <!-- <slot name="views">
+        <slot name="views">
             <cp-table-views v-show="table.views" :views="table.views"></cp-table-views>
-        </slot> -->
+        </slot>
 
         <slot name="heading">
             <cp-table-heading v-show="table.options.title || table.options.description" :options="table.options"></cp-table-heading>
