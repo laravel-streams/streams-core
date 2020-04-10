243d17df34448f2e6dde7e5bb787226cc35c011d
Ryan Thompson <ryan@pyrocms.com>
2020-04-09 23:55:18 -0500

    Correct - no search method

--- tests/Unit/Addon/Module/ModuleCollectionTest.php
+++ tests/Unit/Addon/Module/ModuleCollectionTest.php
@@ -5,15 +5,4 @@ use Anomaly\Streams\Platform\Addon\Module\ModuleCollection;
 
 class ModuleCollectionTest extends TestCase
 {
-
-    /**
-     * @todo confirm that ModuleCollection does not have a search functionality (as it does not use composer keys)
-     */
-    public function testSearch()
-    {
-        $this->markTestSkipped();
-        $addons = app(ModuleCollection::class);
-
-        $this->assertTrue($addons->search('anomaly.module.users::authenticator.*')->isNotEmpty());
-    }
 }
