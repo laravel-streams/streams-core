a52d5d4972aa1c1d6a69c53b01b5af46af60a8c0
Craig Berry <hi@craigberry.co>
2020-04-14 12:17:15 +1000

    Howabout you dont misspell the function

--- tests/factories/_load.php
+++ tests/factories/_load.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!function_exists('loaddAddonFactory')) {
+if (!function_exists('loadAddonFactory')) {
     function loadAddonFactory($vendorPath, $filePath = null)
     {
         app(\Illuminate\Database\Eloquent\Factory::class)->load(base_path('vendor/' . $vendorPath . '/factories/' . ($filePath ?? '')));
