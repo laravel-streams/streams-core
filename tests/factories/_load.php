0a4ce59636d88ffddff17ef8e79ac2d60f4be126
Ryan Thompson <service@aiwebsystems.com>
2020-04-15 16:40:55 -0500

    CS

--- tests/factories/_load.php
+++ tests/factories/_load.php
@@ -1,8 +1,16 @@
 <?php
 
 if (!function_exists('loadAddonFactory')) {
+
+    /**
+     * Load an addon's factory.
+     *
+     * @param  string $path
+     * @return string
+     */
     function loadAddonFactory($vendorPath, $filePath = null)
     {
-        app(\Illuminate\Database\Eloquent\Factory::class)->load(base_path('vendor/' . $vendorPath . '/factories/' . ($filePath ?? '')));
+        app(\Illuminate\Database\Eloquent\Factory::class)
+            ->load(base_path('vendor/' . $vendorPath . '/factories/' . ($filePath ?? '')));
     }
 }
