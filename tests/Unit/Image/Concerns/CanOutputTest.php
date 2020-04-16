113fbdb43348e27f7a4f44891af6e015e7c1fbe2
Ryan Thompson <service@aiwebsystems.com>
2020-04-16 16:56:07 -0500

    Testing

---
+++ tests/Unit/Image/Concerns/CanOutputTest.php
@@ -0,0 +1,103 @@
+<?php
+
+use Tests\TestCase;
+use Anomaly\Streams\Platform\Image\Facades\Images;
+
+/**
+ * Class CanOutputTest
+ */
+class CanOutputTest extends TestCase
+{
+
+    public function testImg()
+    {
+        $this->assertEquals(
+            '<img  src="/vendor/anomaly/streams-platform/resources/testing/cat.jpg" alt="Cat">',
+            Images::make('streams::testing/cat.jpg')->img()
+        );
+
+        $this->assertEquals(
+            '<img  src="/vendor/anomaly/streams-platform/resources/testing/cat.jpg" alt="Cat">',
+            Images::register('cat', 'streams::testing/cat.jpg')->make('cat')->img()
+        );
+    }
+
+    public function testPicture()
+    {
+        $this->markTestIncomplete();
+
+        $this->assertEquals(
+            '<img  src="/vendor/anomaly/streams-platform/resources/testing/cat.jpg" alt="Cat">',
+            Images::make('streams::testing/cat.jpg')->picture()
+        );
+
+        $this->assertEquals(
+            '<img  src="/vendor/anomaly/streams-platform/resources/testing/cat.jpg" alt="Cat">',
+            Images::register('cat', 'streams::testing/cat.jpg')->make('cat')->img()
+        );
+    }
+
+    public function testEncode()
+    {
+        $this->assertTrue(
+            str_is(
+                '<img  src="/vendor/anomaly/streams-platform/resources/testing/*.png" alt="Cat">',
+                Images::make('streams::testing/cat.jpg')->encode('png')->img()
+            )
+        );
+    }
+
+    public function testBase64()
+    {
+        $this->assertTrue(
+            str_is(
+                'data:image/jpg*',
+                Images::make('streams::testing/cat.jpg')->base64()
+            )
+        );
+    }
+
+    public function testUrl()
+    {
+        $this->assertEquals(
+            url('vendor/anomaly/streams-platform/resources/testing/cat.jpg'),
+            Images::make('streams::testing/cat.jpg')->url()
+        );
+    }
+
+    public function testPath()
+    {
+        $this->assertEquals(
+            '/vendor/anomaly/streams-platform/resources/testing/cat.jpg',
+            Images::make('streams::testing/cat.jpg')->path()
+        );
+    }
+
+    public function testInline()
+    {
+        $this->assertTrue(
+            str_is(
+                '<img  src="data:image/jpg*" alt="Cat">',
+                Images::make('streams::testing/cat.jpg')->inline()
+            )
+        );
+    }
+
+    public function testCss()
+    {
+        $this->assertEquals(
+            'url(/vendor/anomaly/streams-platform/resources/testing/cat.jpg)',
+            Images::make('streams::testing/cat.jpg')->css()
+        );
+    }
+
+    public function testData()
+    {
+        $this->markTestIncomplete();
+
+        $this->assertStringContainsString(
+            file_get_contents(base_path('vendor/anomaly/streams-platform/resources/testing/cat.jpg')),
+            Images::make('streams::testing/cat.jpg')->data()
+        );
+    }
+}
