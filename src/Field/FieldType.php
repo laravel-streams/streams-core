3e466a93ba1dcf34ec8677971d491d0b886962a4
Ryan Thompson <ryan@pyrocms.com>
2020-06-19 01:06:48 -0500

    Ya

---
+++ src/Field/FieldType.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Anomaly\Streams\Platform\Field;
+
+use Anomaly\Streams\Platform\Traits\HasMemory;
+use Anomaly\Streams\Platform\Support\Facades\Hydrator;
+use Anomaly\Streams\Platform\Support\Traits\Properties;
+
+/**
+ * Class Field
+ *
+ * @link    http://pyrocms.com/
+ * @author  PyroCMS, Inc. <support@pyrocms.com>
+ * @author  Ryan Thompson <ryan@pyrocms.com>
+ */
+class FieldType
+{
+    use HasMemory;
+    use Properties;
+
+    /**
+     * Create a new class instance.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        $this->setAttributes([
+            'name' => null,
+            'slug' => null,
+            'type' => null,
+            'label' => null,
+            'stream' => null,
+            'warning' => null,
+            'placeholder' => null,
+            'instructions' => null,
+
+            'unique' => false,
+            'required' => false,
+            'searchable' => true,
+            'translatable' => false,
+
+            'config' => [],
+            'rules' => [],
+        ]);
+
+        $this->buildProperties();
+
+        $this->fill($attributes);
+    }
+
+    /**
+     * Modify the value for storage.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function modify($value)
+    {
+        return (string) $value;
+    }
+
+    /**
+     * Restore the value from storage.
+     *
+     * @param $value
+     * @return string
+     */
+    public function restore($value)
+    {
+        return (string) $value;
+    }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return Hydrator::dehydrate($this);
+    }
+
+    /**
+     * Convert the object to its JSON representation.
+     *
+     * @param int $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this->toArray(), $options);
+    }
+}
