2d354c0b9878354d0e33efce5f75f7678939caf3
Craig Berry <hi@craigberry.co>
2020-07-14 23:29:17 +1000

    fix: remove old route references

--- resources/routes/web.php
+++ resources/routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post(
-    'form/handle/{key}',
-    'Anomaly\Streams\Platform\Http\Controller\FormController@handle'
-);
-
 Route::get(
     'entry/handle/restore/{addon}/{stream}/{id}',
     'Anomaly\Streams\Platform\Http\Controller\EntryController@restore'
@@ -15,14 +10,4 @@ Route::get(
 Route::get(
     'entry/handle/export/{addon}/{stream}',
     'Anomaly\Streams\Platform\Http\Controller\EntryController@export'
-);
-
-Route::get(
-    'locks/touch',
-    'Anomaly\Streams\Platform\Http\Controller\LocksController@touch'
-);
-
-Route::get(
-    'locks/release',
-    'Anomaly\Streams\Platform\Http\Controller\LocksController@release'
 );
