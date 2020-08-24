68712c7d2e6c4c37fbdc0f30ed8dd35c7dd2f441
Ryan Thompson <service@aiwebsystems.com>
2020-08-24 17:28:25 -0500

    Do it locally

--- src/Stream/StreamBuilder.php
+++ src/Stream/StreamBuilder.php
@@ -41,12 +41,12 @@ class StreamBuilder
          * Merge extending Stream data.
          */
         if (isset($stream['extends'])) {
-            
+
             $parent = Streams::make($stream['extends'])->toArray()['attributes'];
 
             $fields = array_merge(Arr::pull($parent, 'fields', []), $fields);
 
-            $stream = array_merge_recursive_distinct($parent, $stream);
+            $stream = self::extend($parent, $stream);
         }
 
         $stream = StreamFactory::make($stream);
@@ -59,5 +59,20 @@ class StreamBuilder
         $stream->fire('built', compact($stream));
 
         return $stream;
+    }
+
+    public static function extend(array &$parent, array &$stream)
+    {
+        $merged = $parent;
+
+        foreach ($stream as $key => &$value) {
+            if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                $merged[$key] = array_merge_recursive_distinct($merged[$key], $value);
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+
+        return $merged;
     }
 }
