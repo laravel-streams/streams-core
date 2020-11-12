539ab89fe3edcfabce66b05465b9376f3f1cfec6
Ryan Thompson <ryan@pyrocms.com>
2020-11-11 22:11:17 -0600

    Code

--- tests/Support/Traits/PrototypeTest.php
+++ tests/Support/Traits/PrototypeTest.php
@@ -14,11 +14,43 @@ class PrototypeTest extends TestCase
 
         $prototype->name = 'Testing';
 
+        
         $this->assertTrue($prototype->name === 'Testing');
+        $this->assertTrue($prototype->getPrototypeAttribute('name') === 'Testing');
         
         $prototype->setPrototypeAttributes($prototype->getOriginalPrototypeAttributes());
 
         $this->assertTrue($prototype->name === 'Original');
+        $this->assertTrue(['name' => 'Original'] === $prototype->getPrototypeAttributes());
+    }
+
+    public function testCanLoadAttributes()
+    {
+        $prototype = new TestPrototype();
+
+        $this->assertTrue($prototype->name === 'Original');
+        $this->assertNull($prototype->description);
+
+        $prototype->loadPrototypeAttributes([
+            'name' => 'Testing',
+            'description' => 'Test',
+        ]);
+
+        $this->assertTrue($prototype->name === 'Testing');
+        $this->assertTrue($prototype->description === 'Test');
+    }
+
+    public function testCanSetAttributes()
+    {
+        $prototype = new TestPrototype();
+
+        $this->assertTrue($prototype->name === 'Original');
+
+        $prototype->setPrototypeAttributes([
+            'name' => 'Testing',
+        ]);
+
+        $this->assertTrue($prototype->name === 'Testing');
     }
 }
 
