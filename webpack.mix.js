b372ebeaad1840c4eae88c29a0af3a94d58b3b77
Robin Radic <rradic@hotmail.com>
2020-12-17 20:06:23 +0100

    working now

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,8 @@ let mix = require('laravel-mix');
 
 require('laravel-mix-purgecss');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 mix
     .ts('./resources/ts/index.ts', './resources/public/js')
     .copyDirectory(
@@ -17,12 +19,22 @@ mix
         function (webpack) {
 
             return {
+                devtool  : isDev ? '#source-map' : null,
                 plugins: [
                     require('@tailwindcss/ui'),
                 ],
                 output: {
                     library: ['streams', 'core'],
-                    libraryTarget: 'window'
+                    libraryTarget: 'window',
+                    devtoolFallbackModuleFilenameTemplate: 'webpack:///[resource-path]?[hash]',
+                    devtoolModuleFilenameTemplate        : info => {
+                        var $filename = 'sources://' + info.resourcePath;
+                        $filename = 'webpack:///' + info.resourcePath; // +'?' + info.hash;
+                        if ( info.resourcePath.match(/\.vue$/) && !info.allLoaders.match(/type=script/) && !info.query.match(/type=script/) ) {
+                            $filename = 'webpack-generated:///' + info.resourcePath; // + '?' + info.hash;
+                        }
+                        return $filename;
+                    }
                 }
             };
         })
