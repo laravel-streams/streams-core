ac0cdf9812f6cfcddd09051cdd5e9f7199369c5b
Ryan Thompson <ryan@pyrocms.com>
2021-01-31 02:30:26 -0600

    Docs

--- src/Field/Type/Boolean.php
+++ src/Field/Type/Boolean.php
@@ -3,6 +3,7 @@
 namespace Streams\Core\Field\Type;
 
 use Streams\Core\Field\FieldType;
+use Streams\Core\Field\Value\BooleanValue;
 
 class Boolean extends FieldType
 {
@@ -26,5 +27,16 @@ class Boolean extends FieldType
     public function restore($value)
     {
         return filter_var($value, FILTER_VALIDATE_BOOL);
+    }
+
+    /**
+     * Expand the value.
+     *
+     * @param $value
+     * @return Collection
+     */
+    public function expand($value)
+    {
+        return new BooleanValue($value);
     }
 }
