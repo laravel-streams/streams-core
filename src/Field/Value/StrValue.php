e79d756367fc40972d68b77e57b5599f0efafd12
Ryan Thompson <service@aiwebsystems.com>
2021-03-16 12:43:02 -0500

    Lines

--- src/Field/Value/StrValue.php
+++ src/Field/Value/StrValue.php
@@ -5,6 +5,11 @@ namespace Streams\Core\Field\Value;
 class StrValue extends Value
 {
 
+    public function lines()
+    {
+        return explode("\n", $this->value);
+    }
+
     /**
      * Normalize the URL by default.
      *
