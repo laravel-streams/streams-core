9ab4c27273f62d17ea763e464290e2019f3e1c83
Ryan Thompson <service@aiwebsystems.com>
2021-03-25 14:59:50 -0500

    Move types to config

--- resources/config/core.php
+++ resources/config/core.php
@@ -52,6 +52,67 @@ return [
         ],
     ],
 
+    'fields' => [
+        'types' => [
+
+            // Strings
+            'string' => \Streams\Core\Field\Type\Str::class,
+
+            'url' => \Streams\Core\Field\Type\Url::class,
+            'text' => \Streams\Core\Field\Type\Str::class,
+            'hash' => \Streams\Core\Field\Type\Hash::class,
+            'slug' => \Streams\Core\Field\Type\Slug::class,
+            'email' => \Streams\Core\Field\Type\Email::class,
+
+            'markdown' => \Streams\Core\Field\Type\Markdown::class,
+            'template' => \Streams\Core\Field\Type\Template::class,
+
+            // Numbers
+            'number' => \Streams\Core\Field\Type\Number::class,
+            'integer' => \Streams\Core\Field\Type\Integer::class,
+            'float' => \Streams\Core\Field\Type\Decimal::class,
+
+            'decimal' => \Streams\Core\Field\Type\Decimal::class,
+
+            // Boolean
+            'boolean' => \Streams\Core\Field\Type\Boolean::class,
+
+            // Arrays
+            'array' => \Streams\Core\Field\Type\Arr::class,
+
+            // Objects
+            'prototype' => \Streams\Core\Field\Type\Prototype::class,
+            'object' => \Streams\Core\Field\Type\Prototype::class,
+            'image' => \Streams\Core\Field\Type\Image::class,
+            'file' => \Streams\Core\Field\Type\File::class,
+
+            // Dates
+            'datetime' => \Streams\Core\Field\Type\Datetime::class,
+            'date' => \Streams\Core\Field\Type\Date::class,
+            'time' => \Streams\Core\Field\Type\Time::class,
+
+            // Selections
+            'select' => \Streams\Core\Field\Type\Select::class,
+            'multiselect' => \Streams\Core\Field\Type\Multiselect::class,
+
+            // Collections
+            // @todo Test me
+            'collection' => \Streams\Core\Field\Type\Collection::class,
+
+            // Streams
+            'entry' => \Streams\Core\Field\Type\Entry::class,
+            'entries' => \Streams\Core\Field\Type\Entries::class,
+
+            // Relationships
+            'multiple' => \Streams\Core\Field\Type\Multiple::class,
+            'polymorphic' => \Streams\Core\Field\Type\Polymorphic::class,
+            'relationship' => \Streams\Core\Field\Type\Relationship::class,
+
+            // Miscellaneous
+            'color' => \Streams\Core\Field\Type\Color::class,
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Image Customization
