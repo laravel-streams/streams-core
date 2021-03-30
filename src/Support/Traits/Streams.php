7ec7c0dd95275b1b6d634fc508f0c6c78827b59c
Ryan Thompson <service@aiwebsystems.com>
2021-03-30 17:08:05 -0500

    trait

--- src/Support/Traits/Streams.php
+++ src/Support/Traits/Streams.php
@@ -2,9 +2,9 @@
 
 namespace Streams\Core\Support\Traits;
 
+use Exception;
 use Streams\Core\Stream\Stream;
 use Streams\Core\Support\Facades\Streams as StreamsFacade;
-use Streams\Core\Support\Facades\Hydrator;
 
 /**
  * Trait Streams
@@ -15,10 +15,39 @@ use Streams\Core\Support\Facades\Hydrator;
 trait Streams
 {
 
-    use Prototype;
+    use Prototype {
+        Prototype::__call as private callPrototype;
+    }
 
     /**
-     * Return the model's stream instance.
+     * Mirror attribute changes.
+     *
+     * @param array $attributes
+     * @return Self
+     */
+    public function fill(array $attributes)
+    {
+        $this->fillPrototype($attributes);
+
+        return parent::fill($attributes);
+    }
+
+    /**
+     * Mirror attribute changes.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return mixed
+     */
+    public function setAttribute($key, $value)
+    {
+        $this->setPrototypeAttribute($key, $value);
+
+        return parent::setAttribute($key, $value);
+    }
+
+    /**
+     * Return the stream instance.
      */
     public function stream()
     {
@@ -30,6 +59,68 @@ trait Streams
             $this->stream = $this->table;
         }
 
-        return StreamsFacade::make($this->stream);
+        return $this->stream = StreamsFacade::make($this->stream);
+    }
+
+    /**
+     * Expand field values.
+     *
+     * @param string $key
+     */
+    public function expand($key)
+    {
+        return $this->expandPrototypeAttribute($key);
+    }
+
+    /**
+     * Mirror attribute changes.
+     *
+     * @param array $attributes
+     * @param bool $sync
+     */
+    public function setRawAttributes(array $attributes, $sync = false)
+    {
+        parent::setRawAttributes($attributes, $sync);
+
+        $this->initializePrototype($attributes);
+
+        if ($sync) {
+            $this->__prototype['original'] = $this->__prototype['attributes'];
+        }
+    }
+
+    /**
+     * Mirror attribute changes.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set($key, $value)
+    {
+        parent::__set($key, $value);
+
+        $this->setPrototypeAttributeValue($key, $value);
+    }
+
+    /**
+     * Try Eloquent model but fallback to ours. 
+     *
+     * @param $method
+     * @param $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        try {
+            return parent::__call($method, $arguments);
+        } catch (Exception $e) {
+            return $this->callPrototype($method, $arguments);
+        }
+
+        throw new \BadMethodCallException(sprintf(
+            'Method %s::%s does not exist.',
+            static::class,
+            $method
+        ));
     }
 }
