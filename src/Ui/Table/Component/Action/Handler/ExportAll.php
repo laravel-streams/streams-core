50392be91bf5e14f7e811b9f2761c9319e8fb88a
Ryan Thompson <service@aiwebsystems.com>
2021-09-28 00:54:02 -0500

    Same for export all

--- src/Ui/Table/Component/Action/Handler/ExportAll.php
+++ src/Ui/Table/Component/Action/Handler/ExportAll.php
@@ -1,9 +1,10 @@
 <?php namespace Anomaly\Streams\Platform\Ui\Table\Component\Action\Handler;
 
-use Anomaly\Streams\Platform\Model\EloquentModel;
-use Anomaly\Streams\Platform\Ui\Table\Component\Action\ActionHandler;
-use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
+use Illuminate\Support\Collection;
 use Illuminate\Routing\ResponseFactory;
+use Anomaly\Streams\Platform\Model\EloquentModel;
+use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
+use Anomaly\Streams\Platform\Ui\Table\Component\Action\ActionHandler;
 
 /**
  * Class ExportAll
@@ -30,25 +31,40 @@ class ExportAll extends ActionHandler
         ob_start();
 
         $headers = [
-            'Content-Disposition' => 'attachment; filename=' . $stream->getSlug() . '.csv',
+            //'Content-Disposition' => 'attachment; filename=' . $stream->getSlug() . '.csv',
             'Cache-Control'       => 'must-revalidate, post-check=0, pre-check=0',
-            'Content-type'        => 'text/csv',
+            //'Content-type'        => 'text/csv',
             'Pragma'              => 'public',
             'Expires'             => '0',
         ];
 
-        $callback = function () use ($selected, $model) {
+        $callback = function () use ($selected, $model, $builder) {
             
             $output = fopen('php://output', 'w');
 
+            $query = $model->newQuery();
+
+            $builder->fire('querying', compact('query'));
+
             /* @var EloquentModel $entry */
-            foreach ($model->all() as $k => $entry) {
+            foreach ($query->get() as $k => $entry) {
                 
+                $data = new Collection($entry->toArray());
+
+                $builder->fire('exporting', compact('data'));
+
                 if ($k == 0) {
-                    fputcsv($output, array_keys($entry->toArray()));
+                    fputcsv($output, $data->keys()->all());
                 }
 
-                fputcsv($output, $entry->toArray());
+                fputcsv($output, array_map(function($value) {
+
+                    if (is_array($value)) {
+                        return json_encode($value);
+                    }
+
+                    return $value;
+                }, $data->values()->all()));
 
                 ob_flush();
             }
