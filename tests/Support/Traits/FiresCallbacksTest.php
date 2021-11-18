b027af8048cd90fb58860254b80ac8638e4e4a89
Ryan Thompson <service@aiwebsystems.com>
2021-11-18 15:46:41 -0600

    Wonderful

--- tests/Support/Traits/FiresCallbacksTest.php
+++ tests/Support/Traits/FiresCallbacksTest.php
@@ -21,16 +21,66 @@ class FiresCallbacksTest extends TestCase
         $this->expectOutputString('Testing');
     }
 
-    // public function test_can_observe_callbacks()
-    // {
-    //     CallbacksTestClass::observeCallbacks(TestCallbackObserver::class);
+    public function test_can_observe_callbacks()
+    {
+        CallbacksTestClass::observeCallbacks(TestCallbackObserver::class);
 
-    //     $parent = new AnotherCallbacksTestClass;
+        $instance = new CallbacksTestClass;
 
-    //     $parent->fire('testing');
+        $instance->fire('test_observing');
 
-    //     $this->expectOutputString('Observed');
-    // }
+        $this->expectOutputString('Observed');
+    }
+
+    public function test_can_listen_for_callbacks()
+    {
+        CallbacksTestClass::addCallbackListener('test_listener', function () {
+            echo 'Listen!';
+        });
+
+        $instance = new CallbacksTestClass;
+
+        $instance->fire('test_listener');
+
+        $this->expectOutputString('Listen!');
+    }
+
+    public function test_callbacks_are_specific_to_instance()
+    {
+        $instance = new CallbacksTestClass;
+
+        $instance->addCallback('testing', function () {
+            echo 'Testing';
+        });
+
+        $another = new CallbacksTestClass;
+
+        $another->fire('testing');
+
+        $this->expectOutputString('');
+    }
+
+    public function test_callbacks_are_detectable()
+    {
+        $instance = new CallbacksTestClass;
+
+        $instance->addCallback('testing', function () {
+            echo 'Testing';
+        });
+
+        $this->assertTrue($instance->hasCallback('testing'));
+    }
+
+    public function test_listeners_are_detectable()
+    {
+        CallbacksTestClass::addCallbackListener('detect_listener', function () {
+            echo 'Listen!';
+        });
+
+        $instance = new CallbacksTestClass;
+
+        $this->assertTrue($instance->hasCallbackListener('detect_listener'));
+    }
 }
 
 class CallbacksTestClass
@@ -44,7 +94,7 @@ class AnotherCallbacksTestClass extends CallbacksTestClass
 
 class TestCallbackObserver
 {
-    public function onTesting()
+    public function testObserving()
     {
         echo 'Observed';
     }
