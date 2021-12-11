1ce85375f86126ab174985a09a7ed97ec4eb5690
Ryan Thompson <ryan@pyrocms.com>
2021-12-10 23:49:14 -0600

    CS

--- resources/config/core.php
+++ resources/config/core.php
@@ -5,7 +5,7 @@ return [
     /**
      * Specify the default directory for VCS compatible (file based) data.
      */
-    'data_path'      => env('STREAMS_DATA_PATH', 'streams/data'),
+    'data_path' => env('STREAMS_DATA_PATH', 'streams/data'),
 
     /**
      * Specify the default source adapter for streams.
@@ -15,18 +15,18 @@ return [
     /**
      * Configurable options for source adapters.
      */
-    'sources'        => [
+    'sources' => [
 
         'filebase' => [
 
             'default_format' => env('STREAMS_DEFAULT_FORMAT', 'json'),
 
             'formats' => [
-                'php'  => \Streams\Core\Criteria\Format\Php::class,
+                'php' => \Streams\Core\Criteria\Format\Php::class,
                 'json' => \Streams\Core\Criteria\Format\Json::class,
                 'yaml' => \Streams\Core\Criteria\Format\Yaml::class,
-                'md'   => \Streams\Core\Criteria\Format\Markdown::class,
-                'tpl'  => \Streams\Core\Criteria\Format\Template::class,
+                'md' => \Streams\Core\Criteria\Format\Markdown::class,
+                'tpl' => \Streams\Core\Criteria\Format\Template::class,
             ],
         ],
     ],
@@ -34,70 +34,70 @@ return [
     /**
      * Configure the default field types.
      */
-    'field_types'    => [
+    'field_types' => [
 
         // Numbers
-        'number'  => \Streams\Core\Field\Type\Number::class,
+        'number' => \Streams\Core\Field\Type\Number::class,
         'integer' => \Streams\Core\Field\Type\Integer::class,
         'decimal' => \Streams\Core\Field\Type\Decimal::class,
 
         // Strings
-        'string'  => \Streams\Core\Field\Type\Str::class,
+        'string' => \Streams\Core\Field\Type\Str::class,
 
-        'url'          => \Streams\Core\Field\Type\Url::class,
-        'uuid'         => \Streams\Core\Field\Type\Uuid::class,
-        'hash'         => \Streams\Core\Field\Type\Hash::class,
-        'slug'         => \Streams\Core\Field\Type\Slug::class,
-        'email'        => \Streams\Core\Field\Type\Email::class,
-        'encrypted'    => \Streams\Core\Field\Type\Encrypted::class,
+        'url' => \Streams\Core\Field\Type\Url::class,
+        'uuid' => \Streams\Core\Field\Type\Uuid::class,
+        'hash' => \Streams\Core\Field\Type\Hash::class,
+        'slug' => \Streams\Core\Field\Type\Slug::class,
+        'email' => \Streams\Core\Field\Type\Email::class,
+        'encrypted' => \Streams\Core\Field\Type\Encrypted::class,
 
         // Markup
-        'markdown'     => \Streams\Core\Field\Type\Markdown::class,
-        'template'     => \Streams\Core\Field\Type\Template::class,
+        'markdown' => \Streams\Core\Field\Type\Markdown::class,
+        'template' => \Streams\Core\Field\Type\Template::class,
 
         // Boolean
-        'boolean'      => \Streams\Core\Field\Type\Boolean::class,
+        'boolean' => \Streams\Core\Field\Type\Boolean::class,
 
         // Dates
-        'datetime'     => \Streams\Core\Field\Type\Datetime::class,
-        'date'         => \Streams\Core\Field\Type\Date::class,
-        'time'         => \Streams\Core\Field\Type\Time::class,
+        'datetime' => \Streams\Core\Field\Type\Datetime::class,
+        'date' => \Streams\Core\Field\Type\Date::class,
+        'time' => \Streams\Core\Field\Type\Time::class,
 
         // Arrays
-        'array'        => \Streams\Core\Field\Type\Arr::class,
+        'array' => \Streams\Core\Field\Type\Arr::class,
 
         // Selections
-        'select'       => \Streams\Core\Field\Type\Select::class,
-        'multiselect'  => \Streams\Core\Field\Type\Multiselect::class,
+        'select' => \Streams\Core\Field\Type\Select::class,
+        'multiselect' => \Streams\Core\Field\Type\Multiselect::class,
 
         // Collections
         // @todo Test me
         // 'collection' => \Streams\Core\Field\Type\Collection::class,
 
         // Objects
-        'prototype'    => \Streams\Core\Field\Type\Prototype::class,
-        'object'       => \Streams\Core\Field\Type\Structure::class,
-        'image'        => \Streams\Core\Field\Type\Image::class,
-        'file'         => \Streams\Core\Field\Type\File::class,
+        'prototype' => \Streams\Core\Field\Type\Prototype::class,
+        'object' => \Streams\Core\Field\Type\Structure::class,
+        'image' => \Streams\Core\Field\Type\Image::class,
+        'file' => \Streams\Core\Field\Type\File::class,
 
         // Streams
-        'entry'        => \Streams\Core\Field\Type\Entry::class,
-        'entries'      => \Streams\Core\Field\Type\Entries::class,
+        'entry' => \Streams\Core\Field\Type\Entry::class,
+        'entries' => \Streams\Core\Field\Type\Entries::class,
 
         // Relationships
-        'multiple'     => \Streams\Core\Field\Type\Multiple::class,
-        'polymorphic'  => \Streams\Core\Field\Type\Polymorphic::class,
+        'multiple' => \Streams\Core\Field\Type\Multiple::class,
+        'polymorphic' => \Streams\Core\Field\Type\Polymorphic::class,
         'relationship' => \Streams\Core\Field\Type\Relationship::class,
 
         // Miscellaneous
-        'color'        => \Streams\Core\Field\Type\Color::class,
+        'color' => \Streams\Core\Field\Type\Color::class,
     ],
 
     /**
      * Enabling this feature automatically
      * generages alt tags when not specified.
      */
-    'auto_alt'       => env('STREAMS_AUTO_ALT', true),
+    'auto_alt' => env('STREAMS_AUTO_ALT', true),
 
     /**
      * Enabling this feature automatically
@@ -108,12 +108,12 @@ return [
     'version_images' => env('STREAMS_VERSION_IMAGES', true),
 
     'markdown' => [
-        'configs'    => [
-            'commonmark'          => [
-                'use_asterisk'           => true,
-                'use_underscore'         => true,
-                'enable_strong'          => true,
-                'enable_em'              => true,
+        'configs' => [
+            'commonmark' => [
+                'use_asterisk' => true,
+                'use_underscore' => true,
+                'enable_strong' => true,
+                'enable_em' => true,
                 'unordered_list_markers' => [ '*', '+', '-' ],
             ],
             'disallowed_raw_html' => [
