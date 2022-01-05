3824602762763761d323985a6ae13f00169bc0d6
Ryan Thompson <ryan@pyrocms.com>
2022-01-05 16:22:28 -0600

    Static is 8

--- src/Field/FieldCollection.php
+++ src/Field/FieldCollection.php
@@ -12,7 +12,7 @@ class FieldCollection extends Collection
         return $this->get($key);
     }
 
-    public function required($required = true): static
+    public function required($required = true): self
     {
         return $this->filter(fn ($field) => $field->hasRule('required') === $required);
     }
