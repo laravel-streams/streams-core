f3398a04fdf8c976a3e66529656ad3d96c1763f2
Ryan Thompson <ryan@pyrocms.com>
2022-02-10 01:39:20 -0600

    Lock to L8 real quick

--- src/Field/Value/StrValue.php
+++ src/Field/Value/StrValue.php
@@ -2,12 +2,20 @@
 
 namespace Streams\Core\Field\Value;
 
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\View;
+
 class StrValue extends Value
 {
 
     public function lines()
     {
         return explode("\n", $this->value);
+    }
+
+    public function __call($method, $arguments)
+    {
+        return Str::{$method}(...$arguments);
     }
 
     /**
