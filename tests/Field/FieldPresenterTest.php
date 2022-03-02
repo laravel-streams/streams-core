ab9bea0404cd88bc784070cee6b60956493d37cd
Ryan Thompson <ryan@pyrocms.com>
2022-03-02 03:35:32 -0600

    FieldPresenter tests

---
+++ tests/Field/FieldPresenterTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Streams\Core\Tests\Field;
+
+use Streams\Core\Tests\CoreTestCase;
+use Streams\Core\Support\Facades\Streams;
+use Streams\Core\Field\Types\IntegerFieldType;
+
+class FieldPresenterTest extends CoreTestCase
+{
+    public function test_it_can_set_and_get_field()
+    {
+        $field = new IntegerFieldType([
+            'foo' => 'bar',
+            'stream' => Streams::make('films')
+        ]);
+
+        $this->assertSame('bar', $field->decorate('100')->getField()->foo);
+        $this->assertSame(100, $field->decorate(100)->getValue());
+    }    
+}
