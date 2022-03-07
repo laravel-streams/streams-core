70df83df1b727ad4d7951cbb3bfbb8dd75984447
Robin Radic <rradic@hotmail.com>
2022-03-07 19:40:21 +0100

    core js moved over to ui

--- resources/lib/Support/utils.ts
+++
@@ -1,5 +0,0 @@
-import { IServiceProviderClass, ServiceProvider } from './ServiceProvider';
-
-export const makeLog = (namespace:string) => require('debug')(namespace)
-
-export const isServiceProviderClass = (value: any): value is IServiceProviderClass => !(value instanceof ServiceProvider);
--- resources/lib/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    export interface StreamsGlobal {
-        core: typeof import('./index');
-    }
-
-    export interface Window {
-        streams: StreamsGlobal;
-    }
-}
