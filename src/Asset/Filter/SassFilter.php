bb1431288fe768f364cc5800342566208b9431ed
spektra2147 <spektra2147@gmail.com>
2022-10-05 15:46:52 +0300

    update scss package namespace

--- src/Asset/Filter/SassFilter.php
+++ src/Asset/Filter/SassFilter.php
@@ -4,7 +4,7 @@ use Anomaly\Streams\Platform\Support\Collection;
 use Assetic\Asset\AssetInterface;
 use Assetic\Filter\SassphpFilter;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Leafo\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\Compiler;
 
 /**
  * Class SassFilter
