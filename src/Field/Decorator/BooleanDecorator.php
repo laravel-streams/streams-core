692d5426fc67c65b9b8727add5dda8620cc8f0ce
Ryan Thompson <ryan@pyrocms.com>
2023-03-08 14:03:51 -0600

    Boolean decorator

---
+++ src/Field/Decorator/BooleanDecorator.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Streams\Core\Field\Decorator;
+
+use Streams\Core\Field\FieldDecorator;
+
+class BooleanDecorator extends FieldDecorator
+{
+
+    /**
+     * Return whether the value is true.
+     *
+     * @return bool
+     */
+    public function isTrue()
+    {
+        return $this->is(true);
+    }
+
+    /**
+     * Return whether the value is false.
+     *
+     * @return bool
+     */
+    public function isFalse()
+    {
+        return $this->is(false);
+    }
+
+    /**
+     * Return if the value is true / false.
+     *
+     * @param $test
+     * @return bool
+     */
+    public function is($test)
+    {
+        return $this->value === filter_var($test, FILTER_VALIDATE_BOOLEAN);
+    }
+
+    /**
+     * Return the text value.
+     *
+     * @param  null $on
+     * @param  null $off
+     * @return string
+     */
+    public function text($on = null, $off = null)
+    {
+        if ($on && $this->value) {
+            return $on;
+        }
+
+        if ($off && !$this->value) {
+            return $off;
+        }
+
+        return trans($this->value ? 'ui::message.on' : 'ui::message.off');
+    }
+}
