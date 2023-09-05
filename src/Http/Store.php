913ccec9229029a1f6df3429917e849a94aaab1a
Aidan Watt <aidan@hotfootdesign.co.uk>
2023-09-05 13:41:33 +0100

    Update a couple of extended methods that have a new interface

--- src/Http/Store.php
+++ src/Http/Store.php
@@ -24,7 +24,7 @@ class Store extends \Symfony\Component\HttpKernel\HttpCache\Store
      *
      * @return string A key for the given Request
      */
-    protected function generateCacheKey(Request $request)
+    protected function generateCacheKey(Request $request) : string
     {
 
         /**
