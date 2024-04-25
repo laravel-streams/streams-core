122099a2a77ba7cef4460a33f9c1a2e70c8543d8
Ryan Thompson <ryan@pyrocms.com>
2024-04-25 09:45:42 -0500

    Clean

--- resources/public/types/Support/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { IServiceProviderClass } from './ServiceProvider';
-export declare const makeLog: (namespace: string) => any;
-export declare const isServiceProviderClass: (value: any) => value is IServiceProviderClass;
