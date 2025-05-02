2bbee2a17820edfc272911fa2f26f5c517c4b478
Ryan Thompson <ryan@pyrocms.com>
2025-05-02 09:35:31 -0500

    Update

--- src/View/ViewIncludes.php
+++ src/View/ViewIncludes.php
@@ -11,7 +11,7 @@ class ViewIncludes extends Collection
     public function include(
         string $slot,
         string $name,
-        ?string $include
+        ?string $include = null
     ): ViewIncludes {
 
         $this->slot($slot)->put($name, $include ?: $name);
